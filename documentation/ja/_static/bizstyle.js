8df89368f708cd29d299621f85105de299fdb8c4
Travis CI User <travis@example.org>
2017-12-22 21:23:31 +0000

    Automated documentation update from SDK
    Commit: 76d1e66347b383aab5a9a6fc6b26ef29d7f9b4fb
    
    Travis build: https://travis-ci.org/QISKit/qiskit-sdk-py/builds/320400217

--- documentation/ja/_static/bizstyle.js
+++ documentation/ja/_static/bizstyle.js
@@ -39,7 +39,7 @@ $(window).resize(function(){
         $("div.related:last  ul li:not(.right, #toc-toggle):first a").text("Top");
     }
     else {
-        $("div.related:first ul li:not(.right, #toc-toggle):first a").text("QISKit SDK 0.3.15 ドキュメント");
-        $("div.related:last  ul li:not(.right, #toc-toggle):first a").text("QISKit SDK 0.3.15 ドキュメント");
+        $("div.related:first ul li:not(.right, #toc-toggle):first a").text("QISKit SDK 0.3.16 ドキュメント");
+        $("div.related:last  ul li:not(.right, #toc-toggle):first a").text("QISKit SDK 0.3.16 ドキュメント");
     }
 });
