1443859941796dad0aaa461f6aec6ae76fde104f
Abdón Rodríguez Davila <a@abdonrd.com>
2018-07-10 10:20:02 +0200

    Update copys

--- src/components/page-home.js
+++ src/components/page-home.js
@@ -213,7 +213,6 @@ class PageHome extends localize(i18next)(PageViewElement) {
         <div>
           <h1>${i18next.t('pages.home.headerTitle')}</h1>
           <h2>${i18next.t('pages.home.headerSubTitle')}</h2>
-          <p>${i18next.t('pages.home.headerDescription')}</p>
           <div class="badges">
             <a
                 href="https://github.com/Qiskit"
