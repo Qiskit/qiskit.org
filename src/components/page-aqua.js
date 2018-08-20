a7a6cbe0be64373396455e638f2edc8afae9ab66
Abdón Rodríguez Davila <a@abdonrd.com>
2018-08-20 18:21:43 +0200

    Add Aqua Chemistry information

--- src/components/page-aqua.js
+++ src/components/page-aqua.js
@@ -49,6 +49,19 @@ class PageAqua extends localize(i18next)(connect(store)(PageViewElement)) {
         .stack-list.aqua-applications-domains::before {
           height: 50px;
         }
+
+        section .description .badges a {
+          text-decoration: none;
+        }
+
+        section .description .badges vaadin-button {
+          --ibmq-button-secondary-color: #000000;
+          --ibmq-button-secondary-focus-color: #FFFFFF;
+        }
+
+        section .description img + img {
+          margin-top: 1em;
+        }
       </style>
 
       <header>
@@ -155,7 +168,7 @@ class PageAqua extends localize(i18next)(connect(store)(PageViewElement)) {
             <p class="note">${i18next.t('pages.aqua.pythonIsRequired')}</p>
             <code-sample type="bash">
               <template>
-                [python3] $ pip install qiskit-aqua qiskit-aqua-chemistry
+                [python3] $ pip install qiskit-aqua
               </template>
             </code-sample>
             <h3>${i18next.t('pages.aqua.exampleTitle')}</h3>
@@ -188,6 +201,126 @@ class PageAqua extends localize(i18next)(connect(store)(PageViewElement)) {
               </template>
               <!-- htmlmin:ignore -->
             </code-sample>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div class="row limited-width">
+          <div class="description">
+            <h3>
+              ${i18next.t('pages.aqua.chemistryTitle')}
+              <a
+                  href="https://pypi.python.org/pypi/qiskit-aqua-chemistry"
+                  target="_blank"
+                  rel="noopener">
+                <img
+                    src="https://img.shields.io/pypi/v/qiskit-aqua-chemistry.svg"
+                    alt="Qiskit Aqua version badge"
+                    width="78px"
+                    height="20px">
+              </a>
+            </h3>
+            <div class="badges">
+              <a
+                  href="https://github.com/Qiskit/aqua-chemistry"
+                  target="_blank"
+                  rel="noopener"
+                  tabindex="-1">
+                <vaadin-button theme="secondary small">
+                  ${githubIcon}
+                  GitHub
+                </vaadin-button>
+              </a>
+              <a
+                  href="/documentation/aqua/aqua_chemistry.html"
+                  target="_blank"
+                  rel="noopener"
+                  tabindex="-1">
+                <vaadin-button theme="secondary small">
+                  ${i18next.t('documentation')}
+                </vaadin-button>
+              </a>
+              <a
+                  href="https://nbviewer.jupyter.org/github/QISKit/qiskit-acqua-tutorials/tree/master/chemistry/"
+                  target="_blank"
+                  rel="noopener"
+                  tabindex="-1">
+                <vaadin-button theme="secondary small">
+                  ${i18next.t('tutorials')}
+                </vaadin-button>
+              </a>
+            </div>
+            <p>${i18next.t('pages.aqua.chemistryDescription')}</p>
+            <img src="images/aqua/aqua-chemistry-LiHEnergy.gif" alt="${i18next.t('pages.aqua.entanglionAltImage')}">
+            <img src="images/aqua/aqua-chemistry-LiHDipole.gif" alt="${i18next.t('pages.aqua.entanglionAltImage')}">
+          </div>
+          <div class="illustration">
+            <h3>${i18next.t('pages.aqua.installTitle')}</h3>
+            <p class="note">${i18next.t('pages.aqua.pythonIsRequired')}</p>
+            <code-sample type="bash">
+              <template>
+                [python3] $ pip install qiskit-aqua-chemistry
+              </template>
+            </code-sample>
+            <h3>${i18next.t('pages.aqua.exampleTitle')}</h3>
+            <code-sample type="python" copy-clipboard-button>
+              <!-- htmlmin:ignore -->
+              <template>
+                import numpy as np
+                from qiskit_aqua_chemistry import AquaChemistry
+
+                aqua_chemistry_dict = {
+                  "driver": { "name": "PYSCF" },
+                  "PYSCF": { "atom": "", "basis": "sto3g" },
+                  "operator": {
+                    "name": "hamiltonian",
+                    "qubit_mapping": "parity",
+                    "two_qubit_reduction": True,
+                    "freeze_core": True,
+                    "orbital_reduction": [-3, -2]
+                  },
+                  "algorithm": { "name": "VQE" },
+                  "optimizer": { "name": "COBYLA", "maxiter": 10000 },
+                  "variational_form": { "name": "UCCSD" },
+                  "initial_state": { "name": "HartreeFock" }
+                }
+                molecule = "H .0 .0 -{0}; Li .0 .0 {0}"
+
+                pts  = [x * 0.1  for x in range(6, 20)]
+                pts += [x * 0.25 for x in range(8, 16)]
+                pts += [4.0]
+                energies = np.empty(len(pts))
+                distances = np.empty(len(pts))
+                dipoles = np.empty(len(pts))
+
+                for i, d in enumerate(pts):
+                  aqua_chemistry_dict["PYSCF"]["atom"] = molecule.format(d/2)
+                  solver = AquaChemistry()
+                  result = solver.run(aqua_chemistry_dict)
+                  energies[i] = result["energy"]
+                  dipoles[i] = result["total_dipole_moment"] / 0.393430307
+                  distances[i] = d
+
+                for j in range(len(distances)):
+                  print("{:0.2f}: Energy={:0.8f}, Dipole={:0.5f}".format(distances[j], energies[j], dipoles[j]))
+              </template>
+              <!-- htmlmin:ignore -->
+            </code-sample>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div class="row limited-width">
+          <div class="description">
+            <h3>${i18next.t('pages.aqua.chemistryGui')}</h3>
+            <p>${i18next.t('pages.aqua.chemistryGuiDescription1')}</p>
+            <p>${i18next.t('pages.aqua.chemistryGuiDescription2')}</p>
+            <p>${i18next.t('pages.aqua.chemistryGuiDescription3')}</p>
+          </div>
+          <div class="illustration">
+            <img src="images/aqua/aqua-chemistry-gui.gif" alt="${i18next.t('pages.aqua.chemistryGuiAltImage')}">
           </div>
         </div>
       </section>
