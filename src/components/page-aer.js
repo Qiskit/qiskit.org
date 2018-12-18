7ef4f98fdf3781fb85e70eebbb3163a172931a27
Abdón Rodríguez Davila <a@abdonrd.com>
2018-12-18 16:44:57 +0100

    Add Aer example

--- src/components/page-aer.js
+++ src/components/page-aer.js
@@ -152,11 +152,35 @@ class PageAer extends localize(i18next)(PageViewElement) {
               <!-- htmlmin:ignore -->
               <template>
                 from qiskit import QuantumRegister, ClassicalRegister
-                from qiskit import QuantumCircuit, execute, IBMQ
-                from qiskit_aerimport Aer
-                from qiskit_aer.noise.device import depolarizing_noise_model
+                from qiskit import QuantumCircuit, execute, Aer, IBMQ
+                from qiskit.providers.aer import noise
 
-                ...
+                # Choose a real device to simulate
+                IBMQ.load_accounts()
+                device = IBMQ.get_backend('ibmq_16_melbourne')
+                properties = device.properties()
+                coupling_map = device.configuration().coupling_map
+
+                # Generate an Aer noise model for device
+                noise_model = noise.device.basic_device_noise_model(properties)
+                basis_gates = noise_model.basis_gates
+
+                # Generate a quantum circuit
+                q = QuantumRegister(2)
+                c = ClassicalRegister(2)
+                qc = QuantumCircuit(q, c)
+
+                qc.h(q[0])
+                qc.cx(q[0], q[1])
+                qc.measure(q, c)
+
+                # Perform noisy simulation
+                backend = Aer.get_backend('qasm_simulator')
+                job_sim = execute(qc, backend,
+                                  coupling_map=coupling_map,
+                                  noise_model=noise_model,
+                                  basis_gates=basis_gates)
+                sim_result = job_sim.result()
 
                 print(sim_result.get_counts(qc))
               </template>
