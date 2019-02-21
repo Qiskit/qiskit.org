c2daf3f4e3853f3ebb454a2171294171e48d41ea
Carlos Azaustre <carlos.azaustre@ibm.com>
2019-02-21 09:26:44 +0100

    Revert "add citation section"
    This reverts commit 1e690959a57ed859e3410df37155e917cf682c9c.

--- src/components/page-home.js
+++ src/components/page-home.js
@@ -169,206 +169,236 @@ class PageHome extends localize(i18next)(LitElement) {
   render() {
     // prettier-ignore
     const supportersListTemplate = html`
-<ul class="supporters-list">
-  ${this.organizations.map(organization => html`
-<li>
-  ${organization.url ? html`
-<a href="${organization.url}" target="_blank" rel="noopener">
-  <span class="name">${organization.name}</span>
-</a>
-` : html`
-${organization.name}
-`}
-</li>
-`)}
-  ${this.collaborators.map(collaborator => html`
-<li>
-  ${collaborator.url ? html`
-<a href="${collaborator.url}" target="_blank" rel="noopener">
-  <div class="supporter">
-    <span class="name">${collaborator.name}</span>
-    <span class="institution">${collaborator.institution}</span>
-  </div>
-</a>
-` : html`
-<div class="supporter">
-  <span class="name">${collaborator.name}</span>
-  <span class="institution">${collaborator.institution}</span>
-</div>
-`}
-</li>
-`)}
-</ul>
-`;
+      <ul class="supporters-list">
+        ${this.organizations.map(organization => html`
+          <li>
+            ${organization.url ? html`
+              <a href="${organization.url}" target="_blank" rel="noopener">
+                <span class="name">${organization.name}</span>
+              </a>
+            ` : html`
+              ${organization.name}
+            `}
+          </li>
+        `)}
+        ${this.collaborators.map(collaborator => html`
+          <li>
+            ${collaborator.url ? html`
+              <a href="${collaborator.url}" target="_blank" rel="noopener">
+                <div class="supporter">
+                  <span class="name">${collaborator.name}</span>
+                  <span class="institution">${collaborator.institution}</span>
+                </div>
+              </a>
+            ` : html`
+              <div class="supporter">
+                <span class="name">${collaborator.name}</span>
+                <span class="institution">${collaborator.institution}</span>
+              </div>
+            `}
+          </li>
+        `)}
+      </ul>
+    `;
 
     // prettier-ignore
     return html`
-<header>
-  <img src="images/qiskit-logo.png" .alt=${i18next.t('pages.home.altLogo')}>
-  <div>
-    <h1>
-      ${i18next.t('pages.home.headerTitle')}
-      <a href="https://pypi.python.org/pypi/qiskit" target="_blank" rel="noopener">
-        <img src="https://img.shields.io/pypi/v/qiskit.svg" alt="Qiskit version badge"
-             width="78px" height="20px">
-      </a>
-    </h1>
-    <h2>${i18next.t('pages.home.headerSubTitle')}</h2>
-    <div class="badges">
-      <a href="https://github.com/Qiskit" target="_blank" rel="noopener" tabindex="-1">
-        <vaadin-button theme="secondary small">${githubIcon} GitHub</vaadin-button>
-      </a>
-      <a href="https://join.slack.com/t/qiskit/shared_invite/enQtNDc2NjUzMjE4Mzc0LTMwZmE0YTM4ZThiNGJmODkzN2Y2NTNlMDIwYWNjYzA2ZmM1YTRlZGQ3OGM0NjcwMjZkZGE0MTA4MGQ1ZTVmYzk"
-         target="_blank" rel="noopener" tabindex="-1">
-        <vaadin-button theme="secondary small">${slackIcon}
-          ${i18next.t('joinSlack')}</vaadin-button>
-      </a>
-    </div>
-  </div>
-</header>
-
-<section class="news">
-  <div class="row limited-width">
-    <div class="column">
-      <div class="description">
-        <h3>${i18next.t('pages.home.newsQCampTitle')}</h3>
-        <p>${i18next.t('pages.home.newsQCampDescription')}</p>
-        <div class="actions">
-          <a href="https://qiskit.camp" target="_blank" rel="noopener" tabindex="-1">
-            <vaadin-button theme="secondary">${i18next.t('pages.home.newsQCampButton')}</vaadin-button>
-          </a>
+      <header>
+        <img src="images/qiskit-logo.png" .alt=${i18next.t('pages.home.altLogo')}>
+        <div>
+          <h1>
+            ${i18next.t('pages.home.headerTitle')}
+            <a
+                href="https://pypi.python.org/pypi/qiskit"
+                target="_blank"
+                rel="noopener">
+              <img
+                  src="https://img.shields.io/pypi/v/qiskit.svg"
+                  alt="Qiskit version badge"
+                  width="78px"
+                  height="20px">
+            </a>
+          </h1>
+          <h2>${i18next.t('pages.home.headerSubTitle')}</h2>
+          <div class="badges">
+            <a
+                href="https://github.com/Qiskit"
+                target="_blank"
+                rel="noopener"
+                tabindex="-1">
+              <vaadin-button theme="secondary small">${githubIcon} GitHub</vaadin-button>
+            </a>
+            <a
+                href="https://join.slack.com/t/qiskit/shared_invite/enQtNDc2NjUzMjE4Mzc0LTMwZmE0YTM4ZThiNGJmODkzN2Y2NTNlMDIwYWNjYzA2ZmM1YTRlZGQ3OGM0NjcwMjZkZGE0MTA4MGQ1ZTVmYzk"
+                target="_blank"
+                rel="noopener"
+                tabindex="-1">
+              <vaadin-button theme="secondary small">${slackIcon} ${i18next.t('joinSlack')}</vaadin-button>
+            </a>
+          </div>
         </div>
-      </div>
-    </div>
-    <div class="column video">
-      <div class="description">
-        <h3>${i18next.t('pages.home.newsVideoTitle')}</h3>
-        <p>${i18next.t('pages.home.newsVideoDescription')}</p>
-        <div class="actions">
-          <a href="https://www.youtube.com/watch?v=V3hXSftZuoc" tabindex="-1">
-            <vaadin-button theme="secondary">${i18next.t('pages.home.newsVideoButton')}</vaadin-button>
-          </a>
+      </header>
+
+      <section class="news">
+        <div class="row limited-width">
+          <div class="column">
+            <div class="description">
+              <h3>${i18next.t('pages.home.newsQCampTitle')}</h3>
+              <p>${i18next.t('pages.home.newsQCampDescription')}</p>
+              <div class="actions">
+                <a
+                    href="https://qiskit.camp"
+                    target="_blank"
+                    rel="noopener"
+                    tabindex="-1">
+                  <vaadin-button theme="secondary">${i18next.t('pages.home.newsQCampButton')}</vaadin-button>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="column video">
+            <div class="description">
+              <h3>${i18next.t('pages.home.newsVideoTitle')}</h3>
+              <p>${i18next.t('pages.home.newsVideoDescription')}</p>
+              <div class="actions">
+                <a href="https://www.youtube.com/watch?v=V3hXSftZuoc" tabindex="-1">
+                  <vaadin-button theme="secondary">${i18next.t('pages.home.newsVideoButton')}</vaadin-button>
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</section>
+      </section>
 
-<section class="colored">
-  <div class="row limited-width">
-    <div class="description">
-      <h3>${i18next.t('pages.home.tryTitle')}</h3>
-      <p>${i18next.t('pages.home.tryDescription')}</p>
-      <div class="actions">
-        <a href="https://mybinder.org/v2/gh/QISKit/qiskit-tutorial/master?filepath=index.ipynb"
-           target="_blank" rel="noopener" tabindex="-1">
-          <vaadin-button theme="secondary">${i18next.t('pages.home.tryButton')}</vaadin-button>
-        </a>
-      </div>
-    </div>
-    <div class="illustration">
-      <img src="images/try-and-learn.jpg" .alt=${i18next.t('pages.home.ibmQExperienceAltImage')}>
-    </div>
-  </div>
-</section>
+      <section class="colored">
+        <div class="row limited-width">
+          <div class="description">
+            <h3>${i18next.t('pages.home.tryTitle')}</h3>
+            <p>${i18next.t('pages.home.tryDescription')}</p>
+            <div class="actions">
+              <a
+                  href="https://mybinder.org/v2/gh/QISKit/qiskit-tutorial/master?filepath=index.ipynb"
+                  target="_blank"
+                  rel="noopener"
+                  tabindex="-1">
+                <vaadin-button theme="secondary">${i18next.t('pages.home.tryButton')}</vaadin-button>
+              </a>
+            </div>
+          </div>
+          <div class="illustration">
+            <img src="images/try-and-learn.jpg" .alt=${i18next.t('pages.home.ibmQExperienceAltImage')}>
+          </div>
+        </div>
+      </section>
 
-<section class="community">
-  <div class="row limited-width">
-    <div class="description">
-      <h3>${i18next.t('pages.home.communityTitle')}</h3>
-      <p>${i18next.t('pages.home.communityDescription')}</p>
-    </div>
-    <div class="illustration">
-      <ul class="social-networks-list">
-        <li>
-          <a href="https://qiskit.slack.com" title="Slack community" target="_blank" rel="noopener">
-            <div class="social-network">${slackIcon} <span class="name">Slack</span></div>
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/Qiskit" title="GitHub organization" target="_blank"
-             rel="noopener">
-            <div class="social-network">${githubIcon} <span class="name">GitHub</span></div>
-          </a>
-        </li>
-        <li>
-          <a href="https://quantumcomputing.stackexchange.com/search?q=qiskit" title="Stack Exchange community"
-             target="_blank" rel="noopener">
-            <div class="social-network">${stackexchangeIcon} <span class="name">Stack
-                Exchange</span></div>
-          </a>
-        </li>
-        <li>
-          <a href="https://twitter.com/Qiskit" title="Twitter profile" target="_blank"
-             rel="noopener">
-            <div class="social-network">${twitterIcon} <span class="name">Twitter</span></div>
-          </a>
-        </li>
-        <li>
-          <a href="https://medium.com/Qiskit" title="Medium profile" target="_blank" rel="noopener">
-            <div class="social-network">${mediumIcon} <span class="name">Medium</span></div>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.youtube.com/Qiskit" title="YouTube channel" target="_blank"
-             rel="noopener">
-            <div class="social-network">${youtubeIcon} <span class="name">YouTube</span></div>
-          </a>
-        </li>
-        <li>
-          <a href="https://www.facebook.com/Qiskit" title="Facebook page" target="_blank"
-             rel="noopener">
-            <div class="social-network">${facebookIcon} <span class="name">Facebook</span></div>
-          </a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</section>
+      <section class="community">
+        <div class="row limited-width">
+          <div class="description">
+            <h3>${i18next.t('pages.home.communityTitle')}</h3>
+            <p>${i18next.t('pages.home.communityDescription')}</p>
+          </div>
+          <div class="illustration">
+            <ul class="social-networks-list">
+              <li>
+                <a
+                    href="https://qiskit.slack.com"
+                    title="Slack community"
+                    target="_blank"
+                    rel="noopener">
+                  <div class="social-network">${slackIcon} <span class="name">Slack</span></div>
+                </a>
+              </li>
+              <li>
+                <a
+                    href="https://github.com/Qiskit"
+                    title="GitHub organization"
+                    target="_blank"
+                    rel="noopener">
+                  <div class="social-network">${githubIcon} <span class="name">GitHub</span></div>
+                </a>
+              </li>
+              <li>
+                <a
+                    href="https://quantumcomputing.stackexchange.com/search?q=qiskit"
+                    title="Stack Exchange community"
+                    target="_blank"
+                    rel="noopener">
+                  <div class="social-network">${stackexchangeIcon} <span class="name">Stack Exchange</span></div>
+                </a>
+              </li>
+              <li>
+                <a
+                    href="https://twitter.com/Qiskit"
+                    title="Twitter profile"
+                    target="_blank"
+                    rel="noopener">
+                  <div class="social-network">${twitterIcon} <span class="name">Twitter</span></div>
+                </a>
+              </li>
+              <li>
+                <a
+                    href="https://medium.com/Qiskit"
+                    title="Medium profile"
+                    target="_blank"
+                    rel="noopener">
+                  <div class="social-network">${mediumIcon} <span class="name">Medium</span></div>
+                </a>
+              </li>
+              <li>
+                <a
+                    href="https://www.youtube.com/Qiskit"
+                    title="YouTube channel"
+                    target="_blank"
+                    rel="noopener">
+                  <div class="social-network">${youtubeIcon} <span class="name">YouTube</span></div>
+                </a>
+              </li>
+              <li>
+                <a
+                    href="https://www.facebook.com/Qiskit"
+                    title="Facebook page"
+                    target="_blank"
+                    rel="noopener">
+                  <div class="social-network">${facebookIcon} <span class="name">Facebook</span></div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>
 
-<section class="colored">
-  <div class="row limited-width">
-    <div class="description">
-      <h3>${i18next.t('pages.home.ibmQExperienceTitle')}</h3>
-      <p>${i18next.t('pages.home.ibmQExperienceDescription')}</p>
-      <div class="actions">
-        <a href="https://quantumexperience.ng.bluemix.net/qx/editor" title="IBM Q Experience"
-           target="_blank" rel="noopener" tabindex="-1">
-          <vaadin-button theme="secondary">${i18next.t('pages.home.ibmQExperienceButton')}</vaadin-button>
-        </a>
-      </div>
-    </div>
-    <div class="illustration">
-      <img src="images/ibm-q-experience.jpg" .alt=${i18next.t('pages.home.ibmQExperienceAltImage')}>
-    </div>
-  </div>
-</section>
+      <section class="colored">
+        <div class="row limited-width">
+          <div class="description">
+            <h3>${i18next.t('pages.home.ibmQExperienceTitle')}</h3>
+            <p>${i18next.t('pages.home.ibmQExperienceDescription')}</p>
+            <div class="actions">
+              <a
+                  href="https://quantumexperience.ng.bluemix.net/qx/editor"
+                  title="IBM Q Experience"
+                  target="_blank"
+                  rel="noopener"
+                  tabindex="-1">
+                <vaadin-button theme="secondary">${i18next.t('pages.home.ibmQExperienceButton')}</vaadin-button>
+              </a>
+            </div>
+          </div>
+          <div class="illustration">
+            <img src="images/ibm-q-experience.jpg" .alt=${i18next.t('pages.home.ibmQExperienceAltImage')}>
+          </div>
+        </div>
+      </section>
 
-<section class="citation">
-  <div class="row limited-width">
-    <div class="description">
-      <h3>${i18next.t('pages.home.citationTitle')}</h3>
-      <p>
-        ${i18next.t('pages.home.citationDescription')}
-        <a href="https://raw.githubusercontent.com/Qiskit/qiskit/master/Qiskit.bib" title="BibTeX"
-           target="_blank" rel="noopener">
-           ${i18next.t('pages.home.bibtexLink')}
-        </a>
-      </p>
-    </div>
-  </div>
-</section>
-
-<section class="supporting">
-  <div class="limited-width">
-    <div class="description">
-      <h3>${i18next.t('pages.home.supportingTitle')}</h3>
-      <p>${i18next.t('pages.home.supportingDescription')}</p>
-    </div>
-    ${supportersListTemplate}
-  </div>
-</section>
-`;
+      <section class="supporting">
+        <div class="limited-width">
+          <div class="description">
+            <h3>${i18next.t('pages.home.supportingTitle')}</h3>
+            <p>${i18next.t('pages.home.supportingDescription')}</p>
+          </div>
+          ${supportersListTemplate}
+        </div>
+      </section>
+    `;
   }
 
   constructor() {
