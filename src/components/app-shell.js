e14d6b5746297ab5089093629ff2c118de7d0bf3
Carlos Azaustre <cazaustre@gmail.com>
2019-02-22 10:13:39 +0100

    Remove tutorial links on Elements pages (#91)
    * add tutorials link on navbar
    
    * removed tutorial link on element pages
    
    * add target blank on tutorial link

--- src/components/app-shell.js
+++ src/components/app-shell.js
@@ -163,6 +163,10 @@ class AppShell extends localize(i18next)(connect(store)(LitElement)) {
             <a href="/aqua" ?selected=${this.page === 'aqua'}>Aqua</a>
           </nav>
           <nav class="second">
+            <a rel="noopener" target='_blank'
+              href="https://nbviewer.jupyter.org/github/Qiskit/qiskit-tutorial/blob/master/index.ipynb">
+              ${i18next.t('tutorials')}
+            </a>
             <a href="/documentation/">${i18next.t('documentation')}</a>
             <a href="/vscode" ?selected=${this.page === 'vscode'}>${i18next.t('tools')}</a>
             <a href="/fun" ?selected=${this.page === 'fun'}>${i18next.t('fun')}</a>
--- src/components/page-aer.js
+++ src/components/page-aer.js
@@ -70,13 +70,6 @@ class PageAer extends localize(i18next)(LitElement) {
                 tabindex="-1">
               <vaadin-button theme="secondary small">${githubIcon} GitHub</vaadin-button>
             </a>
-            <a
-                href="https://nbviewer.jupyter.org/github/Qiskit/qiskit-tutorial/blob/master/index.ipynb#1.3-Qiskit-Aer"
-                target="_blank"
-                rel="noopener"
-                tabindex="-1">
-              <vaadin-button theme="secondary small">${i18next.t('tutorials')}</vaadin-button>
-            </a>
           </div>
         </div>
       </header>
