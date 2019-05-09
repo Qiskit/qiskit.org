0efb91bfe4678ae9d648e8e4a15c70416dd52ba2
Carlos Azaustre <cazaustre@gmail.com>
2019-05-09 15:27:10 +0200

    update to trigger travis

--- src/components/page-home.js
+++ src/components/page-home.js
@@ -1,7 +1,7 @@
 /**
  * @license
  *
- * Copyright (c) 2018, IBM.
+ * Copyright (c) 2019, IBM.
  *
  * This source code is licensed under the Apache License, Version 2.0 found in
  * the LICENSE.txt file in the root directory of this source tree.
