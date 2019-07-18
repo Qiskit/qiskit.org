6bff41a3c9107d660a5475426cc676ee9d4a8315
Carlos Azaustre <carlos.azaustre@ibm.com>
2019-07-18 17:17:53 +0200

    add google site verification file

--- gulpfile.js
+++ gulpfile.js
@@ -75,6 +75,9 @@ task('add-ibm-stats-script', () => {
  * Copy static files.
  */
 task('copy-license', () => src('license/**').pipe(dest(`${buildPath}license`)));
+task('copy-google-site-verif', () =>
+  src('googlefd7c7bd12b94442f.html').pipe(dest('googlefd7c7bd12b94442f.html')),
+);
 task('copy-documentation', () =>
   src('documentation/**').pipe(dest(`${buildPath}documentation`)),
 );
@@ -82,7 +85,13 @@ task('copy-modelq', () => src('modelq/**').pipe(dest(`${buildPath}modelq`)));
 task('copy-robots', () => src('robots.txt').pipe(dest(`${buildPath}`)));
 task(
   'copy-static-files',
-  parallel('copy-license', 'copy-documentation', 'copy-modelq', 'copy-robots'),
+  parallel(
+    'copy-license',
+    'copy-google-site-verif',
+    'copy-documentation',
+    'copy-modelq',
+    'copy-robots',
+  ),
 );
 
 /**
