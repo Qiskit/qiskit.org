ed562a4ee8618d51c8ff11c2b9b12b1cf09dc519
Salvador de la Puente González <salva@unoyunodiez.com>
2019-08-08 12:33:42 +0200

    Add education tab

--- src/components/app-shell.js
+++ src/components/app-shell.js
@@ -240,6 +240,7 @@ class AppShell extends localize(i18next)(connect(store)(LitElement)) {
             >
               ${i18next.t('tutorials')}
             </a>
+            <a href="/education">${i18next.t('education')}</a>
             <a href="/documentation">${i18next.t('documentation')}</a>
             <a href="/vscode" ?selected=${this.page === 'vscode'}>${i18next.t('tools')}</a>
           </nav>
