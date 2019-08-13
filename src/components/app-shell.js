682f8e737bb06c6cdaed971124732094407ddb07
Paul Nation <nonhermitian@gmail.com>
2019-08-13 00:01:39 -0400

    fix tutorials link (#183)

--- src/components/app-shell.js
+++ src/components/app-shell.js
@@ -230,7 +230,7 @@ class AppShell extends localize(i18next)(connect(store)(LitElement)) {
           <nav class="second">
             <a href="/education">${i18next.t('education')}</a>
             <a
-              href="https://nbviewer.jupyter.org/github/Qiskit/qiskit-tutorials/blob/master/qiskit/start_here.ipynb"
+              href="https://nbviewer.jupyter.org/github/Qiskit/qiskit-tutorials/blob/master/qiskit/1_start_here.ipynb"
               rel="noopener"
               target="_blank"
               @click=${() => trackClickEvent({
