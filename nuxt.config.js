3de2ab4b3b17e95dc025352605da85e24d56940f
Salvador de la Puente González <salva@unoyunodiez.com>
2019-08-13 17:28:30 +0200

    Setup Segment for the Community site.

--- nuxt.config.js
+++ nuxt.config.js
@@ -29,6 +29,35 @@ export default {
   */
   head: {
     title: pkg.name,
+    script: [
+      {
+        hid: 'segment',
+        innerHTML: `
+(function () {
+  'use strict'
+  window.digitalData = {
+    page: {
+      pageInfo: {
+        productTitle: 'IBM Q Experience',
+        analytics: {
+          category: 'Qiskit.org'
+        }
+      }
+    }
+  }
+  window._analytics = {
+    segment_key: 'ffdYLviQze3kzomaINXNk6NwpY9LlXcw',
+    coremetrics: false,
+    optimizely: false
+  }
+}());
+`
+      },
+      {
+        src: 'https://cloud.ibm.com/analytics/build/bluemix-analytics.min.js',
+        async: true
+      }
+    ],
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -36,7 +65,10 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    ],
+    __dangerouslyDisableSanitizersByTagID: {
+      'segment': ['innerHTML']
+    }
   },
 
   /*
