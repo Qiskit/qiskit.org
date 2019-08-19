04d6ab4756992a61aa33a4924dc34fc597f3f347
Jay Gambetta <jay.gambetta@us.ibm.com>
2019-08-18 22:16:24 -0400

    Update page-terra.js

--- src/components/page-terra.js
+++ src/components/page-terra.js
@@ -89,7 +89,7 @@ class PageTerra extends localize(i18next)(LitElement) {
             <h3>${i18next.t('pages.terra.stackTitle')}</h3>
             <div class="stack-list">
               <div class="element">
-                <div class="title">User Inputs</div>
+                <div class="title">User Inputs (Circuits, and Scheules)</div>
                 <div class="subtitle">Quantum Circuit, Pulse Schedule</div>
               </div>
               <div class="element">
@@ -101,8 +101,8 @@ class PageTerra extends localize(i18next)(LitElement) {
                 <div class="subtitle">Qiskit Aer, IBM Q, Third Party</div>
               </div>
               <div class="element dot">
-                <div class="title">Visualization</div>
-                <div class="subtitle">Histogram, State, Unitary</div>
+                <div class="title">Visualization and Quantum Information Tools</div>
+                <div class="subtitle">Histogram, State, Unitary, Entanglement, ...</div>
               </div>
             </div>
           </div>
