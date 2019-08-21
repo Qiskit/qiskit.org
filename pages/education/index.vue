f9f7b9081af7974ad5467ba92efcd682f4005580
Salvador de la Puente González <salva@unoyunodiez.com>
2019-08-21 17:17:21 +0200

    Update education header video (#53)

--- pages/education/index.vue
+++ pages/education/index.vue
@@ -6,7 +6,7 @@
           <iframe
             width="560"
             height="315"
-            src="https://www.youtube.com/embed/_EweJlth2Sk"
+            src="https://www.youtube.com/embed/NHTDqdGfzcc"
             frameborder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
