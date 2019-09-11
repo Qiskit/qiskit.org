e1e16483554808051bd9adcd7343d0711dc108da
Salvador de la Puente González <salva@unoyunodiez.com>
2019-09-11 08:30:37 +0200

    Host the test on GitHub

--- pages/advocates/index.vue
+++ pages/advocates/index.vue
@@ -48,13 +48,13 @@
       <PageSection id="become-an-advocate">
         <h2>Become an Advocate</h2>
         <ol>
-          <li>Click on the "Apply now" button below.</li>
-          <li>Complete the test in the application with at least a 70%.</li>
+          <li>Download the test by clicking on the "Apply now" button below.</li>
+          <li>Decompress and complete the test in the application with at least a 70%.</li>
           <li>Provide at least 3 contributions to the Qiskit community and complete application by <strong>October 9, 2019</strong>.</li>
         </ol>
         <ul class="actions">
           <li>
-            <Cta to="http://ibm.biz/qiskit-advocate">
+            <Cta to="/files/qiskit_advocates_questions.zip">
               Apply now
             </Cta>
           </li>
