d3862a3f25d23e01f3f071d4cb0fef07cb426d0b
Matthew Treinish <mtreinish@kortar.org>
2019-10-18 17:17:22 -0400

    Fix CONFIG_PATH

--- deploy/deploy_cos.sh
+++ deploy/deploy_cos.sh
@@ -4,7 +4,7 @@
 curl https://downloads.rclone.org/rclone-current-linux-amd64.deb -o rclone.deb
 sudo apt-get install -y ./rclone.deb
 
-CONFIG_PATH=$(rclone config file)
+CONFIG_PATH=$(rclone config file | tail -1)
 
 # Decrypt config
 openssl aes-256-cbc -K $encrypted_rclone_key -iv $encrypted_rclone_iv -in rclone.conf.enc -out $CONFIG_PATH -d
