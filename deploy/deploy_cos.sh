e7f96731cc52c21859261ff1f3edda45aab7c188
Matthew Treinish <mtreinish@kortar.org>
2019-10-18 15:26:14 -0400

    Install rclone from deb

--- deploy/deploy_cos.sh
+++ deploy/deploy_cos.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 CONFIG_PATH=$(rclone config file)
 
+
+#install rclone
+curl https://downloads.rclone.org/rclone-current-linux-amd64.deb -o rclone.deb
+sudo apt-get install -y ./rclone.deb
+
 # Decrypt config
 openssl aes-256-cbc -K $encrypted_rclone_key -iv $encrypted_rclone_iv -in rclone.conf.enc -out $CONFIG_PATH -d
 
