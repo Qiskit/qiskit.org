c8ed85e587f736fdf91d1731fa6e7fe36eedc4d9
Matthew Treinish <mtreinish@kortar.org>
2019-10-18 15:40:55 -0400

    Fix ordering problem in deploy script

--- deploy/deploy_cos.sh
+++ deploy/deploy_cos.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-CONFIG_PATH=$(rclone config file)
-
 
 #install rclone
 curl https://downloads.rclone.org/rclone-current-linux-amd64.deb -o rclone.deb
 sudo apt-get install -y ./rclone.deb
+
+CONFIG_PATH=$(rclone config file)
 
 # Decrypt config
 openssl aes-256-cbc -K $encrypted_rclone_key -iv $encrypted_rclone_iv -in rclone.conf.enc -out $CONFIG_PATH -d
