c6c8b0210df158f2890805b5d797fb5bbfbb19ba
Matthew Treinish <mtreinish@kortar.org>
2019-10-18 17:59:27 -0400

    Statically build and upload static assets

--- deploy/deploy_cos.sh
+++ deploy/deploy_cos.sh
@@ -9,4 +9,11 @@ CONFIG_PATH=$(rclone config file | tail -1)
 # Decrypt config
 openssl aes-256-cbc -K $encrypted_rclone_key -iv $encrypted_rclone_iv -in rclone.conf.enc -out $CONFIG_PATH -d
 
-rclone sync ../build IBMCOS:qiskit-web-test
+cp -r ../documentation ../build/default/.
+cp -r ../license ../build/default/.
+cp -r ../googlefd7c7bd12b94442f.html ../build/default/.
+cp -r ../education/. ../build/default/.
+cp -r ../modelq ../build/default/.
+cp -r ../robots.txt ../build/default/.
+
+rclone sync ../build/default IBMCOS:qiskit-web-test
