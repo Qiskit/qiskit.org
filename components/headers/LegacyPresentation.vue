50ef8b25504277cfaa9310b49c8a889bb518b4fb
Salvador de la Puente González <salva@unoyunodiez.com>
2019-10-20 23:18:36 +0200

    Finish Qiskit Terra first draft migration

--- components/headers/LegacyPresentation.vue
+++ components/headers/LegacyPresentation.vue
@@ -1,20 +1,31 @@
 <template>
   <header class="legacy-presentation">
-    <img
-      class="legacy-presentation__image"
-      :src="image"
-      :alt="`${title} logo`"
-    >
     <div>
-      <h1 class="legacy-presentation__title">
-        {{ title }}
-      </h1>
-      <p class="legacy-presentation__description">
-        {{ description }}
-      </p>
-      <Cta :to="to">
-        Get Started!
-      </Cta>
+      <img
+        class="legacy-presentation__image"
+        :src="image"
+        :alt="`${title} logo`"
+      >
+      <div class="legacy-presentation__info">
+        <h1 class="legacy-presentation__title">
+          {{ title }}
+        </h1>
+        <p class="legacy-presentation__description">
+          {{ description }}
+        </p>
+        <Cta v-if="to" :to="to">
+          Get Started!
+        </Cta>
+        <a
+          v-if="sources"
+          class="github-cta"
+          rel="nofollow noopener"
+          :href="sources"
+          target="_blank"
+        >
+          <div class="social-network"><svg viewBox="0 0 24 24" width="24" height="24" aria-labelledby="github-icon" role="img"><title id="github-icon">GitHub icon</title><path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"></path></svg> <span class="name">GitHub</span></div>
+        </a>
+      </div>
     </div>
   </header>
 </template>
@@ -31,7 +42,8 @@ export default class extends Vue {
   @Prop(String) image
   @Prop(String) title
   @Prop(String) description
-  @Prop(String) to
+  @Prop({ type: String, required: false }) to
+  @Prop({ type: String, required: false }) sources
 }
 </script>
 
@@ -39,25 +51,35 @@ export default class extends Vue {
 @import '~/assets/scss/mixins.scss';
 
 .legacy-presentation {
-  @include framed();
-
-  display: flex;
-  justify-content: center;
-  text-align: left;
-  color: white;
+  color: var(--legacy-presentation-text-color, white);
   background-color: var(--legacy-presentation-color, rgb(36, 42, 46));
   min-height: 340px;
+  display: flex;
   align-items: center;
+
+  & > div {
+    @include framed();
+
+    display: flex;
+    justify-content: center;
+    text-align: left;
+    align-items: center;
+    width: 100%;
+  }
+
+  &__info {
+    flex: 1;
+  }
 
   &__title {
     font-size: 2rem;
-    font-weight: 300;
+    font-weight: 400;
     font-family: 'IBM Plex Mono', 'Menlo', 'DejaVu Sans Mono', 'Bitstream Vera Sans Mono', Courier, monospace;
   }
 
   &__description {
     font-size: 1.2rem;
-    font-weight: 300;
+    font-weight: 400;
     margin: 1.1rem 0;
   }
 
@@ -65,6 +87,37 @@ export default class extends Vue {
     width: 200px;
     height: 200px;
     margin-right: 3em;
+  }
+}
+
+.github-cta {
+  font-size: 0.7rem;
+  display: inline-block;
+  padding: 0.4rem 0.5rem;
+  border: 2px black solid;
+  color: black;
+  text-decoration: none;
+  font-weight: 400;
+  transition: all linear 200ms;
+
+  &:hover {
+    background-color: black;
+    color: white;
+    fill: white;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+
+  .name {
+    padding-left: 0.4rem;
+  }
+
+  svg {
+    width: 0.8rem;
+    height: 0.8rem;
   }
 }
 </style>
