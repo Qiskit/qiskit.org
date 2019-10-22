bffa439095fa8bd38ea1e81b3bee40f965816925
Matthew Treinish <mtreinish@kortar.org>
2019-10-22 08:56:59 -0400

    Pivot config for production website bucket
    This commit updates the config and deploy script to upload the built
    website content to the bucket for the production website.

--- deploy/deploy_cos.sh
+++ deploy/deploy_cos.sh
@@ -9,14 +9,12 @@ CONFIG_PATH=$(rclone config file | tail -1)
 # Decrypt config
 openssl aes-256-cbc -K $encrypted_rclone_key -iv $encrypted_rclone_iv -in rclone.conf.enc -out $CONFIG_PATH -d
 
-mv ../build/es5-bundled ../build/default
+cp -r ../documentation ../build/es5-bundled/.
+cp -r ../license ../build/es5-bundled/.
+cp -r ../googlefd7c7bd12b94442f.html ../build/es5-bundled/.
+cp -r ../education ../build/es5-bundled/.
+cp -r ../modelq ../build/es5-bundled/.
+cp -r ../robots.txt ../build/es5-bundled/.
+cp -r ../404.html ../build/es5-bundled/.
 
-cp -r ../documentation ../build/default/.
-cp -r ../license ../build/default/.
-cp -r ../googlefd7c7bd12b94442f.html ../build/default/.
-cp -r ../education ../build/default/.
-cp -r ../modelq ../build/default/.
-cp -r ../robots.txt ../build/default/.
-cp -r ../404.html ../build/default/.
-
-rclone sync ../build/default IBMCOS:qiskit-web-test
+rclone sync ../build/es5-bundled IBMCOS:qiskit-org-website
