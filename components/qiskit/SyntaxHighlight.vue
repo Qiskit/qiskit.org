dce2ec0b2b7e517cb501ecee0a00f8b3e351cc09
Salvador de la Puente González <salva@unoyunodiez.com>
2019-11-02 10:33:07 +0100

    Add copy to clipboard feature to syntax highlihgt

--- components/qiskit/SyntaxHighlight.vue
+++ components/qiskit/SyntaxHighlight.vue
@@ -1,7 +1,13 @@
 <template>
-  <pre v-highlightjs>
-    <code :class="lang"><slot /></code>
-  </pre>
+  <div class="syntax-highlight">
+    <button
+      title="Copy to clipboard"
+      @click="copyToClipboard"
+    >
+      Copy
+    </button>
+    <pre v-highlightjs><code ref="code" :class="lang"><slot /></code></pre>
+  </div>
 </template>
 
 <script lang="ts">
@@ -10,12 +16,31 @@ import { Component, Prop } from 'vue-property-decorator'
 
 @Component
 export default class extends Vue {
-  @Prop({ type: String, default: 'python' }) lang;
+  @Prop({ type: String, default: 'python' }) lang
+
+  copyToClipboard() {
+    const code = this.$refs.code as HTMLElement
+    navigator.clipboard.writeText(code.innerText)
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-code {
-  font-size: 0.7rem;
+.syntax-highlight {
+  position: relative;
+
+  button {
+    position: absolute;
+    top: 0; right: 0;
+    text-transform: uppercase;
+    border: none;
+    padding: 0.2rem 0.5rem;
+    cursor: pointer;
+  }
+
+  code {
+    padding: 1.5rem 1rem;
+    font-size: 0.7rem;
+  }
 }
 </style>
