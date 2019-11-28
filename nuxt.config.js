12fadc042414e9813be32a3b1689d706264a8709
y4izus <yaiza.garcia.mm@gmail.com>
2019-11-28 13:19:26 +0100

    Divides common segment config from dinamical config

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,29 +35,16 @@ export default {
       {
         hid: 'segment',
         innerHTML: `
-(function () {
-  'use strict'
-  window.digitalData = {
-    page: {
-      pageInfo: {
-        productTitle: 'IBM Q Experience',
-        analytics: {
-          category: 'Qiskit.org'
-        }
-      }
-    }
-  }
-  window._analytics = {
-    segment_key: 'ffdYLviQze3kzomaINXNk6NwpY9LlXcw',
-    coremetrics: false,
-    optimizely: false
-  }
-}());
-`
-      },
-      {
-        src: 'https://cloud.ibm.com/analytics/build/bluemix-analytics.min.js',
-        async: true
+          window.digitalData = {
+            page: {
+              pageInfo: {
+                productTitle: 'IBM Q Experience',
+                analytics: {
+                  category: 'Qiskit.org'
+                }
+              }
+            }
+          }`
       }
     ],
     meta: [
