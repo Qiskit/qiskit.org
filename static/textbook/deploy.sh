090d63efddcb68593fc2842c429a55d461ecb7e7
Travis CI User <travis@example.org>
2019-12-02 18:29:23 +0000

    Updating textbook to Qiskit/qiskit-textbook@8e947ba0f97c9b015a370ad93d51b636f17eece9

---
+++ static/textbook/deploy.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+user=delapuente
+org=qiskit-community
+repo=community.qiskit.org
+
+download_repo () {
+  root=$1
+  mkdir -p $root
+  pushd $root || return
+  git clone --depth 1 "https://${user}:${GITHUB_TOKEN}@github.com/${org}/${repo}.git"
+  popd || return
+}
+
+push_repo () {
+  root=$1
+  pushd $root/$repo || return
+  git add static/textbook
+  git commit -am"Updating textbook to ${TRAVIS_REPO_SLUG}@${TRAVIS_COMMIT}"
+  git push origin master
+  popd || return
+}
+
+main () {
+  download_repo _repo
+  mkdir -p _repo/$repo/static/textbook
+  rsync -r --delete _site/ _repo/$repo/static/textbook
+  push_repo _repo
+}
+
+main
