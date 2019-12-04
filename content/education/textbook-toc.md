d23dfb5a69654e90e37ac38f688a356537e379a2
Salvador de la Puente González <salva@unoyunodiez.com>
2019-12-04 18:22:33 +0100

    Update to the new textbook TOC format (#126)
    * Update to the new textbook TOC format

--- content/education/textbook-toc.md
+++ content/education/textbook-toc.md
@@ -1,1 +1,38 @@
 ## Table of Contents
+### Chapter 0. Prerequisites
+- Python and Jupyter Notebooks
+- Qiskit
+- Linear Algebra
+### Chapter 1. Quantum States and Qubits
+- Introduction
+- The Atoms of Computation
+- The Unique Properties of Qubits
+- Writing Down Qubit States
+- Pauli Matrices and the Bloch Sphere
+- States for Many Qubits
+### Chapter 2. Single Qubits and Multi-Qubits gates
+- Introduction
+- Quantum Gates
+- Fun with Matrices
+- The Standard Gate Set
+- Proving Universality
+- Basic Circuit Identities
+### Chapter 3. Quantum Algorithms
+- Quantum Teleportation
+- Deutsch-Josza Algorithm
+- Bernstein-Vazirani Algorithm
+- Simon's Algorithm
+- Quantum Fourier Transform
+- Quantum Phase Estimation
+- Grover's Algorithm
+### Chapter 4. Quantum Algorithms for Applications
+- Simulating Molecules using VQE
+- Solving Satisfiability Problems using Grover's Algorithm
+### Chapter 5. Investigating Quantum Hardware Using Qiskit
+- Calibrating Qubits with OpenPulse
+- Introduction to Quantum Error Correction using Repetition Codes
+- Measurement Error Mitigation
+- Randomized Benchmarking
+- Measuring Quantum Volume
+### Chapter 6. Implementations of Recent Quantum Algorithms
+- Variational Quantum Linear Solver
--- tests/hooks/textbook-toc-utils.spec.ts
+++ tests/hooks/textbook-toc-utils.spec.ts
@@ -1,18 +1,76 @@
 import { extractToc, formatTocLines, TocType } from '~/hooks/textbook-toc-utils'
 
-const sampleHTML = `<p><strong>Chapter 0. Title 0</strong><br /></p>
-<p>&nbsp; &nbsp; 0.0 <a href="./ch-prerequisites/python-and-jupyter-notebooks.html">Topic 0.0</a><br />
-&nbsp; &nbsp; 0.1 <a href="./ch-prerequisites/qiskit.html">Topic 0.1</a><br />
-<p><strong>Chapter 1. Title 1</strong><br /></p>
-<p>&nbsp; &nbsp; 1.0 <a href="./ch-states/introduction.html">Topic 1.0</a><br />
-&nbsp; &nbsp; 1.1 <a href="./ch-states/atoms-computation.html">Topic 1.1</a><br />`
+const sampleHTML = `<li class="c-sidebar__chapter">
+  <a class="c-sidebar__entry "
+    href=".html"
+  >
+
+      0.
+
+    Title 0
+  </a>
+  <ul class="c-sidebar__sections ">
+    <li class="c-sidebar__section">
+      <a class="c-sidebar__entry "
+        href="/textbook/ch-prerequisites/python-and-jupyter-notebooks.html"
+      >
+
+          0.0
+
+        Topic 0.0
+      </a>
+    </li>
+    <li class="c-sidebar__section">
+      <a class="c-sidebar__entry "
+        href="/textbook/ch-prerequisites/qiskit.html"
+      >
+
+          0.1
+
+        Topic 0.1
+      </a>
+    </li>
+  </ul>
+</li>
+<li class="c-sidebar__chapter">
+  <a class="c-sidebar__entry "
+    href=".html"
+  >
+
+      1.
+
+    Title 1
+  </a>
+  <ul class="c-sidebar__sections ">
+    <li class="c-sidebar__section">
+      <a class="c-sidebar__entry "
+        href="/textbook/ch-states/introduction.html"
+      >
+
+          1.0
+
+        Topic 1.0
+      </a>
+    </li>
+    <li class="c-sidebar__section">
+      <a class="c-sidebar__entry "
+        href="/textbook/ch-states/atoms-computation.html"
+      >
+
+          1.1
+
+        Topic 1.1
+      </a>
+    </li>
+  </ul>
+</li>`
 
 const expectedToc: TocType = [
-  ['Chapter 0. Title 0', [
+  ['0. Title 0', [
     '0.0 Topic 0.0',
     '0.1 Topic 0.1'
   ]],
-  ['Chapter 1. Title 1', [
+  ['1. Title 1', [
     '1.0 Topic 1.0',
     '1.1 Topic 1.1'
   ]]
