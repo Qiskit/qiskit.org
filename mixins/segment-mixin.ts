38ec6d9575359dfb7ced5a75747b5bb3ed8769c5
y4izus <yaiza.garcia.mm@gmail.com>
2019-12-20 15:59:34 +0100

    Removes values that are set by default

--- mixins/segment-mixin.ts
+++ mixins/segment-mixin.ts
@@ -6,21 +6,19 @@ declare global {
   }
 }
 
-const category: string = 'Qiskit.org'
-const productTitle: string = 'IBM Q Experience'
-const navigationType: string = 'pushState'
-
 /**
  * Sends the page event to segment. Needs the following information:
- * @param routeName identifies the visited page regardless of the URL 
- * changing over the time
- * @param path is the route of the visited page
- * @param title is the title of the visited page
+ * @param pageComponent has the information of the page.
  * 
- * category, productTitle and the navigationType are always the same,
- * that's why they are in contants 
+ * It's not needed to add the 'path' because it's done automatically
  */
-const trackPage = (routeName: string, path: string, title: string) => {
+const trackPage = (pageComponent: any) => {
+  const category: string = 'Qiskit.org'
+  const productTitle: string = 'IBM Q Experience'
+  const navigationType: string = 'pushState'
+  const routeName: string = pageComponent.routeName
+  const title: string = pageComponent.$metaInfo.title
+
   if (window.bluemixAnalytics && window.bluemixAnalytics.pageEvent) {
     window.bluemixAnalytics.pageEvent(
       category,
@@ -28,7 +26,6 @@ const trackPage = (routeName: string, path: string, title: string) => {
       {
         navigationType,
         productTitle,
-        path,
         title
       }
     )
@@ -40,51 +37,35 @@ const trackPage = (routeName: string, path: string, title: string) => {
  * - To include this js file in all pages:
  * https://cloud.ibm.com/analytics/build/bluemix-analytics.min.js
  * We do this on nuxt.config.js
- * - To include a title. We do this on the meta info on the header
- * - To add the path for each page
- * - To add the routeName for each page
+ * - To include the title of the page. We do this on the meta info on the header
+ * - To add the routeName for each page (on vue index component and in .md files)
+ *   The routheName identifies the visited page regardless of the URL changing 
+ *   over the time
  */
 export const segmentMixin = {
   created () {
     const pageComponent = this as any
-    const routeName: string = pageComponent.routeName
-    const title: string = pageComponent.$metaInfo.title
-    const path: string = pageComponent.to
 
     if (process.client) {
-      window.digitalData = {
-        page: {
-          pageInfo: {
-            productTitle,
-            analytics: { category }
-          }
-        }
-      }
-
+      /* To see the window._analytics default values go to
+      * https://github.ibm.com/Bluemix/Bluemix.Analytics/blob/master/webpack.constants.js
+      */
       window._analytics = {
         segment_key: 'zbHWEXPUfXm0K6C7HbegwB5ewDEC8o1H',
-        coremetrics: false,
         optimizely: false,
         googleAddServices: false,
-        fullStory: false,
-        autoPageEventSpa: false,
-        autoFormEvents: false,
         autoPageView: false
       }
 
       window.onload = () => {
-        trackPage (routeName, path, title)
+        trackPage (pageComponent)
       }
     }
 
   },
   beforeRouteEnter (to, from, next) {
     next((pageComponent) => {
-      const routeName: string = pageComponent.routeName
-      const title: string = pageComponent.$metaInfo.title
-      const path: string = to.path
-
-      trackPage (routeName, path, title)
+      trackPage (pageComponent)
     })
   }
 }
