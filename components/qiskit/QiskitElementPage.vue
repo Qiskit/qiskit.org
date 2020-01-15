716ce099ddde82490e084e194473aa3fc00530c1
Salvador de la Puente González <salva@unoyunodiez.com>
2020-01-15 08:56:28 +0100

    [Issue 359] Enable applying Component decorator to abstract classes. (#390)
    * Enable applying Component decorator to abstract classes.
    
    Abstract page components are convenient in this project to enforce all
    pages to add the `routeName` property for tracking purposes, and `title`
    and `description` for element-specific pages.
    
    However, the Component decorator that comes with Nuxt is not ready for
    decorating abstract classes. The `abstract` modifier is TypeScript
    specific and has no impact during runtime. An abstract class is the same
    as a regular class, from a JavaScript perspective. This PR augments the
    TypeScript definition of Component to allow abstract classes to be
    decorated.
    
    It also introduces the interface `TrackedPage` which is implemented by
    the `QiskitPage` page component so all the concrete descendants must
    implement the requirements imposed by `TrackedPage`. In this case, the
    existence of the `routeName` property.

--- components/qiskit/QiskitElementPage.vue
+++ components/qiskit/QiskitElementPage.vue
@@ -3,8 +3,8 @@ import { Component } from 'vue-property-decorator'
 import QiskitPage from '~/components/qiskit/QiskitPage.vue'
 
 @Component
-export default class extends QiskitPage {
-  title: string = ''
-  description: string = ''
+export default abstract class extends QiskitPage {
+  abstract title: string
+  abstract description: string
 }
 </script>
