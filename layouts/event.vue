663adbe2c3b374a864cf45891edaf040b3ac117e
Marina Aisa <marina@marinaaisa.com>
2020-02-19 16:56:58 +0100

    Start events index page

--- layouts/event.vue
+++ layouts/event.vue
@@ -1,3 +1,13 @@
 <template>
   <nuxt />
 </template>
+
+<style lang="scss">
+@import '~/assets/scss/theme.scss';
+@import '~/assets/scss/layout.scss';
+@import url(~/static/css/fonts.css);
+
+html {
+  background-color: var(--primary-color);
+}
+</style>
