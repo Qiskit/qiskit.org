5e9a966d0ebe5d3a3b15147f68ce7b282c3c9eb7
Salvador de la Puente González <salva@unoyunodiez.com>
2020-03-02 09:40:57 +0100

    Fix CSS import that blocked build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -68,7 +68,7 @@ const config: Configuration = {
   */
   plugins: [
     '~/plugins/router-hooks.ts',
-    '~/plugins/directives.ts',
+    '~/plugins/highlight-js.ts',
     '~/plugins/carbon.ts',
     '~/plugins/deep-load.ts',
     { src: '~/plugins/hotjar.ts', mode: 'client' },
