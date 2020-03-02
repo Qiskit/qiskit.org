b79e168a5a01179134ccd23a052d2b1a831db49a
y4izus <yaiza.garcia.mm@gmail.com>
2020-03-02 11:56:12 +0100

    Deletes icons-vue use from carbon.ts

--- plugins/carbon.ts
+++ plugins/carbon.ts
@@ -1,10 +1,5 @@
 import Vue from 'vue'
 import 'carbon-components/css/carbon-components.css'
 import CarbonComponentsVue from '@carbon/vue'
-import { Bee32 } from '@carbon/icons-vue'
 
-Vue.use(CarbonComponentsVue, {
-  components: {
-    Bee32
-  }
-})
+Vue.use(CarbonComponentsVue)
