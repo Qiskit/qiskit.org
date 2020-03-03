e08767b92ae79efa0c87c57365a799b6f5d2ae28
Yaiza <yaiza.garcia.mm@gmail.com>
2020-03-03 17:15:32 +0100

    Event card for desktop (#441)
    * Creates basic template for event card
    
    * Adding @carbon/icons-vue
    
    * Adds icons and colors
    
    * Uses semantic HTML and CSS
    
    * Adds resizing for svgs
    
    * Adds time attribute to date

---
+++ components/cards/EventCard.vue
@@ -0,0 +1,117 @@
+<template>
+  <nuxt-link
+    :to="to"
+    class="card-link"
+  >
+    <article class="event-card">
+      <div class="event-card__content">
+        <header>
+          <span class="event-card__subtitle">
+            {{ type }}
+          </span>
+          <h3 class="event-card__title">
+            {{ title }}
+          </h3>
+        </header>
+
+        <footer>
+          <div class="event-card__info-detail">
+            <Map20 class="event-card__icon" />
+            <span class="event-card__place">{{ place }}</span>
+          </div>
+          <div class="event-card__date-and-arrow">
+            <div class="event-card__info-detail">
+              <Calendar20 class="event-card__icon" />
+              <span class="event-card__date"><time>{{ date }}</time></span>
+            </div>
+            <ArrowRight20 class="event-card__icon event-card__icon--purple" />
+          </div>
+        </footer>
+      </div>
+
+      <div
+        class="event-card__media"
+        :style="`background-image: url(${image});`"
+      />
+    </article>
+  </nuxt-link>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {
+  @Prop(String) type
+  @Prop(String) title
+  @Prop(String) image
+  @Prop(String) place
+  @Prop(String) date
+  @Prop(String) to
+}
+</script>
+
+<style lang="scss" scoped>
+.card-link {
+  text-decoration: none;
+}
+
+.event-card {
+  height: 15.88rem;
+  width: 100%;
+  background-color: var(--gray-70);
+  color: var(--white);
+  display: flex;
+
+  &__content {
+    width: 50%;
+    margin: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+
+  &__title {
+    font-size: 1.5rem;
+    margin-top: 0.31rem;
+  }
+
+  &__subtitle, &__place, &__date {
+    font-size: 0.88rem;
+  }
+
+  &__place, &__date {
+    padding-left: 0.31rem;
+  }
+
+  &__info-detail {
+    margin-top: 0.63rem;
+  }
+
+  &__icon {
+    fill: currentColor;
+    position: relative;
+    top: .3em;
+    width: 1.25rem;
+    height: 1.25rem;
+
+    &--purple {
+      color: var(--purple-60)
+    }
+  }
+
+  &__date-and-arrow {
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+  }
+
+  &__media {
+    width: 50%;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+  }
+}
+</style>
--- plugins/carbon.ts
+++ plugins/carbon.ts
@@ -1,5 +1,16 @@
 import Vue from 'vue'
 import 'carbon-components/css/carbon-components.css'
 import CarbonComponentsVue from '@carbon/vue'
+import { CarbonIconsVue } from '@carbon/icons-vue'
+import Calendar20 from '@carbon/icons-vue/lib/calendar/20'
+import Map20 from '@carbon/icons-vue/lib/map/20'
+import ArrowRight20 from '@carbon/icons-vue/lib/arrow--right/20'
 
 Vue.use(CarbonComponentsVue)
+Vue.use(CarbonIconsVue, {
+  components: {
+    Calendar20,
+    Map20,
+    ArrowRight20
+  }
+})
