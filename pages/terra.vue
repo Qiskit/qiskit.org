fef42d5db0c8a4a9e2295a7a3fecb7f175a73dbd
y4izus <yaiza.garcia.mm@gmail.com>
2020-04-29 12:00:10 +0200

    Aligns the terra example code

--- pages/terra.vue
+++ pages/terra.vue
@@ -93,17 +93,17 @@ export default class extends QiskitElementPage {
 
   codeExample = `from qiskit import QuantumCircuit, Aer, execute
 
-  qc = QuantumCircuit(2, 2)
+qc = QuantumCircuit(2, 2)
 
-  qc.h(0)
-  qc.cx(0, 1)
-  qc.measure([0, 1], [0, 1])
+qc.h(0)
+qc.cx(0, 1)
+qc.measure([0, 1], [0, 1])
 
-  backend = Aer.get_backend('qasm_simulator')
-  job_sim = execute(qc, backend)
-  sim_result = job_sim.result()
+backend = Aer.get_backend('qasm_simulator')
+job_sim = execute(qc, backend)
+sim_result = job_sim.result()
 
-  print(sim_result.get_counts(qc))`
+print(sim_result.get_counts(qc))`
 }
 </script>
 
