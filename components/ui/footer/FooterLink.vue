4b2a8a7e8f118506f731b2a55d6a1eb917803453
y4izus <yaiza.garcia.mm@gmail.com>
2020-05-06 16:28:36 +0200

    Removes style dependency from FooterLink component

--- components/ui/footer/FooterLink.vue
+++ components/ui/footer/FooterLink.vue
@@ -1,11 +1,11 @@
 <template>
   <component
     :is="!isStatic && isInternal(url) ? 'nuxt-link' : 'a'"
-    class="footer-link"
     :href="url"
     :to="!isStatic && isInternal(url) ? url : null"
     :rel="isExternal(url) ? 'noopener' : null"
     :target="isExternal(url) ? '_blank' : null"
+    @click="segment && $trackClickEvent(segment)"
   >
     {{ label }}
   </component>
@@ -19,23 +19,7 @@ import MenuLinkMixin from '~/mixins/menuLink'
 export default class extends Mixins(MenuLinkMixin) {
   @Prop(String) url
   @Prop(String) label
+  @Prop(Object) segment
   @Prop({ type: Boolean, default: false }) isStatic
 }
 </script>
-
-<style lang="scss" scoped>
-@import '~carbon-components/scss/globals/scss/typography';
-
-.footer-link {
-  @include type-style('productive-heading-02');
-  color: inherit;
-  text-decoration: none;
-  display: inline-block;
-  width: 100%;
-  margin-top: .5rem;
-
-  &:hover {
-    color: $text-01;
-  }
-}
-</style>
--- components/ui/footer/FooterSubsection.vue
+++ components/ui/footer/FooterSubsection.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
-    <h2 class="footer-title">
+  <div class="footer-subsection">
+    <h2 class="footer-subsection__title">
       {{ title }}
     </h2>
     <FooterLink
       v-for="element in elements"
       :key="element.label"
+      class="footer-subsection__link"
       v-bind="element"
     />
   </div>
@@ -28,11 +29,26 @@ export default class extends Vue {
 <style lang="scss" scoped>
 @import '~carbon-components/scss/globals/scss/typography';
 
-.footer-title {
-  @include type-style('productive-heading-03');
-  margin-top: 2rem;
-  margin-bottom: 1.5rem;
-  padding-bottom: 0.5rem;
-  border-bottom: 1px solid $ui-03;
+.footer-subsection {
+  &__title {
+    @include type-style('productive-heading-03');
+    margin-top: 2rem;
+    margin-bottom: 1.5rem;
+    padding-bottom: 0.5rem;
+    border-bottom: 1px solid $ui-03;
+  }
+
+  &__link {
+    @include type-style('productive-heading-02');
+    color: inherit;
+    text-decoration: none;
+    display: inline-block;
+    width: 100%;
+    margin-top: .5rem;
+
+    &:hover {
+      color: $text-01;
+    }
+  }
 }
 </style>
