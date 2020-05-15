e0bd3f6c4444d6045ea57fabd68edc6ed36a5e83
Salvador de la Puente González <salva@unoyunodiez.com>
2020-05-15 22:20:08 +0200

    Add TheHeroMoment component (#590)
    * Add Carbon Colors to the project
    
    * Use SCSS colour functions to add transparency
    
    * Add TheHeroMoment component with the layout
    
    * Add typefaces for each subsection
    
    * Add LandingCta and VersionInfo components
    
    * Add grid and illustration. Include a responsive design.
    
    * Change links for pointing to GitHub and release notes maximizing the area pointing to GitHub
    
    * Adjust spacing and layout scales for smaller screens.
    
    * Make the grid fit the scale of Figma but following Carbon size.
    
    * Follow accessibility recommendations and express media query breakpoints in em units
    
    * Add hero illustration and fix positioning

---
+++ components/landing/TheHeroMoment.vue
@@ -0,0 +1,105 @@
+<template>
+  <section class="the-hero-moment">
+    <article class="the-hero-moment__content">
+      <VersionInfo class="the-hero-moment__version-info" />
+      <h1 class="the-hero-moment__title">
+        Open-Source Quantum Development
+      </h1>
+      <p class="the-hero-moment__description">
+        Qiskit [kiss-kit] is an open source SDK for working with quantum
+        computers at the level of pulses, circuits and algorithms.
+      </p>
+      <LandingCta
+        class="the-hero-moment__cta"
+        href="https://qiskit.org/documentation/install.html"
+      >
+        Get started
+      </LandingCta>
+    </article>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import VersionInfo from './VersionInfo.vue'
+import LandingCta from './LandingCta.vue'
+
+@Component({
+  components: { VersionInfo, LandingCta }
+})
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '~carbon-components/scss/globals/scss/typography';
+
+.the-hero-moment {
+  position: relative;
+  padding: 0 $spacing-05;
+
+  &__content {
+    position: relative;
+    margin: 0;
+    padding: 0;
+    max-width: $medium / 2;
+    display: flex;
+    flex-direction: column;
+
+    @include mq($from: medium) {
+      width: 50%;
+      max-width: unset;
+    }
+  }
+
+  &__version-info {
+    margin: 0 0 $spacing-09;
+    padding: 0;
+
+    @include mq($until: large) {
+      margin: 0 0 $spacing-08;
+    }
+
+    @include mq($until: medium) {
+      margin: 0 0 $spacing-07;
+    }
+  }
+
+  &__title {
+    @include type-style('display-01', true);
+    color: $white-text-01;
+    margin: 0 0 $spacing-09;
+    padding: 0;
+    width: 100%;
+
+    @include mq($from: medium, $until: large) {
+      margin: 0 0 $spacing-08;
+    }
+
+    @include mq($until: medium) {
+      margin: 0 0 $spacing-07;
+    }
+  }
+
+  &__description {
+    @include type-style('body-short-02');
+    color: $white-text-01;
+    margin: 0 0 $layout-07;
+    padding: 0 25% 0 0;
+
+    @include mq($from: medium, $until: large) {
+      @include type-style('body-short-01');
+      margin: 0 0 $layout-06;
+    }
+
+    @include mq($until: medium) {
+      @include type-style('body-short-01');
+      margin: 0 0 $layout-05;
+    }
+  }
+
+  &__cta {
+    @include type-style('body-short-01');
+  }
+}
+</style>
