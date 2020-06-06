af954cf62e014f7fa274d43ba6785741aa81a309
Salvador de la Puente González <salva@unoyunodiez.com>
2020-06-06 08:40:10 +0200

    Acknowledge our support to Black lives matters movement

---
+++ components/layouts/TheBanner.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="banner">
+    <div class="banner__layout">
+      <slot />
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '~carbon-components/scss/globals/scss/typography';
+
+.banner {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 4rem;
+  background-color: black;
+
+  &__layout {
+    @include framed();
+  }
+}
+</style>
