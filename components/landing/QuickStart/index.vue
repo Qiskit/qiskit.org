64e08cbacc9b71d5c941fe3f79f667027b25aace
Yaiza <yaiza.garcia.mm@gmail.com>
2020-06-24 16:41:33 +0200

    Quick install UI desktop (#684)
    Implement the widget with install choices and the section redirecting to Quantum Experience.

---
+++ components/landing/QuickStart/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <article class="quick-start">
+    <h2 class="quick-start__header">
+      Quick Start
+    </h2>
+    <p class="quick-start__description">
+      When you are looking to start with Qiskit you have two options. You can get started
+      locally and use your favorite development environment, or you can get started online
+      with IBM's hosted Jupyter Notebooks.
+    </p>
+    <div class="quick-start__sections">
+      <StartLocally />
+      <StartOnTheCloud />
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import StartLocally from './StartLocally.vue'
+import StartOnTheCloud from './StartOnTheCloud.vue'
+
+@Component({
+  components: {
+    StartLocally,
+    StartOnTheCloud
+  }
+})
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '~carbon-components/scss/globals/scss/typography';
+
+.quick-start {
+  color: $gray-60;
+
+  &__header {
+    @include type-style('expressive-heading-05');
+    color: $cool-gray-80;
+    margin-bottom: $layout-03;
+  }
+
+  &__description {
+    @include type-style('body-long-01');
+    max-width: 7 * $column-size-large;
+    margin-bottom: $layout-01;
+  }
+
+  &__sections {
+    display: flex;
+    flex-direction: row;
+  }
+}
+</style>
--- components/ui/SyntaxHighlight.vue
+++ components/ui/SyntaxHighlight.vue
@@ -4,11 +4,15 @@
       size="small"
       title="Copy to clipboard"
       class="syntax-highlight__copy-button"
+      :class="{ 'syntax-highlight__copy-button_legacy': legacy }"
       @click="copyToClipboard"
     >
       Copy
     </cv-button>
-    <pre v-highlightjs="code"><code class="syntax-highlight__code" :class="lang" /></pre>
+    <pre v-highlightjs="code"><code
+    class="syntax-highlight__code"
+    :class="[lang, { 'syntax-highlight__code_white-design': !legacy }]"
+    /></pre>
   </div>
 </template>
 
@@ -21,6 +25,7 @@ export default class extends Vue {
   @Prop({ type: String, default: 'python' }) lang!: string
   @Prop(String) label
   @Prop({ type: String, default: '' }) code!: string
+  @Prop({ type: Boolean, default: false }) legacy
 
   copyToClipboard () {
     navigator.clipboard.writeText(this.code)
@@ -40,9 +45,17 @@ export default class extends Vue {
   &__copy-button {
     position: absolute;
     right: 0;
-    text-transform: uppercase;
+    bottom: 0;
     border: none;
-    padding: 0.5rem;
+    text-transform: lowercase;
+    background-color: $purple-70;
+    padding: $spacing-03 $spacing-05;
+
+    &_legacy {
+      top: 0;
+      bottom: auto;
+      text-transform: uppercase;
+    }
 
     &:hover {
       background-color: $interactive-01;
@@ -53,6 +66,14 @@ export default class extends Vue {
   &__code {
     @include type-style('code-02');
     padding: $spacing-07 $spacing-05 $spacing-05;
+
+    &_white-design {
+      background-color: $cool-gray-10;
+      color: $gray-60;
+      padding-top: $spacing-05;
+      padding-left: $spacing-07;
+      padding-bottom: $spacing-09 + $spacing-06;
+    }
   }
 }
 </style>
