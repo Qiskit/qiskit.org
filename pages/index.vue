d0d44f51875b4d3048fb21606fb6be7f87478bc2
y4izus <yaiza.garcia.mm@gmail.com>
2020-06-24 18:03:22 +0200

    Implement requested changes

--- pages/index.vue
+++ pages/index.vue
@@ -172,7 +172,7 @@ export default class extends QiskitPage {
   &__paragraph {
     @include type-style('body-long-01');
     color: $gray-60;
-    max-width: 6.25 * $column-size-large;
+    max-width: 6 * $column-size-large;
     margin-bottom: $layout-04;
 
     @include mq($from: medium, $until: large) {
