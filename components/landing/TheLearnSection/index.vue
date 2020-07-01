35e0f08fe998d30d2dde62db30847bd54f3651e9
Salvador de la Puente González <salva@unoyunodiez.com>
2020-07-01 12:26:15 +0200

    Isolate TheLearnSection component

---
+++ components/landing/TheLearnSection/index.vue
@@ -0,0 +1,35 @@
+<template>
+  <article class="the-learn-section">
+    <div
+      class="
+        the-learn-section__container
+        landing-page__util
+        landing-page__util_vertical-spacer
+      "
+    >
+      <h2 class="landing-page__title">
+        Learn with Qiskit
+      </h2>
+      <LearnCard />
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import LearnCard from '~/components/landing/TheLearnSection/LearnCard.vue'
+
+@Component({ components: { LearnCard } })
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/blocks/landing-page.scss';
+
+.the-learn-section {
+  &__container {
+    @include contained();
+  }
+}
+</style>
