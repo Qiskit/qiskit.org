4ed101a9e6fe74a29a140d3112c990cddae9929d
Salvador de la Puente González <salva@unoyunodiez.com>
2020-07-01 10:44:01 +0200

    Add the prerequisites section to TheQuickInstall component

---
+++ components/landing/TheQuickStart/PrerequisitesForLinux.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="prerequisites">
+    <p class="prerequisites__title">
+      Install the compiler
+    </p>
+    <p class="prerequisites__copy">
+      If you’re using Fedora or an equivalent Linux distribution,
+      install using:
+    </p>
+    <pre class="prerequisites__code">dnf install @development-tools</pre>
+    <p class="prerequisites__copy">
+      For Ubuntu/Debian install it using:
+    </p>
+    <pre class="prerequisites__code">apt-get install build-essential</pre>
+    <p class="prerequisites__title">
+      Install OpenBLAS development headers
+    </p>
+    <p class="prerequisites__copy">
+      If you’re using Fedora or an equivalent Linux distribution,
+      install using:
+    </p>
+    <pre class="prerequisites__code">dnf install openblas-devel</pre>
+    <p class="prerequisites__copy">
+      For Ubuntu/Debian install it using:
+    </p>
+    <pre class="prerequisites__code">apt-get install libopenblas-dev</pre>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue { }
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/blocks/prerequisites.scss';
+</style>
