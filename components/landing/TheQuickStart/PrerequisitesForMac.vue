96596602b1bf4c86fa6e5c74735caf55da6ac7ba
Salvador de la Puente González <salva@unoyunodiez.com>
2020-07-01 10:44:01 +0200

    Add the prerequisites section to TheQuickInstall component

---
+++ components/landing/TheQuickStart/PrerequisitesForMac.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="prerequisites">
+    <p class="prerequisites__copy">
+      To use the Clang compiler on macOS, you need to install
+      an extra library for supporting OpenMP. You can use
+      <AppLink class="prerequisites__link" url="https://brew.sh/">
+        brew
+      </AppLink>
+      to install this and other dependencies.
+    </p>
+    <p class="prerequisites__title">
+      Install the compiler
+    </p>
+    <pre class="prerequisites__code">brew install libomp</pre>
+    <p class="prerequisites__title">
+      Install OpenBLAS development headers
+    </p>
+    <pre class="prerequisites__code">brew install openblas</pre>
+    <p class="prerequisites__title">
+      Install Xcode Command Line Tools.
+    </p>
+    <pre class="prerequisites__code">xcode-select --install</pre>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+import AppLink from '~/components/ui/AppLink.vue'
+
+@Component({ components: { AppLink } })
+export default class extends Vue { }
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/blocks/prerequisites.scss';
+</style>
---
+++ components/landing/TheQuickStart/PrerequisitesForWindows.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="prerequisites">
+    <p class="prerequisites__copy">
+      On Windows you need to use Anaconda3 or Miniconda3 to install all the dependencies.
+    </p>
+    <p class="prerequisites__title">
+      Install the compiler
+    </p>
+    <pre class="prerequisites__code">conda install --update-deps vs2017_win-64 vs2017_win-32 msvc_runtime</pre>
+    <p class="prerequisites__title">
+      Install OpenBLAS development headers
+    </p>
+    <pre class="prerequisites__code">conda install --update-deps -c conda-forge -y openblas cmake</pre>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue { }
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/blocks/prerequisites.scss';
+</style>
