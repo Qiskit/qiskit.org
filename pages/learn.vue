e365b0af12c82c546e756d886366df7285caf2b6
Salvador de la Puente González <salva@unoyunodiez.com>
2020-07-15 15:42:30 +0200

    Clarify names of functions regarding current filtering

--- pages/learn.vue
+++ pages/learn.vue
@@ -83,20 +83,18 @@
             </client-only>
           </fieldset>
           <section id="results" class="the-learning-resources-list__results">
-            <client-only>
-              <TheCarefulExplanationForBeginner
-                v-if="showingOneMinuteForBeginner && !showingEverything"
-                class="the-learning-resources-list__item"
-                :compact="showingMoreResources"
-                @ctaClick="showExplanation('beginner')"
-              />
-              <TheCarefulExplanationForAdvanced
-                v-if="showingOneMinuteForAdvanced && !showingEverything"
-                class="the-learning-resources-list__item"
-                :compact="showingMoreResources"
-                @ctaClick="showExplanation('advanced')"
-              />
-            </client-only>
+            <TheCarefulExplanationForBeginner
+              v-if="isShowingOneMinuteFor(LEARN_LEVELS.beginner) && !isShowingEverything"
+              class="the-learning-resources-list__item"
+              :compact="isShowingMoreResources"
+              @ctaClick="showExplanation('beginner')"
+            />
+            <TheCarefulExplanationForAdvanced
+              v-if="isShowingOneMinuteFor(LEARN_LEVELS.advanced) && !isShowingEverything"
+              class="the-learning-resources-list__item"
+              :compact="isShowingMoreResources"
+              @ctaClick="showExplanation('advanced')"
+            />
             <LearningResourceCard
               v-for="resource in filteredLearningResources"
               :key="resource.path"
@@ -178,32 +176,29 @@ export default class extends QiskitPage {
     this.$store.commit('setLearnLevel', level)
   }
 
-  get showingEverything (): boolean {
+  get isShowingEverything (): boolean {
     const { timeScale, learnLevel } = (this as any)
     return timeScale === TIME_SCALES.any && learnLevel === LEARN_LEVELS.all
   }
 
-  get showingOneMinuteForBeginner (): boolean {
-    return this.showingOneMinute && this.showingLevel(LEARN_LEVELS.beginner)
+  isShowingOneMinuteFor (level: LearnLevel): boolean {
+    return this.isShowingOneMinute && this.isShowingLevel(level)
   }
 
-  get showingOneMinuteForAdvanced (): boolean {
-    return this.showingOneMinute && this.showingLevel(LEARN_LEVELS.advanced)
-  }
-
-  get showingOneMinute (): boolean {
+  get isShowingOneMinute (): boolean {
     const { timeScale } = (this as any)
     return [TIME_SCALES.any, TIME_SCALES.minute].includes(timeScale)
   }
 
-  showingLevel (level: LearnLevel): boolean {
+  isShowingLevel (level: LearnLevel): boolean {
     const { learnLevel } = (this as any)
     return [LEARN_LEVELS.all, level].includes(learnLevel)
   }
 
-  get showingMoreResources () {
+  get isShowingMoreResources () {
+    const { advanced, beginner } = LEARN_LEVELS
     return (this as any).filteredLearningResources.length > 0 ||
-      (this.showingOneMinuteForAdvanced && this.showingOneMinuteForBeginner)
+      (this.isShowingOneMinuteFor(advanced) && this.isShowingOneMinuteFor(beginner))
   }
 }
 </script>
