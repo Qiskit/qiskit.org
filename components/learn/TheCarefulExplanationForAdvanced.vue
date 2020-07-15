bbc6fd87f70de9e6e02d6f9607f9bab78cfb2bd8
Salvador de la Puente González <salva@unoyunodiez.com>
2020-07-15 19:34:50 +0200

    Update copies

--- components/learn/TheCarefulExplanationForAdvanced.vue
+++ components/learn/TheCarefulExplanationForAdvanced.vue
@@ -5,35 +5,14 @@
         A Carefully Worded Summary of Quantum Computing for Advanced
       </h2>
       <p class="copy__paragraph">
-        There are many misconceptions about quantum computers, this is understandable since the science is exciting and potentially very useful, but is unfortunately not easily accessible to someone without the appropriate background. Part of the problem is that some quantum mechanical concepts cannot be analogised to everyday experiences, so that has been avoided here.
+        The ability to control and manipulate matter on an atomic scale was a dream first shared by Richard Feynman in his speech, “There’s Plenty of Room at the Bottom,” in 1959. By the 80’s, this idea really began to pick up speed, and a new subject called Quantum Computing had begun to emerge.
       </p>
     </template>
     <p class="copy__paragraph">
-      To understand why quantum computing is useful we need to explain how quantum mechanics came about. Before the development of quantum mechanics, physicists described the world around us using mathematical models that we now know as 'Classical Mechanics'.
+      In the 90’s, Peter Shor presented, for the first time, an algorithm that if run on a quantum processor, could provide a substantial speedup to the fundamental computational limits set by classical physics. The possibilities of a quantum computer, from being able to speed up algorithms, to their ability to more reliably simulate quantum systems themselves, was enough of an incentive for scientists and engineers across multiple disciplines to begin to dedicate their professional lives to this end goal.
     </p>
     <p class="copy__paragraph">
-      In the early 1900s, physicists carried out some experiments that classical mechanics could not explain: these results were impossible according to classical mechanics. A more accurate model was needed that could predict these results and we now call this new model 'Quantum Mechanics'.
-    </p>
-    <p class="copy__paragraph">
-      Classical mechanics is very accurate for a large number of applications, and we normally only see the disagreements between classical and quantum mechanics in very specific experiments, for example interactions on the atomic scale. We say that atoms and electrons are in 'quantum states' which means a state described by quantum mechanics. As you can imagine, it is very difficult to manipulate single atoms or electrons and as a result it is generally very difficult to manipulate quantum states.
-    </p>
-    <p class="copy__paragraph">
-      Around the 1950s &amp; '60s, computer science as a distinct discipline took off and theoretical computer scientists aimed to discover the limitations of computers. Since these computers could not manipulate quantum states, and there was not any known advantage of them being able to do so, these scientists used classical physics to create algorithms and determine how quickly certain problems could be solved.
-    </p>
-    <p class="copy__paragraph">
-      As mentioned before, classical mechanics determined some results impossible that quantum mechanics does not, and at the beginning of the 1980s it became apparent that a computer that can manipulate quantum states (quantum computer) would be able to perform certain calculations more efficiently than believed on a conventional (classical) computer.
-    </p>
-    <p class="copy__paragraph">
-      Currently we only know how to use quantum computers to improve a handful of algorithms and building a machine precise enough to manipulate quantum states is difficult and expensive. There are only a few working quantum computers at the time of writing, and they are still in the experimental stages.
-    </p>
-    <p class="copy__paragraph">
-      What Can Quantum Computers Be Used For?
-    </p>
-    <p class="copy__paragraph">
-      As mentioned before, quantum computers can perform certain calculations much faster than a classical computer. There are problems that would benefit from quantum computers that appear in many areas from drug design to cryptography.
-    </p>
-    <p class="copy__paragraph">
-      Building a quantum computer would not only be useful to solve computational problems, but also to test our theories of quantum physics. And as with all scientific research, the process of building a working quantum computer helps advance technology and materials sciences.
+      Given the rapid progress made in the field, it is hard to believe that It has only been a few years since Serge Haroche and David Wineland won the Nobel Prize in Physics in 2012 for “ground breaking experimental methods that enable measuring and manipulation of individual quantum systems”, and thus, fulfilled Feynman’s original proposal. We are now at a point where we are not only casual observers of quantum phenomena, but <em>designers</em> and <em>creators</em> of the quantum world. There remain numerous, fundamental challenges in physics, computer science, and information theory that will need to be confronted before a fault tolerant, universal quantum computer is realized. However, it would have been very hard to predict the progress this community has made just a decade ago, and we would be hard-pressed to guess what the next ten years are going to look like. Maybe now is the perfect time to get involved yourself, and we have provided all the tools you need here&mdash;real quantum machines, a programming framework through Qiskit, and a community that is happy to include you and answers any questions you may have along the way.
     </p>
   </CarefulExplanation>
 </template>
--- components/learn/TheCarefulExplanationForBeginner.vue
+++ components/learn/TheCarefulExplanationForBeginner.vue
@@ -5,35 +5,14 @@
         A Carefully Worded Summary of Quantum Computing for Beginners
       </h2>
       <p class="copy__paragraph">
-        There are many misconceptions about quantum computers, this is understandable since the science is exciting and potentially very useful, but is unfortunately not easily accessible to someone without the appropriate background. Part of the problem is that some quantum mechanical concepts cannot be analogised to everyday experiences, so that has been avoided here.
+        Physics is an attempt to describe the behaviour of the world around us. Around the early 20th century, through delicate experiments in laboratories, physicists saw strange behaviours which could not be explained by their current understanding of physics. These discoveries led to the development of the more complete ‘quantum’ physics, which was able to describe this behaviour very successfully.
       </p>
     </template>
     <p class="copy__paragraph">
-      To understand why quantum computing is useful we need to explain how quantum mechanics came about. Before the development of quantum mechanics, physicists described the world around us using mathematical models that we now know as 'Classical Mechanics'.
+      Quantum physics explained behaviour previously thought impossible, and it was later discovered that this could be exploited to solve specific computational problems that were also previously thought impossible. Since this behaviour occurs only in delicate laboratory experiments, performing these new computations require delicate machines known as quantum computers. We are still learning how to build quantum computers, and it will be a while before they are large and stable enough to be useful, but progress is promising and the science is fascinating.
     </p>
     <p class="copy__paragraph">
-      In the early 1900s, physicists carried out some experiments that classical mechanics could not explain: these results were impossible according to classical mechanics. A more accurate model was needed that could predict these results and we now call this new model 'Quantum Mechanics'.
-    </p>
-    <p class="copy__paragraph">
-      Classical mechanics is very accurate for a large number of applications, and we normally only see the disagreements between classical and quantum mechanics in very specific experiments, for example interactions on the atomic scale. We say that atoms and electrons are in 'quantum states' which means a state described by quantum mechanics. As you can imagine, it is very difficult to manipulate single atoms or electrons and as a result it is generally very difficult to manipulate quantum states.
-    </p>
-    <p class="copy__paragraph">
-      Around the 1950s &amp; '60s, computer science as a distinct discipline took off and theoretical computer scientists aimed to discover the limitations of computers. Since these computers could not manipulate quantum states, and there was not any known advantage of them being able to do so, these scientists used classical physics to create algorithms and determine how quickly certain problems could be solved.
-    </p>
-    <p class="copy__paragraph">
-      As mentioned before, classical mechanics determined some results impossible that quantum mechanics does not, and at the beginning of the 1980s it became apparent that a computer that can manipulate quantum states (quantum computer) would be able to perform certain calculations more efficiently than believed on a conventional (classical) computer.
-    </p>
-    <p class="copy__paragraph">
-      Currently we only know how to use quantum computers to improve a handful of algorithms and building a machine precise enough to manipulate quantum states is difficult and expensive. There are only a few working quantum computers at the time of writing, and they are still in the experimental stages.
-    </p>
-    <p class="copy__paragraph">
-      What Can Quantum Computers Be Used For?
-    </p>
-    <p class="copy__paragraph">
-      As mentioned before, quantum computers can perform certain calculations much faster than a classical computer. There are problems that would benefit from quantum computers that appear in many areas from drug design to cryptography.
-    </p>
-    <p class="copy__paragraph">
-      Building a quantum computer would not only be useful to solve computational problems, but also to test our theories of quantum physics. And as with all scientific research, the process of building a working quantum computer helps advance technology and materials sciences.
+      Quantum computing is a great way to dip your toes into quantum physics, and we have all the tools you need to learn right here: real quantum computers, a programming framework through Qiskit, and a community that is happy to include you and answer any questions you may have along the way.
     </p>
   </CarefulExplanation>
 </template>
