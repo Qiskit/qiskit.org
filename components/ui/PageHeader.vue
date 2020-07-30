95cd9e93f6644adc1fb6a774f76dc1142589dd63
y4izus <yaiza.garcia.mm@gmail.com>
2020-07-30 14:01:05 +0200

    Creates page header component

---
+++ components/ui/PageHeader.vue
@@ -0,0 +1,56 @@
+<template>
+  <header class="page-header">
+    <div class="page-header__container">
+      <h1 class="page-header__title">
+        <slot />
+      </h1>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/blocks/copy.scss';
+@import '~carbon-components/scss/globals/scss/typography';
+@include carbon--theme($carbon--theme--white);
+
+.page-header {
+  @include responsive-grid-bg-strip('/images/grid/grid-hero-learn.svg', auto, 28rem);
+  min-height: 28rem;
+
+  @include mq($until: large) {
+    min-height: 28rem * 40 / 64;
+  }
+
+  &__container {
+    @include contained();
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 28rem;
+
+    @include mq($until: large) {
+      height: 28rem * 40 / 64;
+    }
+  }
+
+  &__title {
+    @include type-style('expressive-heading-05', true);
+    color: $white-text-01;
+  }
+
+  &__relevant-word {
+    font-style: italic;
+    background-color: $purple-70;
+    color: white;
+    padding: $spacing-01 $spacing-03;
+  }
+}
+</style>
