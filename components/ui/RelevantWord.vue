128f0aefe36bef19648160418cd8cea268233fce
Yaiza <yaiza.garcia.mm@gmail.com>
2020-07-31 10:34:22 +0200

    Overview header (#802)
    * Adds overview link to menu and shows overview page with header
    
    * Creates page header component
    
    * Refactors overview to use PageHeader component
    
    * Refactors learn page to use PageHeader component
    
    * Creates RelevantWord component

---
+++ components/ui/RelevantWord.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="relevant-word">
+    <slot />
+  </span>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+.relevant-word {
+  font-style: italic;
+  background-color: $purple-70;
+  color: white;
+  padding: $spacing-01 $spacing-03;
+}
+</style>
---
+++ pages/overview.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="overview-page">
+    <PageHeader>
+      The most
+      <RelevantWord>
+        feature rich
+      </RelevantWord><br>
+      quantum computing SDK
+    </PageHeader>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import QiskitPage from '~/components/logic/QiskitPage.vue'
+import PageHeader from '~/components/ui/PageHeader.vue'
+import RelevantWord from '~/components/ui/RelevantWord.vue'
+
+@Component({
+  components: { PageHeader, RelevantWord },
+  head () {
+    return {
+      title: 'Qiskit Overview'
+    }
+  }
+})
+export default class extends QiskitPage {
+  routeName = 'overview'
+}
+</script>
+
+<style lang="scss" scoped>
+.overview-page {
+  background-color: white;
+}
+</style>
