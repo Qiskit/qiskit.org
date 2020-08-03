6b1494517c862d8b8387061100db78e1d152ae28
y4izus <yaiza.garcia.mm@gmail.com>
2020-08-03 17:35:23 +0200

    Creates a generic ContenSection element

--- components/overview/ContentSection.vue
+++ components/overview/ContentSection.vue
@@ -2,14 +2,13 @@
   <article class="content-section">
     <div class="content-section__overview">
       <h2 class="copy__title">
-        Research Applications
+        {{ title }}
       </h2>
       <p class="copy__paragraph content-section__description">
-        Qiskit allows for easy research and development for specific industry use
-        cases that have the highest potential for quantum advantage.
+        {{ description }}
       </p>
-      <AppLink class="copy__link" url="/">
-        See docs
+      <AppLink class="copy__link" :url="link.url">
+        {{ link.label }}
       </AppLink>
     </div>
     <div class="content-section__detail">
@@ -20,33 +19,33 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { Component } from 'vue-property-decorator'
+import { Component, Prop } from 'vue-property-decorator'
 import AppLink from '~/components/ui/AppLink.vue'
 
 @Component({
   components: { AppLink }
 })
-export default class extends Vue {}
+export default class extends Vue {
+  @Prop(String) title
+  @Prop(String) description
+  @Prop(Object) link
+}
 </script>
 
 <style lang="scss" scoped>
 @import '~/assets/scss/blocks/copy.scss';
 
 .content-section {
-  width: 13 * $column-size-large;
+  width: 100%;
   display: flex;
   margin-bottom: $layout-01 + $layout-05;
-
-  @include mq($until: large) {
-    width: 100%;
-  }
 
   @include mq($until: large) {
     flex-direction: column;
   }
 
   &__overview {
-    width: 40%;
+    width: 45%;
     padding-right: $spacing-07;
     margin-bottom: $layout-03;
 
@@ -62,7 +61,7 @@ export default class extends Vue {}
   }
 
   &__detail {
-    width: 60%;
+    width: 55%;
     background-color: blueviolet;
 
     @include mq($until: large) {
--- pages/overview.vue
+++ pages/overview.vue
@@ -10,10 +10,18 @@
     <section class="overview-page__content-container">
       <div class="overview-page__table-of-contents">TABLE OF CONTENTS</div>
       <div class="overview-page__list-of-contents">
-        <ContentSection>
+        <ContentSection
+          :title="researchAppsSection.title"
+          :description="researchAppsSection.description"
+          :link="researchAppsSection.link"
+        >
           Content Section
         </ContentSection>
-        <ContentSection>
+        <ContentSection
+          :title="libraryOfAlgorithms.title"
+          :description="libraryOfAlgorithms.description"
+          :link="libraryOfAlgorithms.link"
+        >
           Content Section 2
         </ContentSection>
       </div>
@@ -45,6 +53,24 @@ import ContentSection from '~/components/overview/ContentSection.vue'
 })
 export default class extends QiskitPage {
   routeName = 'overview'
+
+  researchAppsSection = {
+    title: 'Research Applications',
+    description: 'Qiskit allows for easy research and development for specific industry use cases that have the highest potential for quantum advantage.',
+    link: {
+      label: 'See docs',
+      url: '/'
+    }
+  }
+
+  libraryOfAlgorithms = {
+    title: 'Library of Algorithms',
+    description: 'Qiskit contains a library of cross-domain quantum algorithms upon which applications for near-term quantum computing can be built. These algorithms are designed to be extensible, and employs a pluggable framework where quantum algorithms can easily be added. It currently contains machine learning, optimization and core sets of algorithms.',
+    link: {
+      label: 'See docs',
+      url: '/'
+    }
+  }
 }
 </script>
 
@@ -65,16 +91,20 @@ export default class extends QiskitPage {
   }
 
   &__table-of-contents {
-    width: 3 * $column-size-large;
+    width: 20%;
     padding-right: $spacing-07;
     color: black;
 
-    @include mq($from: medium, $until: large) {
-      width: 2 * $column-size-medium;
-    }
-
     @include mq($until: medium) {
       display: none;
+    }
+  }
+
+  &__list-of-contents {
+    width: 80%;
+
+    @include mq($until: medium) {
+      width: 100%;
     }
   }
 }
