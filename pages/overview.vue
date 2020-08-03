dbe1e6e9df0a92841eb90e47d7e477fa96b824a8
Yaiza <yaiza.garcia.mm@gmail.com>
2020-08-03 12:04:35 +0200

    Creates the layout for overview page (#807)

--- pages/overview.vue
+++ pages/overview.vue
@@ -7,6 +7,11 @@
       </RelevantWord><br>
       quantum computing SDK
     </PageHeader>
+    <section class="overview-page__content-container copy__util_vertical-spacer">
+      <div class="overview-page__table-of-contents">TABLE OF CONTENTS</div>
+      <div class="overview-page__list-of-contents">LIST OF CONTENTS</div>
+    </section>
+    <TheQuickStart />
   </div>
 </template>
 
@@ -15,9 +20,10 @@ import { Component } from 'vue-property-decorator'
 import QiskitPage from '~/components/logic/QiskitPage.vue'
 import PageHeader from '~/components/ui/PageHeader.vue'
 import RelevantWord from '~/components/ui/RelevantWord.vue'
+import TheQuickStart from '~/components/landing/TheQuickStart/index.vue'
 
 @Component({
-  components: { PageHeader, RelevantWord },
+  components: { PageHeader, RelevantWord, TheQuickStart },
   head () {
     return {
       title: 'Qiskit Overview'
@@ -30,7 +36,32 @@ export default class extends QiskitPage {
 </script>
 
 <style lang="scss" scoped>
+@import '~/assets/scss/blocks/copy.scss';
+
 .overview-page {
   background-color: white;
+
+  &__content-container {
+    @include contained();
+    display: flex;
+  }
+
+  &__table-of-contents {
+    max-width: 3 * $column-size-large;
+    padding-right: $spacing-07;
+    background-color: black;
+
+    @include mq($from: medium, $until: large) {
+      max-width: 2 * $column-size-medium;
+    }
+
+    @include mq($until: medium) {
+      display: none;
+    }
+  }
+
+  &__list-of-contents {
+    background-color: green;
+  }
 }
 </style>
