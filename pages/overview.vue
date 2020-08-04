35787f36beea281fd1e059d77a72b0134a33a854
Yaiza <yaiza.garcia.mm@gmail.com>
2020-08-04 10:18:28 +0200

    Create a generic container for sections (#808)

--- pages/overview.vue
+++ pages/overview.vue
@@ -7,9 +7,24 @@
       </RelevantWord><br>
       quantum computing SDK
     </PageHeader>
-    <section class="overview-page__content-container copy__util_vertical-spacer">
+    <section class="overview-page__content-container">
       <div class="overview-page__table-of-contents">TABLE OF CONTENTS</div>
-      <div class="overview-page__list-of-contents">LIST OF CONTENTS</div>
+      <div class="overview-page__list-of-contents">
+        <ContentSection
+          :title="researchAppsSection.title"
+          :description="researchAppsSection.description"
+          :link="researchAppsSection.link"
+        >
+          Content Section
+        </ContentSection>
+        <ContentSection
+          :title="libraryOfAlgorithms.title"
+          :description="libraryOfAlgorithms.description"
+          :link="libraryOfAlgorithms.link"
+        >
+          Content Section 2
+        </ContentSection>
+      </div>
     </section>
     <TheQuickStart />
   </div>
@@ -21,9 +36,15 @@ import QiskitPage from '~/components/logic/QiskitPage.vue'
 import PageHeader from '~/components/ui/PageHeader.vue'
 import RelevantWord from '~/components/ui/RelevantWord.vue'
 import TheQuickStart from '~/components/landing/TheQuickStart/index.vue'
+import ContentSection from '~/components/overview/ContentSection.vue'
 
 @Component({
-  components: { PageHeader, RelevantWord, TheQuickStart },
+  components: {
+    PageHeader,
+    RelevantWord,
+    ContentSection,
+    TheQuickStart
+  },
   head () {
     return {
       title: 'Qiskit Overview'
@@ -32,6 +53,24 @@ import TheQuickStart from '~/components/landing/TheQuickStart/index.vue'
 })
 export default class extends QiskitPage {
   routeName = 'overview'
+
+  researchAppsSection = {
+    title: 'Research Applications',
+    description: 'Qiskit allows for easy research and development for specific industry use cases that have the highest potential for quantum advantage.',
+    link: {
+      label: 'See docs',
+      url: '/'
+    }
+  }
+
+  libraryOfAlgorithms = {
+    title: 'Library of Algorithms',
+    description: 'Qiskit contains a library of cross-domain quantum algorithms upon which applications for near-term quantum computing can be built. These algorithms are designed to be extensible, and employs a pluggable framework where quantum algorithms can easily be added. It currently contains machine learning, optimization and core sets of algorithms.',
+    link: {
+      label: 'See docs',
+      url: '/'
+    }
+  }
 }
 </script>
 
@@ -44,16 +83,17 @@ export default class extends QiskitPage {
   &__content-container {
     @include contained();
     display: flex;
+    margin-bottom: $layout-07 / 2;
+
+    @include mq($until: medium) {
+      margin-bottom: 0;
+    }
   }
 
   &__table-of-contents {
-    max-width: 3 * $column-size-large;
+    width: 20%;
     padding-right: $spacing-07;
-    background-color: black;
-
-    @include mq($from: medium, $until: large) {
-      max-width: 2 * $column-size-medium;
-    }
+    color: black;
 
     @include mq($until: medium) {
       display: none;
@@ -61,7 +101,11 @@ export default class extends QiskitPage {
   }
 
   &__list-of-contents {
-    background-color: green;
+    width: 80%;
+
+    @include mq($until: medium) {
+      width: 100%;
+    }
   }
 }
 </style>
