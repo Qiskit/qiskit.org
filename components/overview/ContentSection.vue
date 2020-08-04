761f1c598c4941c60ff9ad0fc362ad87be530cc1
Yaiza <yaiza.garcia.mm@gmail.com>
2020-08-04 10:18:28 +0200

    Create a generic container for sections (#808)

---
+++ components/overview/ContentSection.vue
@@ -0,0 +1,77 @@
+<template>
+  <article class="content-section">
+    <div class="content-section__overview">
+      <h2 class="copy__title">
+        {{ title }}
+      </h2>
+      <p class="content-section__description">
+        {{ description }}
+      </p>
+      <AppLink class="copy__link" :url="link.url">
+        {{ link.label }}
+      </AppLink>
+    </div>
+    <div class="content-section__detail">
+      <slot />
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+import AppLink from '~/components/ui/AppLink.vue'
+
+@Component({
+  components: { AppLink }
+})
+export default class extends Vue {
+  @Prop(String) title
+  @Prop(String) description
+  @Prop(Object) link
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/blocks/copy.scss';
+
+.content-section {
+  width: 100%;
+  display: flex;
+  margin-bottom: $layout-07 / 2;
+
+  @include mq($until: large) {
+    flex-direction: column;
+  }
+
+  &__overview {
+    width: 45%;
+    padding-right: $spacing-07;
+    margin-bottom: $layout-03;
+
+    @include mq($until: large) {
+      width: 100%;
+    }
+  }
+
+  &__description {
+    @include type-style('body-long-01');
+    color: $cool-gray-80;
+    margin-bottom: $layout-05;
+
+    @include mq($until: large) {
+      @include type-style('label-01');
+      margin-bottom: $layout-03;
+    }
+  }
+
+  &__detail {
+    width: 55%;
+    background-color: blueviolet;
+
+    @include mq($until: large) {
+      width: 100%;
+    }
+  }
+}
+</style>
