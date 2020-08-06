bda6f0fcda7713beb21b339b73d42209298c8c71
y4izus <yaiza.garcia.mm@gmail.com>
2020-08-06 12:52:57 +0200

    Creates TableOfContents component

---
+++ components/overview/TableOfContents.vue
@@ -0,0 +1,100 @@
+<template>
+  <nav class="table-of-contents">
+    <AppLink
+      v-for="element in elements"
+      :key="element.label"
+      class="table-of-contents__element"
+      :class="{ 'table-of-contents__element_secondary': element.isSecondary }"
+      :url="element.url"
+    >
+      {{ element.label }}
+    </AppLink>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import AppLink from '~/components/ui/AppLink.vue'
+
+@Component({
+  components: {
+    AppLink
+  }
+})
+export default class extends Vue {
+  elements = [
+    {
+      url: '#applications',
+      label: 'Write quantum code'
+    },
+    {
+      url: '#applications',
+      label: 'Applications',
+      isSecondary: true
+    },
+    {
+      url: '#algorithms',
+      label: 'Algorithms',
+      isSecondary: true
+    },
+    {
+      url: '#benchmarking',
+      label: 'Benchmarking',
+      isSecondary: true
+    },
+    {
+      url: '#circuits',
+      label: 'Circuits',
+      isSecondary: true
+    },
+    {
+      url: '#simulators',
+      label: 'Run Quantum Code'
+    },
+    {
+      url: '#simulators',
+      label: 'Simulators',
+      isSecondary: true
+    },
+    {
+      url: '#hardware',
+      label: 'Hardware',
+      isSecondary: true
+    }
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~carbon-components/scss/globals/scss/typography';
+
+.table-of-contents {
+  width: 20%;
+  padding-right: $spacing-07;
+  position: -webkit-sticky; /* Safari */
+  position: sticky;
+  top: 0;
+  display: flex;
+  flex-direction: column;
+
+  @include mq($until: medium) {
+    display: none;
+  }
+
+  &__element {
+    @include type-style('label-01');
+    color: $gray-80;
+    text-decoration: none;
+    margin-bottom: $layout-02;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    &_secondary {
+      margin-left: $layout-03;
+    }
+  }
+}
+</style>
