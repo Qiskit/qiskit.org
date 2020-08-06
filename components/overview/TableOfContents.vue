f9e62cc1a247f22d9472fe39f58bbc0a3ad1ed34
Yaiza <yaiza.garcia.mm@gmail.com>
2020-08-06 17:47:38 +0200

    Create sticky menu (#814)

---
+++ components/overview/TableOfContents.vue
@@ -0,0 +1,99 @@
+<template>
+  <nav class="table-of-contents">
+    <AppLink
+      v-for="element in elements"
+      :key="element.label"
+      class="table-of-contents__element"
+      :class="{ 'table-of-contents__element_secondary': element.isSecondary }"
+      :url="element.url"
+    >
+      {{ element.label }}
+    </AppLink>
+  </nav>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import AppLink from '~/components/ui/AppLink.vue'
+
+@Component({
+  components: { AppLink }
+})
+export default class extends Vue {
+  elements = [
+    {
+      url: '#applications',
+      label: 'Write quantum code'
+    },
+    {
+      url: '#applications',
+      label: 'Applications',
+      isSecondary: true
+    },
+    {
+      url: '#algorithms',
+      label: 'Algorithms',
+      isSecondary: true
+    },
+    {
+      url: '#characterization',
+      label: 'Characterization',
+      isSecondary: true
+    },
+    {
+      url: '#circuits',
+      label: 'Circuits',
+      isSecondary: true
+    },
+    {
+      url: '#simulators',
+      label: 'Run Quantum Code'
+    },
+    {
+      url: '#simulators',
+      label: 'Simulators',
+      isSecondary: true
+    },
+    {
+      url: '#hardware',
+      label: 'Hardware',
+      isSecondary: true
+    }
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~carbon-components/scss/globals/scss/typography';
+
+.table-of-contents {
+  display: flex;
+  flex-direction: column;
+
+  &__element {
+    @include type-style('body-long-01');
+    color: $cool-gray-80;
+    text-decoration: none;
+    margin-bottom: $layout-02;
+
+    @include mq($until: large) {
+      @include type-style('label-01');
+    }
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    &_secondary {
+      &::before {
+        content: "-";
+        color: $purple-70;
+        font-weight: bold;
+        display:inline-block;
+        padding-right: $layout-02;
+      }
+    }
+  }
+}
+</style>
