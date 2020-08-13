60874bbc9b43bca52ba9254e908368a141f821ad
y4izus <yaiza.garcia.mm@gmail.com>
2020-08-13 12:36:08 +0200

    Adds collection of algorithms content

--- constants/overviewContent.ts
+++ constants/overviewContent.ts
@@ -98,7 +98,73 @@ const RESEARCH_APPS: ContentSection = {
   ]
 }
 
+const algorithmsCollectionId: string = 'algorithms'
+
+const ALGORITHMS_COLLECTION_IN_MENU: TableOfContentItem = {
+  url: `#${algorithmsCollectionId}`,
+  label: 'Algorithms',
+  isSecondary: true
+}
+
+const ALGORITHMS_COLLECTION: ContentSection = {
+  id: 'algorithms',
+  title: 'Collection of Algorithms',
+  description: 'Qiskit contains a generic framework of cross-domain quantum algorithms upon which applications for near-term quantum computing can be built.',
+  link: {
+    label: 'See docs',
+    url: '/'
+  },
+  subSections: [
+    {
+      title: 'Grover',
+      content: {
+        image: '/images/overview/algorithms/grover.png',
+        description: 'Grover\'s algorithm is a well know quantum algorithm part of the amplitude amplifier category that provides quadratic speedup for searching through unstructured collections of records in search of particular targets.',
+        cta: {
+          label: 'Try it out',
+          url: '/'
+        }
+      }
+    },
+    {
+      title: 'VQE',
+      content: {
+        image: '/images/overview/algorithms/vqe.png',
+        description: 'VQE is another well known quantum algorithm part of the minimum eigensolvers category. This algorithm uses variational techniques and interleaves quantum and classical computations in order to find the minimum eigenvalue of the hamiltonian of a given system.',
+        cta: {
+          label: 'Try it out',
+          url: '/'
+        }
+      }
+    },
+    {
+      title: 'QAOA',
+      content: {
+        image: '/images/overview/algorithms/qaoa.png',
+        description: 'QAOA is also part of the minimum eigensolvers category. This algorithm extends VQE and inherits VQE\'s general optimization structure but uses its own fine-tuned variational form.',
+        cta: {
+          label: 'Try it out',
+          url: '/'
+        }
+      }
+    },
+    {
+      title: 'QSVM',
+      content: {
+        image: '/images/overview/algorithms/qsvm.png',
+        description: 'QSVM is  part of the classifiers category and applies to problems that require a feature map for which computing the collection of inner products is not efficient classically. ',
+        cta: {
+          label: 'Try it out',
+          url: '/'
+        }
+      }
+    }
+  ]
+}
+
 export {
   RESEARCH_APPS_IN_MENU,
-  RESEARCH_APPS
+  RESEARCH_APPS,
+  ALGORITHMS_COLLECTION_IN_MENU,
+  ALGORITHMS_COLLECTION
 }
