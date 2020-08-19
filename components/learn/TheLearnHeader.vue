4d324aef07be24e9eed9fdfa5b873198b942040d
Salvador de la Puente González <salva@unoyunodiez.com>
2020-08-19 09:10:15 +0200

    Extract TheLearnHeader

---
+++ components/learn/TheLearnHeader.vue
@@ -0,0 +1,59 @@
+<template>
+  <header class="the-learn-header">
+    <div class="the-learn-header__container">
+      <h1 class="the-learn-header__title">
+        Start your path towards<br>
+        learning
+        <span class="the-learn-header__relevant-word">
+          Qiskit
+        </span>
+      </h1>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue { }
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/blocks/copy.scss';
+
+.the-learn-header {
+  @include responsive-grid-bg-strip('/images/grid/grid-hero-learn.svg', auto, 28rem);
+  min-height: 28rem;
+
+  @include mq($until: large) {
+    min-height: 28rem * 40 / 64;
+  }
+
+  &__container {
+    @include contained();
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 28rem;
+
+    @include mq($until: large) {
+      height: 28rem * 40 / 64;
+    }
+  }
+
+  &__title {
+    @include type-style('expressive-heading-05', true);
+    color: $white-text-01;
+  }
+
+  &__relevant-word {
+    font-style: italic;
+    background-color: $purple-70;
+    color: white;
+    padding: $spacing-01 $spacing-03;
+  }
+}
+
+</style>
