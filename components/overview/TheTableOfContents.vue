9a9ead1886787646278e697c7b18d84a4dfcdb48
Salvador de la Puente González <salva@unoyunodiez.com>
2020-08-20 12:27:22 +0200

    Connect the TOC with the URL fragment and section visibility (#850)
    * Add active style to TOC entries and implement API to control what is the active section
    
    * Connect URL fragment with active TOC entry
    
    * Connect active TOC entry with the visibility of the section

--- components/overview/TheTableOfContents.vue
+++ components/overview/TheTableOfContents.vue
@@ -2,13 +2,14 @@
   <nav class="the-table-of-contents">
     <AppLink
       v-for="entry in entries"
-      :key="entry.label"
+      :key="entry.sectionId || entry.label"
       class="the-table-of-contents__entry"
-      :class="{ 'the-table-of-contents__entry_second-level': entry.isSecondary }"
-      :url="entry.url"
-    >
-      {{ entry.label }}
-    </AppLink>
+      :class="{
+        'the-table-of-contents__entry_active': isActive(entry),
+        'the-table-of-contents__entry_second-level': entry.isSecondary
+      }"
+      :url="entry.sectionId && `#${entry.sectionId}`"
+    >{{ entry.label }}</AppLink>
   </nav>
 </template>
 
@@ -16,12 +17,18 @@
 import Vue from 'vue'
 import { Component, Prop } from 'vue-property-decorator'
 import AppLink from '~/components/ui/AppLink.vue'
+import { TableOfContentEntry } from '~/constants/overviewContent'
 
 @Component({
   components: { AppLink }
 })
 export default class extends Vue {
-  @Prop(Array) entries
+  @Prop({ type: Array, default: [] }) entries!: Array<TableOfContentEntry>
+  @Prop(String) activeSection!: string
+
+  isActive (entry: TableOfContentEntry): boolean {
+    return !!(entry.sectionId && (entry.sectionId === this.activeSection))
+  }
 }
 </script>
 
@@ -42,17 +49,31 @@ export default class extends Vue {
       @include type-style('label-01');
     }
 
-    &:hover {
-      text-decoration: underline;
-    }
-
     &_second-level {
+      &:hover {
+        text-decoration: underline;
+      }
+
       &::before {
         content: "-";
         color: $purple-70;
         font-weight: bold;
-        display:inline-block;
+        display: inline-block;
         padding-right: $layout-02;
+      }
+    }
+
+    &_active {
+      font-weight: bold;
+    }
+
+    // A way for expressing combinations of multiple BEM modifiers in SASS.
+    // Inspiration at:
+    // https://stackoverflow.com/questions/41181012/scss-bem-style-children-structure-when-parent-has-modificator
+    // https://stackoverflow.com/questions/47652849/matching-multiple-bem-modifiers-in-sass/47653943#47653943
+    &_active#{&} {
+      &_second-level::before {
+        content: "■";
       }
     }
   }
