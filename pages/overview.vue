c2e4ef7481accb78f4ae235ac1d126eb0ca5b273
Salvador de la Puente González <salva@unoyunodiez.com>
2020-08-20 12:27:22 +0200

    Connect the TOC with the URL fragment and section visibility (#850)
    * Add active style to TOC entries and implement API to control what is the active section
    
    * Connect URL fragment with active TOC entry
    
    * Connect active TOC entry with the visibility of the section

--- pages/overview.vue
+++ pages/overview.vue
@@ -13,7 +13,10 @@
       "
     >
       <div class="overview-page__table-of-contents">
-        <TheTableOfContents :entries="tocEntries" />
+        <TheTableOfContents
+          :entries="tocEntries"
+          :active-section="activeSection"
+        />
         <AppCta class="overview-page__get-started" url="#quick-start">
           Get Started
         </AppCta>
@@ -82,6 +85,64 @@ export default class extends QiskitPage {
   tocEntries = TABLE_OF_CONTENTS
   contentSections = CONTENT_SECTIONS
 
+  activeSection = ''
+
+  _observer: IntersectionObserver | null = null
+
+  mounted () {
+    const threshold = [...Array(25).keys()].map(x => 4 * x / 100)
+    const windowTriggerMargins = '-16px 0px -66% 0px'
+    this._observer = new IntersectionObserver(
+      this._onSectionAppearing,
+      {
+        root: this.$root.$el,
+        rootMargin: windowTriggerMargins,
+        threshold
+      }
+    );
+    (this.$el as HTMLElement)
+      .querySelectorAll('.overview-page__content-section')
+      .forEach((section) => {
+        (this._observer as IntersectionObserver).observe(section)
+      })
+  }
+
+  beforeDestroy () {
+    this._observer && this._observer.disconnect()
+  }
+
+  beforeRouteEnter (route, _, next) {
+    next(overviewPage => overviewPage._parseSectionFromUrl(route))
+  }
+
+  beforeRouteUpdate (route, _, next) {
+    this._parseSectionFromUrl(route)
+    next()
+  }
+
+  _onSectionAppearing (entries: Array<IntersectionObserverEntry>) {
+    let highestTopValue = Infinity
+    entries.forEach((entry) => {
+      const {
+        target,
+        boundingClientRect,
+        rootBounds
+      } = entry
+      if (!rootBounds) { return }
+      const targetTop = boundingClientRect.top
+      const triggerWindowBottom = rootBounds.bottom
+      const onTop = targetTop >= 0 && targetTop <= triggerWindowBottom
+      if (onTop && targetTop < highestTopValue) {
+        this.activeSection = target.id
+        highestTopValue = targetTop
+      }
+    })
+  }
+
+  _parseSectionFromUrl (route) {
+    this.activeSection = route.hash.substr(1)
+  }
+
   asTabs (subsections: Array<OverviewSubSection>): Array<ContentAccordionTab> {
     return subsections.map(subsection => subsection as ContentAccordionTab)
   }
@@ -134,6 +195,7 @@ export default class extends QiskitPage {
   }
 
   &__content-section-image {
+    max-width: 100%;
     max-height: 30.5rem;
   }
 }
