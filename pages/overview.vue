de1f8679f73f0028e9f71a105be92589dae767d9
Salvador de la Puente González <salva@unoyunodiez.com>
2020-08-20 11:59:27 +0200

    Connect active TOC entry with the visibility of the section

--- pages/overview.vue
+++ pages/overview.vue
@@ -87,6 +87,30 @@ export default class extends QiskitPage {
 
   activeSection = ''
 
+  _observer: IntersectionObserver | null = null
+
+  mounted () {
+    const threshold = [...Array(25).keys()].map(x => 4*x/100)
+    const windowTriggerMargins = '-16px 0px -66% 0px'
+    this._observer = new IntersectionObserver(
+      this._onSectionAppearing,
+      {
+        root: this.$root.$el,
+        rootMargin: windowTriggerMargins,
+        threshold
+      }
+    );
+    (this.$el as HTMLElement)
+      .querySelectorAll('.overview-page__content-section')
+      .forEach(section => {
+        (this._observer as IntersectionObserver).observe(section)
+      })
+  }
+
+  beforeDestroy() {
+    this._observer && this._observer.disconnect()
+  }
+
   beforeRouteEnter (route, _, next) {
     next(overviewPage => overviewPage._parseSectionFromUrl(route))
   }
@@ -94,6 +118,26 @@ export default class extends QiskitPage {
   beforeRouteUpdate (route, _, next) {
     this._parseSectionFromUrl(route)
     next()
+  }
+
+  _onSectionAppearing (entries: Array<IntersectionObserverEntry>) {
+    let highestTopValue = Infinity
+    entries.forEach(entry => {
+      const {
+        target,
+        boundingClientRect,
+        rootBounds
+      } = entry
+      if (!rootBounds) { return }
+      const targetTop = boundingClientRect.top
+      const triggerWindowTop = rootBounds.top
+      const triggerWindowBottom = rootBounds.bottom
+      const onTop = targetTop >= 0 && targetTop <= triggerWindowBottom
+      if (onTop && targetTop < highestTopValue) {
+        this.activeSection = target.id
+        highestTopValue = targetTop
+      }
+    })
   }
 
   _parseSectionFromUrl (route) {
@@ -152,6 +196,7 @@ export default class extends QiskitPage {
   }
 
   &__content-section-image {
+    max-width: 100%;
     max-height: 30.5rem;
   }
 }
