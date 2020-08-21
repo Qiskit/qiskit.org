220003a6967136b43078633a180774cb89e673e1
Salvador de la Puente González <salva@unoyunodiez.com>
2020-08-21 13:11:08 +0200

    Review Overview Content wording and CTAs (#851)

--- constants/overviewContent.ts
+++ constants/overviewContent.ts
@@ -50,16 +50,16 @@ const RESEARCH_APPS: OverviewSection = {
   description: 'Qiskit allows for easy research and development for specific industry use cases that have the highest potential for quantum advantage.',
   link: {
     label: 'See docs',
-    url: '/'
+    url: 'https://qiskit.org/documentation/apidoc/aqua.html'
   },
   subSections: [
     {
       title: 'Optimization',
       content: {
         image: '/images/overview/applications/optimization.png',
-        description: 'The Qiskit Optimization package covers the whole range from high-level modeling of optimization problems, with automatic conversion of problems to different required representations, to a suite of easy-to-use quantum optimization algorithms that are ready to run on classical simulators, as well as on real quantum.',
+        description: 'The Qiskit Optimization package covers the whole range from high-level modeling of optimization problems, with automatic conversion of problems to different required representations, to a suite of easy-to-use quantum optimization algorithms that are ready to run on classical simulators, as well as on real quantum systems.',
         cta: {
-          label: 'Try the Max Cut Problem',
+          label: 'Solve the Max Cut Problem',
           url: 'https://github.com/Qiskit/qiskit-aqua#creating-your-first-optimization-programming-experiment-in-qiskit'
         }
       }
@@ -68,9 +68,9 @@ const RESEARCH_APPS: OverviewSection = {
       title: 'Finance',
       content: {
         image: '/images/overview/applications/finance.png',
-        description: 'The Qiskit Finance package contains uncertainty components for stock/securities problems, Ising translators for portfolio optimizations and data providers to source real or random data to finance experiments.',
+        description: 'The Qiskit Finance package contains components to load uncertainty models, e.g., for pricing securities/derivatives or analyzing the risk involved. It also contains data providers to source real or random data to finance experiments and together with the Qiskit Optimization package allows easy modeling of optimization problems as arising e.g. in portfolio management.',
         cta: {
-          label: 'Try Option Pricing with qGans',
+          label: 'Perform Option Pricing with qGans',
           url: 'https://qiskit.org/documentation/tutorials/finance/10_qgan_option_pricing.html'
         }
       }
@@ -81,7 +81,7 @@ const RESEARCH_APPS: OverviewSection = {
         image: '/images/overview/applications/machine-learning.png',
         description: 'The Qiskit Machine Learning package simply contains sample datasets at present. Qiskit has some classification algorithms such as QSVM and VQC (Variational Quantum Classifier), where this data can be used for experiments, and there is also QGAN (Quantum Generative Adversarial Network) algorithm.',
         cta: {
-          label: 'VQC with trained dataset',
+          label: 'Classify data with a VQC',
           url: 'https://github.com/Qiskit/qiskit-aqua#creating-your-first-machine-learning-programming-experiment-in-qiskit'
         }
       }
@@ -92,7 +92,7 @@ const RESEARCH_APPS: OverviewSection = {
         image: '/images/overview/applications/chemestry.png',
         description: 'The Qiskit Chemistry package supports problems including ground state energy computations, excited states and dipole moments of molecule, both open and closed-shell.',
         cta: {
-          label: 'Ground State energy of a Molecule',
+          label: 'Find the Energy Ground State of a Molecule',
           url: 'https://github.com/Qiskit/qiskit-aqua#creating-your-first-chemistry-programming-experiment-in-qiskit'
         }
       }
@@ -132,10 +132,10 @@ const ALGORITHMS_COLLECTION: OverviewSection = {
       title: 'VQE',
       content: {
         image: '/images/overview/algorithms/vqe.png',
-        description: 'VQE is another well known quantum algorithm part of the minimum eigensolvers category. This algorithm uses variational techniques and interleaves quantum and classical computations in order to find the minimum eigenvalue of the hamiltonian of a given system.',
+        description: 'VQE (Variational Quantum Eigensolver) is another well known quantum algorithm part of the minimum eigensolvers category. This algorithm uses variational techniques and interleaves quantum and classical computations in order to find the minimum eigenvalue of the Hamiltonian of a given system.',
         cta: {
           label: 'Try it out',
-          url: 'https://qiskit.org/documentation/apidoc/qiskit.aqua.algorithms.html#minimum-eigensolvers'
+          url: 'https://qiskit.org/documentation/stubs/qiskit.aqua.algorithms.VQE.html#qiskit.aqua.algorithms.VQE'
         }
       }
     },
@@ -146,7 +146,7 @@ const ALGORITHMS_COLLECTION: OverviewSection = {
         description: 'QAOA is also part of the minimum eigensolvers category. This algorithm extends VQE and inherits VQE\'s general optimization structure but uses its own fine-tuned variational form.',
         cta: {
           label: 'Try it out',
-          url: 'https://qiskit.org/documentation/apidoc/qiskit.aqua.algorithms.html#minimum-eigensolvers'
+          url: 'https://qiskit.org/documentation/stubs/qiskit.aqua.algorithms.QAOA.html#qiskit.aqua.algorithms.QAOA'
         }
       }
     },
@@ -178,17 +178,17 @@ const EXPERIMENTALIST_TOOLBOX: OverviewSection = {
   description: 'Qiskit provides a framework for understanding and mitigating noise in quantum circuits and systems. The experiments provided in Qiskit are grouped into the topics of characterization, verification and mitigation.',
   link: {
     label: 'See docs',
-    url: '/'
+    url: 'https://qiskit.org/documentation/apidoc/ignis.html'
   },
   subSections: [
     {
       title: 'Characterization',
       content: {
         image: '/images/overview/characterization/characterization.png',
-        description: 'Qiskit\'s characterization framework brings the analysis parameters and circuits to users in order to understand and characterize the source of noice that impacts our devices. Such parameters include T1, T2-start, T2, Hamiltonian parameters such as the ZZ interaction rate and control errors in the gates.',
+        description: 'Qiskit\'s characterization framework offers circuits and analysis methods to understand and characterize the source of noice that impacts our devices. Such parameters include T1, T₂* ,T2 , Hamiltonian parameters such as the ZZ interaction rate and control errors in the gates.',
         cta: {
           label: 'See Characterization framework',
-          url: '/'
+          url: 'https://qiskit.org/documentation/apidoc/characterization.html'
         }
       }
     },
@@ -199,7 +199,7 @@ const EXPERIMENTALIST_TOOLBOX: OverviewSection = {
         description: 'Qiskit\'s verification framework provides experiments that are designed to verify gates and small circuit performance through tomography, quantum volume and randomized benchmarking.',
         cta: {
           label: 'See Verification framework',
-          url: '/'
+          url: 'https://qiskit.org/documentation/apidoc/verification.html'
         }
       }
     },
@@ -207,10 +207,10 @@ const EXPERIMENTALIST_TOOLBOX: OverviewSection = {
       title: 'Calibration',
       content: {
         image: '/images/overview/characterization/calibration.png',
-        description: 'Qiskit\'s calibration module allows users to perform proper calibration through rabi schedules and drag schedules in order to mitigate noise by producing the appropriate fitter for the experiment.',
+        description: 'Qiskit\'s calibration module allows users to optimize pulse parameters to minimize errors. Perform simple parameter scans, with more sophisticated error amplification sequences coming soon.',
         cta: {
           label: 'See Calibration module',
-          url: '/'
+          url: 'https://qiskit.org/documentation/apidoc/characterization.html#calibrations'
         }
       }
     }
@@ -227,11 +227,11 @@ const CIRCUITS_IN_MENU: TableOfContentEntry = {
 
 const CIRCUITS: OverviewSection = {
   id: 'circuits',
-  title: 'Circuits',
+  title: 'Library of Circuits',
   description: 'Qiskit provides a set of tools for composing quantum programs at the level of circuits and pulses, optimizing them for the constraints of a particular physical quantum processor, and managing the batched execution of experiments on remote-access backends.',
   link: {
     label: 'See docs',
-    url: '/'
+    url: 'https://qiskit.org/documentation/apidoc/circuit.html'
   },
   subSections: [
     {
@@ -240,8 +240,8 @@ const CIRCUITS: OverviewSection = {
         image: '/images/overview/circuits/compiler.png',
         description: 'Qiskit robust compiler provides users with the ability to explore how quantum circuits run on real hardware and optimize such circuits using modules such as pass managers.',
         cta: {
-          label: 'Call to action',
-          url: '/'
+          label: 'See Compiler',
+          url: 'https://qiskit.org/documentation/apidoc/compiler.html'
         }
       }
     },
@@ -251,19 +251,19 @@ const CIRCUITS: OverviewSection = {
         image: '/images/overview/circuits/circuit-library.png',
         description: 'Circuit Library is a collection of well studied circuits and gates that can be plugged into many experiments allowing users to program at higher levels of abstraction when developing and experimenting with circuits.',
         cta: {
-          label: 'Call to action',
-          url: '/'
+          label: 'See Circuit Library',
+          url: 'https://qiskit.org/documentation/apidoc/circuit_library.html'
         }
       }
     },
     {
-      title: 'Pulses',
+      title: 'Pulse',
       content: {
         image: '/images/overview/circuits/pulses.png',
         description: 'Pulse is a lower level quantum programming tool. This tool allows users more control over using quantum circuits when interacting with real quantum hardware.',
         cta: {
-          label: 'Call to action',
-          url: '/'
+          label: 'See Pulse',
+          url: 'https://qiskit.org/documentation/apidoc/pulse.html'
         }
       }
     }
@@ -284,7 +284,7 @@ const SIMULATORS: OverviewSection = {
   description: 'Qiskit provides a framework for understanding and mitigating noise in quantum circuits and systems. The experiments provided in Qiskit are grouped into the topics of characterization, verification and mitigation.',
   link: {
     label: 'See docs',
-    url: '/'
+    url: 'https://qiskit.org/documentation/apidoc/aer.html'
   },
   image: '/images/overview/simulators/laptop.png'
 }
@@ -303,14 +303,14 @@ const HARDWARE: OverviewSection = {
   description: 'Circuits are the foundational roots for our software stack. Qiskit provides a set of tools for composing quantum programs at the level of circuits and pulses, optimizing them for the constraints of a particular physical quantum processor, and managing the batched execution of experiments on remote-access backends. Qiskit is modularly constructed, simplifying the addition of extensions for circuit optimizations and backends.',
   link: {
     label: 'See docs',
-    url: '/'
+    url: 'https://qiskit.org/documentation/apidoc/ibmq_provider.html'
   },
   image: '/images/overview/hardware/quantum-computer.png'
 }
 
 const TABLE_OF_CONTENTS: TableOfContentEntry[] = [
   {
-    label: 'Write quantum code'
+    label: 'Write Quantum Code'
   },
   RESEARCH_APPS_IN_MENU,
   ALGORITHMS_COLLECTION_IN_MENU,
