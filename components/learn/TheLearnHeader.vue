db6f014bac553ac831904934dd7f6a045a1285b9
Salvador de la Puente González <salva@unoyunodiez.com>
2020-09-03 21:52:15 +0200

    Use ThePageHeader for implementing TheLearnHeader

--- components/learn/TheLearnHeader.vue
+++ components/learn/TheLearnHeader.vue
@@ -1,60 +1,16 @@
 <template>
-  <header class="the-learn-header">
-    <div class="the-learn-header__container">
-      <h1 class="the-learn-header__title">
-        Start your path towards<br>
-        learning
-        <span class="the-learn-header__relevant-word">
-          Qiskit
-        </span>
-      </h1>
-    </div>
-  </header>
+  <ThePageHeader class="the-learn-header">
+    Start your path towards<br>
+    learning <RelevantWord>Qiskit</RelevantWord>
+  </ThePageHeader>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
+import ThePageHeader from '~/components/ui/ThePageHeader.vue'
+import RelevantWord from '~/components/ui/RelevantWord.vue'
 
-@Component
+@Component({ components: { ThePageHeader, RelevantWord } })
 export default class extends Vue { }
 </script>
-
-<style lang="scss" scoped>
-@import '~/assets/scss/blocks/copy.scss';
-@import '~carbon-components/scss/globals/scss/typography';
-
-.the-learn-header {
-  @include responsive-grid-bg-strip('/images/grid/grid-hero-learn.svg', auto, 28rem);
-  min-height: 28rem;
-
-  @include mq($until: large) {
-    min-height: 28rem * 40 / 64;
-  }
-
-  &__container {
-    @include contained();
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    height: 28rem;
-
-    @include mq($until: large) {
-      height: 28rem * 40 / 64;
-    }
-  }
-
-  &__title {
-    @include type-style('expressive-heading-05', true);
-    color: $white-text-01;
-  }
-
-  &__relevant-word {
-    font-style: italic;
-    background-color: $purple-70;
-    color: white;
-    padding: $spacing-01 $spacing-03;
-  }
-}
-
-</style>
