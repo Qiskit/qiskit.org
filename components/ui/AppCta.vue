cd1561908d545c4ecd32ff4c80422c1545416c9e
korgan00 <korgan00@gmail.com>
2020-09-18 04:18:48 +0200

    Link arrow animation (#914)
    * Arrow animation on hover
    
    Co-authored-by: Salvador de la Puente González <salva@unoyunodiez.com>

--- components/ui/AppCta.vue
+++ components/ui/AppCta.vue
@@ -2,10 +2,19 @@
   <AppLink
     ref="link"
     class="app-cta copy__link"
+    :class="{
+      'app-cta_is-external': isExternal,
+      'app-cta_is-internal': !isExternal
+    }"
     v-bind="$attrs"
   >
-    <div><slot /></div>
-    <component :is="isExternal ? 'Launch16' : 'ArrowRight16'" />
+    <div class="app-cta__content">
+      <slot />
+    </div>
+    <component
+      :is="isExternal ? 'Launch16' : 'ArrowRight16'"
+      class="app-cta__icon"
+    />
   </AppLink>
 </template>
 
@@ -34,8 +43,44 @@ export default class extends Vue {
   align-items: center;
   fill: currentColor;
 
-  & :first-child {
+  &__content {
     margin-right: $layout-02;
+  }
+
+  &__icon {
+    overflow: visible;
+  }
+
+  &_is-external {
+    $arrow-path: "path:nth-child(2)";
+
+    #{$arrow-path} {
+      transform: translate(0, 0);
+      transition: transform 0.3s ease-in-out;
+    }
+
+    &:hover,
+    &:active {
+      #{$arrow-path} {
+        transform: translate(2px, -2px);
+      }
+    }
+  }
+
+  &_is-internal {
+    $arrow-path: "path:nth-child(1)";
+
+    #{$arrow-path} {
+      transform: translate(0, 0);
+      transition: transform 0.2s ease-in-out;
+    }
+
+    &:hover,
+    &:active {
+      #{$arrow-path} {
+        transform: translate(4px, 0);
+      }
+    }
   }
 }
 </style>
