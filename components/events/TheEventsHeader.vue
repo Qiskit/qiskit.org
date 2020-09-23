28041fb9000cf3541e2f1f3b9ce9b9ef64e5dd35
Yaiza <yaiza.garcia.mm@gmail.com>
2020-09-23 18:57:30 +0200

    Implement events header (#949)

---
+++ components/events/TheEventsHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <ThePageHeader class="the-learn-header">
+    Join events from the world's
+    <TypewriterEffect
+      :values="[
+        'largest',
+      ]"
+    />
+    quantum computing community
+  </ThePageHeader>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import ThePageHeader from '~/components/ui/ThePageHeader.vue'
+import TypewriterEffect from '~/components/ui/TypewriterEffect.vue'
+
+@Component({ components: { ThePageHeader, TypewriterEffect } })
+export default class extends Vue { }
+</script>
