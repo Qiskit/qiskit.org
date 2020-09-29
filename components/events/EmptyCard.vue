1d7054696a7870a0e2f7a96e159138b030378093
randytolentino <randytolentino@ibm.com>
2020-09-29 08:19:51 -0600

    add lazy background

--- components/events/EmptyCard.vue
+++ components/events/EmptyCard.vue
@@ -2,8 +2,9 @@
   <article class="empty-card">
     <img
       class="empty-card__image"
-      src="data:image/svg+xml,%3Csvg width='224' height='200' viewBox='0 0 224 200' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Crect width='224' height='200' fill='%23C1C7CD'/%3E%3Cpath d='M112 111.346V81.7308M156 138H68L112 61L156 138Z' stroke='white' stroke-width='2' stroke-linejoin='round'/%3E%3Cpath d='M112 126.154C113.62 126.154 114.933 124.828 114.933 123.192C114.933 121.557 113.62 120.231 112 120.231C110.38 120.231 109.067 121.557 109.067 123.192C109.067 124.828 110.38 126.154 112 126.154Z' fill='white'/%3E%3C/svg%3E%0A"
       alt=""
+      src="data:image/svg+xml,%3Csvg width='224' height='200' viewBox='0 0 224 200' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Crect width='224' height='200' fill='%23C1C7CD'/%3E%3Cpath d='M112 111.346V81.7308M156 138H68L112 61L156 138Z' stroke='white' stroke-width='2' stroke-linejoin='round'/%3E%3Cpath d='M112 126.154C113.62 126.154 114.933 124.828 114.933 123.192C114.933 121.557 113.62 120.231 112 120.231C110.38 120.231 109.067 121.557 109.067 123.192C109.067 124.828 110.38 126.154 112 126.154Z' fill='white'/%3E%3C/svg%3E%0A"
+      :lazy-background="image"
     >
     <div class="empty-card__content">
       <h3 class="empty-card__title">
@@ -24,6 +25,8 @@ import { Component, Prop } from 'vue-property-decorator'
 export default class extends Vue {
   @Prop(String) title
   @Prop(String) description
+
+  image = '/images/events/no-events.svg'
 }
 </script>
 
@@ -48,6 +51,10 @@ export default class extends Vue {
 
   &__image {
     width: 14rem;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position: center;
+    overflow: hidden;
     background-color: $cool-gray-30;
 
     @include mq($from: medium, $until: large) {
@@ -55,6 +62,7 @@ export default class extends Vue {
     }
 
     @include mq($until: medium) {
+      background-size: initial;
       height: 11rem;
       width: auto;
     }
