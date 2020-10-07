87c842a2bfb946cd8c3ffea9421ee52280cd7988
Gregorio <korgan00@gmail.com>
2020-10-07 18:13:56 +0200

    fake ui component

---
+++ components/metal/FakeUI.vue
@@ -0,0 +1,38 @@
+<template>
+  <article class="fake-ui">
+    <div class="fake-ui__wrapper">
+      <img src="images/metal/fake-ui.svg" alt="fake software user interface" class="fake-ui__bg">
+      <div class="fake-ui__content-container">
+        <slot />
+      </div>
+    </div>
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+  .fake-ui {
+    &__wrapper {
+      position: relative;
+      width: 100%;
+    }
+    &__bg {
+      position: relative;
+      width: 100%;
+    }
+    &__content-container {
+      position: absolute;
+      width: 67.4%;
+      top: 15.5%;
+      left: 32.6%;
+      height: 65.3%;
+    }
+  }
+</style>
