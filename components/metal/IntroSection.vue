1f2f9ae5d8de3e76d6b76da6f207085720415e02
y4izus <yaiza.garcia.mm@gmail.com>
2020-10-08 17:45:01 +0200

    Adds video and updates styles

--- components/metal/IntroSection.vue
+++ components/metal/IntroSection.vue
@@ -9,6 +9,15 @@
           Metal’s vision is to develop a community-driven universal toolkit capable of orchestrating quantum chip development from concept to fabrication in a simple, scalable, and open framework. <br><br>
           A first-of-its-kind, open-source project for engineers and scientists to design superconducting quantum device with ease: Qiskit Metal.
         </p>
+        <iframe
+          class="intro-section__video"
+          width="608"
+          height="341"
+          src="https://www.youtube.com/embed/NHTDqdGfzcc"
+          frameborder="0"
+          allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+          allowfullscreen
+        />
       </div>
       <AppCta
         class="intro-section__cta"
@@ -54,21 +63,39 @@ export default class extends Vue {
     }
   }
 
+  &__content {
+    display: flex;
+    flex-direction: row;
+
+    @include mq($until: medium) {
+      flex-direction: column;
+    }
+  }
+
   &__description {
     @include type-style('body-long-01');
-    max-width: 9 * $column-size-large;
+    max-width: 6 * $column-size-large;
     margin-bottom: $layout-06;
+    padding-right: $spacing-07;
 
     @include mq($until: large) {
       margin-bottom: $layout-03;
     }
 
     @include mq($from: medium, $until: large) {
-      max-width: 6 * $column-size-medium;
+      max-width: 4 * $column-size-medium;
     }
 
     @include mq($until: medium) {
-      width: 100%;
+      max-width: 100%;
+    }
+  }
+
+  &__video {
+    width: 100%;
+
+    @include mq($until: medium) {
+      margin-bottom: $layout-03;
     }
   }
 
@@ -77,6 +104,13 @@ export default class extends Vue {
     background-color: $purple-70;
     padding: $spacing-05;
     width: fit-content !important;
+    position: relative;
+    top: -3.2rem;
+
+    @include mq($until: large) {
+      top: 0;
+      margin-top: $layout-03;
+    }
   }
 }
 </style>
