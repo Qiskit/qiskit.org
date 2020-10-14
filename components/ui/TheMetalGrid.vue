eca02f33b9d4e13a9c9397e5f45b07fc69a99648
Gregorio <korgan00@gmail.com>
2020-10-14 18:18:22 +0200

    Added transitions and some variables for tweak behaviors

--- components/ui/TheMetalGrid.vue
+++ components/ui/TheMetalGrid.vue
@@ -61,6 +61,10 @@ import AppIcon from '~/components/ui/AppIcon.vue'
   components: { AppIcon }
 })
 export default class extends Vue {
+  timeToRemoveCell = 10 // in ms
+  timeToLoadMetal = 1000 // in ms
+  triggerPositionFromTopCenter = { x: -3, y: 2 } // y: 0 is the first line
+
   width: number = 64
   height: number = 64
 
@@ -141,7 +145,6 @@ export default class extends Vue {
       }
     }
 
-    // columnWidth = columnCount * this.width
     return genRows()
   })())
 
@@ -171,7 +174,7 @@ export default class extends Vue {
 
   isTrigger (pos) {
     const centralColumn = Math.floor(this.columnCount / 2)
-    return pos.c === centralColumn - 3 && pos.r === 2
+    return pos.c === centralColumn + this.triggerPositionFromTopCenter.x && pos.r === this.triggerPositionFromTopCenter.y
   }
 
   triggerAnimation (pos) {
@@ -184,14 +187,14 @@ export default class extends Vue {
     const length = this.visibleCells.length
     if (!length) {
       setTimeout(() => {
-        // this.$router.push({ path: '/metal' })
-      }, 1000)
+        this.$router.push({ path: '/metal' })
+      }, this.timeToLoadMetal)
       return
     }
     const index = Math.floor(Math.random() * length)
     const posId = this.visibleCells.splice(index, 1)[0]
     this.hiddenCells.splice(0, 0, posId)
-    setTimeout(this.removeCell, 30)
+    setTimeout(this.removeCell, this.timeToRemoveCell)
   }
 
   posId (pos) {
@@ -243,6 +246,10 @@ $medium-cell: 40px;
       font-size: 1.5rem;
       margin-left: 10rem;
     }
+    @include mq($until: medium) {
+      font-size: 1rem;
+      margin-left: 6rem;
+    }
   }
 
   &__logo {
@@ -259,6 +266,11 @@ $medium-cell: 40px;
       height: 8rem;
       margin-right: 2rem;
     }
+    @include mq($until: medium) {
+      width: 5rem;
+      height: 5rem;
+      margin-right: 1rem;
+    }
   }
 
   &__title {
@@ -274,6 +286,10 @@ $medium-cell: 40px;
     @include mq($until: large) {
       line-height: 6rem;
       font-size: 6rem;
+    }
+    @include mq($until: medium) {
+      line-height: 4rem;
+      font-size: 4rem;
     }
   }
 
@@ -316,8 +332,9 @@ $medium-cell: 40px;
       width: $large-cell - 1px;
       height: $large-cell - 1px;
       background-color: white;
-      transition: transform 700ms ease-in, opacity 700ms, border-radius 700ms;
       box-sizing: content-box;
+      // Transition speed and function
+      transition: transform 700ms ease-in, opacity 700ms, border-radius 700ms;
 
       @include mq($until: large) {
         width: $medium-cell - 1px;
@@ -325,22 +342,100 @@ $medium-cell: 40px;
       }
     }
 
+    // This is the final state of each tile
+    // if you want to change the speed look at 9 lines above
     &_hidden::before {
-      // transform: translateY(150%);
+      /* // falling transition
+      transform: translateY(150%);
+      */
+
+      // semi-rounded scale and rotate
+      transform: scale(0.3, 0.3) rotate(45deg);
+      border-radius: 20%;
+      /*/
+      // fully-rounded and scale
       transform: scale(0.3, 0.3);
-      border-radius: 100%;
+      border-radius: 20%;
+      //*/
+
+      // Common CSS for all transitions
       opacity: 0;
     }
 
+    // NO EFFECT ON HOLE
+    /*
     &_trigger::before {
       border: none;
       background-color: transparent;
       cursor: pointer;
-      /*
-      background-color: $cool-gray-90;
-      box-shadow: inset 0px 0px 20px 2px black, inset 0px 0px 10px 1px black;
-      */
       z-index: 200;
+    }
+    */
+
+    // PUMPING BACKGROUND HOLE EFFECT
+    /*
+    @keyframes pumpingBackground {
+      0%   { background-color: $cool-gray-80; }
+      30%  { background-color: $cool-gray-70; }
+      100% { background-color: $cool-gray-80; }
+    }
+
+    &_trigger::before {
+      border: none;
+      cursor: pointer;
+      z-index: 200;
+
+      background-color: $cool-gray-80;
+      box-shadow: inset 0px 0px 20px 2px black, inset 0px 0px 10px 1px black;
+
+      animation: pumpingBackground 1.5s linear 0s infinite normal;
+    }
+    */
+
+    // ANXIOUS TILE EFFECT
+    // this variables can be changed to tweak the effect.
+    // also you can change the degrees directly.
+    $anxiousTile_RotationAmount: 1;
+    $veryAnxiousTile_Scaling: 1.1;
+    @keyframes anxiousTile {
+      0%    { box-shadow: 0px 0px 0px 0px $cool-gray-70; transform: rotate( 0deg); }
+      10%   { box-shadow: 0px 1px 5px 0px $cool-gray-20; transform: rotate( 0deg); }
+      18%   { box-shadow: 0px 1px 5px 0px $cool-gray-20; transform: rotate(-3deg * $anxiousTile_RotationAmount); }
+      26%   { box-shadow: 0px 1px 5px 0px $cool-gray-20; transform: rotate( 4deg * $anxiousTile_RotationAmount); }
+      34%   { box-shadow: 0px 1px 5px 0px $cool-gray-20; transform: rotate(-3deg * $anxiousTile_RotationAmount); }
+      45%   { box-shadow: 0px 1px 5px 0px $cool-gray-20; transform: rotate( 1deg * $anxiousTile_RotationAmount); }
+      55%   { box-shadow: 0px 1px 5px 0px $cool-gray-20; transform: rotate( 0deg); }
+      65%   { box-shadow: 0px 0px 0px 0px $cool-gray-60; transform: rotate( 0deg); }
+      100%  { box-shadow: 0px 0px 0px 0px $cool-gray-70; transform: rotate( 0deg); }
+    }
+    @keyframes veryAnxiousTile {
+      0%    { box-shadow: 0px 0px 0px 0px $cool-gray-70; transform: rotate( 0deg) scale(1);  }
+      10%   { box-shadow: 0px 1px 5px 0px $cool-gray-20; transform: rotate( 0deg) scale($veryAnxiousTile_Scaling); }
+      18%   { box-shadow: 0px 1px 5px 0px $cool-gray-20; transform: rotate(-5deg * $anxiousTile_RotationAmount) scale($veryAnxiousTile_Scaling); }
+      26%   { box-shadow: 0px 1px 5px 0px $cool-gray-20; transform: rotate( 7deg * $anxiousTile_RotationAmount) scale($veryAnxiousTile_Scaling); }
+      34%   { box-shadow: 0px 1px 5px 0px $cool-gray-20; transform: rotate(-5deg * $anxiousTile_RotationAmount) scale($veryAnxiousTile_Scaling); }
+      45%   { box-shadow: 0px 1px 5px 0px $cool-gray-20; transform: rotate( 3deg * $anxiousTile_RotationAmount) scale($veryAnxiousTile_Scaling); }
+      55%   { box-shadow: 0px 1px 5px 0px $cool-gray-20; transform: rotate( 0deg) scale($veryAnxiousTile_Scaling); }
+      65%   { box-shadow: 0px 0px 0px 0px $cool-gray-60; transform: rotate( 0deg) scale(1); }
+      100%  { box-shadow: 0px 0px 0px 0px $cool-gray-70; transform: rotate( 0deg) scale(1); }
+    }
+    &_trigger{
+      &::before {
+        cursor: pointer;
+        z-index: 200;
+        animation: anxiousTile 2s linear 0s infinite normal;
+
+        @include mq($until: medium) { // Mobile animation
+          animation: veryAnxiousTile 2s linear 0s infinite normal;
+        }
+      }
+      &:hover::before {
+        animation: veryAnxiousTile 2s linear 0s infinite normal;
+      }
+      &_triggered::before,
+      &_triggered:hover::before {
+        animation: none;
+      }
     }
 
     &_decoherent::before {
