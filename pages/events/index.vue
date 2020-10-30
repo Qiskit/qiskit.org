cff3d5d2cf1a94341ae78050f13795b20d7c3c28
y4izus <yaiza.garcia.mm@gmail.com>
2020-10-30 12:07:00 +0100

    Fixes key attribute for EventCard for loop

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -79,7 +79,7 @@
             <EventCard
               v-for="event in filteredEvents"
               v-else
-              :key="`${event.place}-${event.date}`"
+              :key="`${event.title}-${event.place}-${event.date}`"
               :types="event.types"
               :title="event.title"
               :image="event.image"
