eb2680a2f8308707f6475610c4d394eb94810aa4
Randy <randytolentino@ibm.com>
2020-11-04 05:07:19 -0700

    Adds Advocates page header (#1078)

---
+++ components/advocates/TheAdvocatesHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <ThePageHeader>
+    Connect with the
+    <TypewriterEffect
+      :values="[
+        'enthusiasts',
+        'advocates',
+        'mentors',
+        'experts'
+      ]"
+    /><br>
+    from within the Qiskit community
+  </ThePageHeader>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import ThePageHeader from '~/components/ui/ThePageHeader.vue'
+import TypewriterEffect from '~/components/ui/TypewriterEffect.vue'
+
+@Component({ components: { ThePageHeader, TypewriterEffect } })
+export default class extends Vue { }
+</script>
