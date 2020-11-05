c8bb5ea274d622a6e99b82f901eb1cd44489b2e2
y4izus <yaiza.garcia.mm@gmail.com>
2020-11-05 11:30:16 +0100

    Create AppCheckbox component

---
+++ components/ui/AppCheckbox.vue
@@ -0,0 +1,30 @@
+<template>
+  <cv-checkbox
+    class="app-checkbox"
+    :value="value"
+    :label="label"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class extends Vue {
+  @Prop(String) value!: string
+  @Prop(String) label!: string
+}
+</script>
+
+<style lang="scss">
+.app-checkbox {
+  .bx--checkbox-label::before {
+    border: 1px solid $black-100;
+  }
+
+  .bx--checkbox:focus + .bx--checkbox-label::before {
+    box-shadow: 0 0 0 2px $white, 0 0 0 4px $purple-60;
+  }
+}
+</style>
