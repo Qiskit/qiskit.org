014852d1f0e6bbbfc12383004f12c3812bf77fd0
y4izus <yaiza.garcia.mm@gmail.com>
2020-11-05 13:50:29 +0100

    Refactor and clean code

--- components/advocates/MeetTheAdvocates.vue
+++ components/advocates/MeetTheAdvocates.vue
@@ -1,50 +1,42 @@
 <template>
   <div class="meet-the-advocates">
-    <div class="meet-the-advocates__container">
-      <h2 class="copy__title">
-        Meet the Advocates
-      </h2>
-      <p class="copy__paragraph copy__paragraph_importance_support">
-        Qiskit advocates are some of the finest minds in quantum computing,
-        all over the world. If you are interested in getting involved with the
-        quantum computing community, reach out to an advocate local to your area.
-      </p>
-      <div
-        class="meet-the-advocates__extra-filters meet-the-advocates__extra-filters_on-small-screen"
-      >
-        <AppMultiSelect
-          :label="extraFilter.label"
-          :options="extraFilter.options"
+    <h2 class="copy__title">
+      Meet the Advocates
+    </h2>
+    <p class="copy__paragraph copy__paragraph_importance_support">
+      Qiskit advocates are some of the finest minds in quantum computing,
+      all over the world. If you are interested in getting involved with the
+      quantum computing community, reach out to an advocate local to your area.
+    </p>
+    <AppFiltersResultsLayout>
+      <template slot="filters-on-m-l-screen">
+        <AppFieldset :label="filter.label">
+          <client-only>
+            <AppCheckbox
+              v-for="option in filter.options"
+              :key="option.label"
+              v-bind="option"
+            />
+          </client-only>
+        </AppFieldset>
+      </template>
+      <template slot="filters-on-s-screen">
+        <AppMultiSelect v-bind="filter.label" />
+      </template>
+      <template slot="results">
+        <AdvocateCard
+          v-for="advocate in advocates"
+          :key="`advocate-${advocate.attributes.name}`"
+          v-bind="advocate.attributes"
         />
-      </div>
-      <AppFiltersResultsLayout>
-        <template slot="filters">
-          <AppFieldset :label="extraFilter.label">
-            <client-only>
-              <AppCheckbox
-                v-for="option in extraFilter.options"
-                :key="option.label"
-                v-bind="option"
-              />
-            </client-only>
-          </AppFieldset>
-        </template>
-        <template slot="results">
-          <AdvocateCard
-            v-for="advocate in advocates"
-            :key="`advocate-${advocate.attributes.name}`"
-            v-bind="advocate.attributes"
-          />
-        </template>
-      </AppFiltersResultsLayout>
-    </div>
+      </template>
+    </AppFiltersResultsLayout>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { Component, Prop } from 'vue-property-decorator'
-import AppCard from '~/components/ui/AppCard.vue'
 import AdvocateCard from '~/components/advocates/AdvocateCard.vue'
 import AppMultiSelect from '~/components/ui/AppMultiSelect.vue'
 import AppFieldset from '~/components/ui/AppFieldset.vue'
@@ -60,7 +52,6 @@ type multiSelectOption = {
 @Component({
   components: {
     AdvocateCard,
-    AppCard,
     AppMultiSelect,
     AppFieldset,
     AppCheckbox,
@@ -71,17 +62,12 @@ export default class extends Vue {
   @Prop(Array) advocates!: any
 
   regions = ['Europe', 'Asia', 'Africa', 'America']
-  emptyCard = {
-    title: 'No people found',
-    description: 'No Qiskit advocates found for the given criteria. Trying doing a broader search. There are literally dozens of us.',
-    img: '/images/events/no-events.svg'
-  }
 
   regionsOptions = this.getOptions(this.regions)
   regionsLabel: string = 'Locations'
   regionsFilters: string = 'regionFilters'
 
-  extraFilter = {
+  filter = {
     label: this.regionsLabel,
     options: this.regionsOptions,
     filterType: this.regionsFilters
@@ -92,30 +78,3 @@ export default class extends Vue {
   }
 }
 </script>
-
-<style lang="scss">
-@import '~carbon-components/scss/globals/scss/typography';
-
-.meet-the-advocates {
-  background-color: $white;
-  color: $white-text-01;
-
-  &__container {
-    @include contained();
-  }
-
-  &__extra-filters {
-    &_on-large-screen {
-      @include mq($until: medium) {
-        display: none;
-      }
-    }
-
-    &_on-small-screen {
-      @include mq($from: medium) {
-        display: none;
-      }
-    }
-  }
-}
-</style>
