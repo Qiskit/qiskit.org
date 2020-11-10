a2f375ebea651b0a707737df7e784e998b481ad9
y4izus <yaiza.garcia.mm@gmail.com>
2020-11-10 12:19:04 +0100

    Delete unnecesary code and files

--- content/advocates/index/kifumi-numata.md
+++
@@ -1,8 +0,0 @@
----
-name: Kifumi Numata
-image: kifumi-numata.png
-location: Tokyo, Japan
-latitude: 35.68
-longitude: 139.69
-areas: Tutorials
----
--- hooks/update-advocates.ts
+++ hooks/update-advocates.ts
@@ -5,8 +5,6 @@ import { fetchAdvocates } from './advocates-conversion-utils'
 
 export default async function (apiKey: any, outputFolder: string) {
   const advocates = await fetchAdvocates(apiKey)
-  console.log('apiKey = ', apiKey)
-  console.log('advocates = ', advocates)
 
   const writeFile = util.promisify(fs.writeFile)
   await writeFile(`${outputFolder}/advocates.json`, JSON.stringify(advocates, null, 2))
