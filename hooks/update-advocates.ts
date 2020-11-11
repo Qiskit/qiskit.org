a8ac7231c3e807f5fd99b05e5c3916e7e9b8efce
y4izus <yaiza.garcia.mm@gmail.com>
2020-11-11 11:46:02 +0100

    Renames file

--- hooks/update-advocates.ts
+++ hooks/update-advocates.ts
@@ -1,7 +1,7 @@
 import fs from 'fs'
 import util from 'util'
 
-import { fetchAdvocates } from './advocates-conversion-utils'
+import { fetchAdvocates } from './advocate-conversion-utils'
 
 export default async function (apiKey: any, outputFolder: string) {
   const advocates = await fetchAdvocates(apiKey)
