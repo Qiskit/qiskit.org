afc87e28aa3c3caaa890c855645fe1c9421efcbe
y4izus <yaiza.garcia.mm@gmail.com>
2020-11-11 12:30:28 +0100

    Shows new advocates card

--- components/advocates/AdvocateCard.vue
+++ components/advocates/AdvocateCard.vue
@@ -1,45 +1,37 @@
 <template>
-  <article class="advocate-card">
-    <div
-      class="advocate-card__picture"
-      :style="`background-image: ${decorate(image)};`"
-    />
-    <div class="advocate-card__copy">
-      <h3 class="advocate-card__name">
-        {{ name }}
-      </h3>
+  <AppCard
+    class="advocate-card"
+    :image="image"
+    :title="name"
+    :tags="[region]"
+  >
+    <div>
       <p class="advocate-card__location">
+        <Map20 class="advocate-card__icon" />
         {{ location }}
       </p>
-      <h3 class="advocate-card__focus-areas-title">
-        Focus areas
-      </h3>
-      <p class="advocate-card__focus-areas">
-        {{ areas }}
+      <p class="advocate-card__contact">
+        <Calendar20 class="advocate-card__icon" />
+        <time>{{ slackId }}</time>
       </p>
     </div>
-  </article>
+  </AppCard>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { Component, Prop } from 'vue-property-decorator'
+import AppCard from '~/components/ui/AppCard.vue'
 
-@Component
+@Component({
+  components: { AppCard }
+})
 export default class extends Vue {
   @Prop(String) name!: any
   @Prop(String) image!: any
   @Prop(String) location!: any
-  @Prop(String) areas!: any
-
-  decorate (image: any) {
-    const bgEffects = [
-      'linear-gradient(170deg, #0000 0%, #0000 90%, var(--component-bg) 90.3%)',
-      'linear-gradient(-170deg, #0000 0%, #0000 90%, var(--component-bg) 90.3%)',
-      `url(/images/advocates/${image})`
-    ]
-    return bgEffects.join(', ')
-  }
+  @Prop(String) region!: any
+  @Prop(String) slackId!: any
 }
 </script>
 
@@ -47,33 +39,25 @@ export default class extends Vue {
 @import '~carbon-components/scss/globals/scss/typography';
 
 .advocate-card {
-  --component-bg: #{$ui-01};
+  margin-bottom: $layout-02;
 
-  text-align: center;
-  border: 1px solid $focus;
-  background-color: var(--component-bg);
-
-  &__picture {
-    height: 350px;
-    background-repeat: no-repeat;
-    background-size: cover, cover, cover;
-    background-position: top center;
+  @include mq($until: large) {
+    margin-bottom: $layout-01;
   }
 
-  &__copy {
-    margin: $spacing-05 $spacing-07 $spacing-05;
+  &__location, &__contact {
+    @include type-style('body-long-01');
+    display: flex;
+    align-items: center;
   }
 
-  &__name, &__focus-areas-title {
-    @include type-style('productive-heading-04');
+  &__location {
+    margin-bottom: $spacing-03;
   }
 
-  &__focus-areas-title {
-    margin-top: $spacing-06;
-  }
-
-  &__location, &__focus-areas {
-    @include type-style('body-short-02');
+  &__icon {
+    margin-right: $spacing-03;
+    fill: currentColor;
   }
 }
 </style>
