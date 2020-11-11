b2d99b32ce7e2ca91f0310695894e5c7b218cb67
y4izus <yaiza.garcia.mm@gmail.com>
2020-11-11 12:55:39 +0100

    Update styles

--- components/advocates/AdvocateCard.vue
+++ components/advocates/AdvocateCard.vue
@@ -5,14 +5,14 @@
     :title="name"
     :tags="[region]"
   >
-    <div>
+    <div class="advocate-card__content">
       <p class="advocate-card__location">
         <Map20 class="advocate-card__icon" />
         {{ location }}
       </p>
       <p class="advocate-card__contact">
-        <Calendar20 class="advocate-card__icon" />
-        <time>{{ slackId }}</time>
+        <LogoSlack20 class="advocate-card__icon" />
+        {{ slackId }}
       </p>
     </div>
   </AppCard>
@@ -36,17 +36,32 @@ export default class extends Vue {
 </script>
 
 <style lang="scss" scoped>
+@import '~carbon-components/scss/globals/scss/typography';
+
 .advocate-card {
   margin-bottom: $layout-02;
+  position: relative;
 
   @include mq($until: large) {
     margin-bottom: $layout-01;
+  }
+
+  &__content {
+    position: absolute;
+    margin-top: $layout-02;
+    bottom: $layout-02;
+
+    @include mq($until: medium) {
+      position: relative;
+      margin-bottom: 0;
+    }
   }
 
   &__location, &__contact {
     @include type-style('body-long-01');
     display: flex;
     align-items: center;
+    width: max-content;
   }
 
   &__location {
