2744268c598a74474dd30bd1e9f2aad8362df94c
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2020-12-08 01:56:23 +0100

    refactor: simplify filters state
    Make the store the single source of truth and avoid creating a local state for the filters.
    Handle the logic to update the filters in the component a commit mutation from there.

--- components/advocates/MeetTheAdvocates.vue
+++ components/advocates/MeetTheAdvocates.vue
@@ -20,10 +20,10 @@
             <cv-checkbox
               v-for="option in filter.options"
               :key="option"
-              v-model="regionFilters"
+              :checked="isRegionFilterChecked(option)"
               :label="option"
               :value="option"
-              @change="setRegionFilters(regionFilters)"
+              @change="updateRegionFilter(option, $event)"
             />
           </client-only>
         </AppFieldset>
@@ -33,7 +33,7 @@
           :label="filter.label"
           :options="filter.options"
           :value="regionFilters"
-          @change-selection="setRegionFilters($event)"
+          @change-selection="updateRegionFilters($event)"
         />
       </template>
       <template slot="results">
@@ -53,7 +53,7 @@
 <script lang="ts">
 import Vue from 'vue'
 import { mapState, MapperForStateWithNamespace } from 'vuex'
-import { Component, Prop, Watch } from 'vue-property-decorator'
+import { Component, Prop } from 'vue-property-decorator'
 import AdvocateCard from '~/components/advocates/AdvocateCard.vue'
 import AppMultiSelect from '~/components/ui/AppMultiSelect.vue'
 import AppFieldset from '~/components/ui/AppFieldset.vue'
@@ -74,7 +74,7 @@ import { Advocate, ADVOCATES_WORLD_REGION_OPTIONS, State } from '~/store/modules
 
   computed: {
     ...mapState<MapperForStateWithNamespace>('advocates', {
-      regionFiltersFromStore: (state: State) => state.regionFilters
+      regionFilters: (state: State) => state.regionFilters
     })
   }
 })
@@ -86,39 +86,28 @@ export default class MeetTheAdvocates extends Vue {
    *
    * Initialized with mapState.
    */
-  public regionFiltersFromStore!: string[]
-
-  /**
-   * Region filters.
-   *
-   * Reflects the component local state of the region filters, which syncs with
-   * the region filters state from the Vuex store.
-   *
-   * This variable is necessary in order to bind its value to `cv-checkbox`. If
-   * we were to bind the region filters value from the Vuex store directly, it
-   * would result in store mutations when the active elements in the checkbox
-   * group change.
-   */
-  private regionFilters: string[] = [];
+  public regionFilters!: string[]
 
   private filter = {
     label: 'Locations',
     options: ADVOCATES_WORLD_REGION_OPTIONS
   }
 
-  mounted () {
-    this.regionFilters = this.regionFiltersFromStore
+  isRegionFilterChecked (filterValue: string): boolean {
+    return this.regionFilters.includes(filterValue)
   }
 
-  @Watch('regionFiltersFromStore')
-  onRegionFiltersFromStoreChanged (regionFiltersFromStore: string[]) {
-    this.regionFilters = regionFiltersFromStore
+  updateRegionFilter (option: string, isChecked: boolean): void {
+    const regionFilters = this.regionFilters.filter(oldOption => oldOption !== option)
+
+    if (isChecked) {
+      regionFilters.push(option)
+    }
+
+    this.updateRegionFilters(regionFilters)
   }
 
-  /**
-   * Updates the region filters in the Vuex store.
-   */
-  setRegionFilters (regionFilters: string[]): void {
+  updateRegionFilters (regionFilters: string[]): void {
     this.$store.commit('advocates/setRegionFilters', regionFilters)
   }
 
