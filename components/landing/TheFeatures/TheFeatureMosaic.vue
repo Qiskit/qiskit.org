4ea5cd41a0ee5b33ca7b4b899d79523ea1afc42c
y4izus <yaiza.garcia.mm@gmail.com>
2021-01-08 16:45:23 +0100

    Reorder scss

--- components/landing/TheFeatures/TheFeatureMosaic.vue
+++ components/landing/TheFeatures/TheFeatureMosaic.vue
@@ -146,8 +146,88 @@ export default class TheFeatureMosaic extends Vue {}
       height: 18.75rem;
     }
 
+    &_first {
+      grid-area: a;
+    }
+
+    &_second {
+      grid-area: b;
+      flex-direction: column-reverse;
+    }
+
+    &_third {
+      grid-area: c;
+
+      @include mq($from: medium, $until: large) {
+        display: grid;
+        grid-template-columns: 2fr 3fr;
+        gap: $spacing-07;
+      }
+
+      @include mq($until: medium) {
+        display: flex;
+        flex-direction: column-reverse;
+      }
+    }
+
+    &_fourth {
+      grid-area: d;
+      flex-direction: row;
+      padding-bottom: $spacing-09;
+
+      @include mq($until: medium) {
+        flex-direction: column;
+      }
+    }
+
     &-decoration {
       flex: 1 0 0;
+
+      &_first {
+        background-position: right bottom;
+        background-size: contain;
+        background-repeat: no-repeat;
+      }
+
+      &_second {
+        background-position: center top;
+        background-size: cover;
+        background-repeat: no-repeat;
+        min-height: 15rem;
+
+        @include mq($from: medium, $until: large) {
+          min-height: 10rem;
+        }
+
+        @include mq($until: medium) {
+          min-height: 4rem;
+        }
+      }
+
+      &_third {
+        background-position: center bottom;
+        background-size: cover;
+        background-repeat: no-repeat;
+        height: 25rem;
+
+        @include mq($from: medium, $until: large) {
+          height: auto;
+        }
+
+        @include mq($until: medium) {
+          min-height: 4rem;
+        }
+      }
+
+      &_fourth {
+        background-position: right bottom;
+        background-size: 12rem auto;
+        background-repeat: no-repeat;
+
+        @include mq($until: large) {
+          background-size: 10rem auto;
+        }
+      }
     }
 
     &-copy {
@@ -158,88 +238,9 @@ export default class TheFeatureMosaic extends Vue {}
       }
     }
   }
+}
 
-  &__feature_first {
-    grid-area: a;
-  }
-
-  &__feature-decoration_first {
-    background-position: right bottom;
-    background-size: contain;
-    background-repeat: no-repeat;
-
-    @include mq($until: large) {
-      background-size: 17rem auto;
-    }
-  }
-
-  &__feature_second {
-    grid-area: b;
-    flex-direction: column-reverse;
-  }
-
-  &__feature-decoration_second {
-    background-position: center top;
-    background-size: cover;
-    background-repeat: no-repeat;
-    min-height: 15rem;
-
-    @include mq($from: medium, $until: large) {
-      min-height: 10rem;
-    }
-
-    @include mq($until: medium) {
-      min-height: 4rem;
-    }
-  }
-
-  &__feature_third {
-    grid-area: c;
-
-    @include mq($from: medium, $until: large) {
-      display: grid;
-      grid-template-columns: 2fr 3fr;
-      gap: $spacing-07;
-    }
-
-    @include mq($until: medium) {
-      display: flex;
-      flex-direction: column-reverse;
-    }
-  }
-
-  &__feature-decoration_third {
-    background-position: center bottom;
-    background-size: cover;
-    background-repeat: no-repeat;
-    height: 25rem;
-
-    @include mq($from: medium, $until: large) {
-      height: auto;
-    }
-
-    @include mq($until: medium) {
-      min-height: 4rem;
-    }
-  }
-
-  &__feature_fourth {
-    flex-direction: row;
-    grid-area: d;
-
-    @include mq($until: medium) {
-      flex-direction: column;
-    }
-  }
-
-  &__feature-decoration_fourth {
-    background-position: right bottom;
-    background-size: 12rem auto;
-    background-repeat: no-repeat;
-
-    @include mq($until: large) {
-      background-size: 10rem auto;
-    }
-  }
+.copy__paragraph_importance_outstanding {
+  margin-bottom: 0;
 }
 </style>
