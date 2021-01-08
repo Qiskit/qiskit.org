cc27105b2d24e60a1db1a323f36154ae89316235
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-01-08 12:06:44 +0100

    create: base Seminar Series page

--- pages/events/seminar-series.vue
+++ pages/events/seminar-series.vue
@@ -5,17 +5,17 @@
 </template>
 
 <script lang="ts">
-import { Component } from 'vue-property-decorator'
-import QiskitPage from '~/components/logic/QiskitPage.vue'
+import { Component } from "vue-property-decorator";
+import QiskitPage from "~/components/logic/QiskitPage.vue";
 
 @Component({
-  head () {
+  head() {
     return {
-      title: 'Qiskit Seminar Series'
-    }
-  }
+      title: "Qiskit Seminar Series",
+    };
+  },
 })
 export default class SeminarSeriesPage extends QiskitPage {
-  routeName = 'seminar-series';
+  routeName = "seminar-series";
 }
 </script>
