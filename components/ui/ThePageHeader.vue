84f4c23674147721b68e6b5db3aa5fd98767e144
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-01-11 15:20:13 +0100

    feat: display aside content in ThePageHeader

--- components/ui/ThePageHeader.vue
+++ components/ui/ThePageHeader.vue
@@ -1,13 +1,18 @@
 <template>
   <header class="page-header">
     <div class="page-header__container">
-      <h1 class="page-header__title">
-        <slot />
-      </h1>
-      <div v-if="this.$slots.description" class="page-header__description">
-        <slot name="description" />
+      <div class="page-header__main">
+        <h1 class="page-header__title">
+          <slot />
+        </h1>
+        <div v-if="this.$slots.description" class="page-header__description">
+          <slot name="description" />
+        </div>
+        <AppCta v-if="cta" v-bind="cta" class="page-header__cta" />
       </div>
-      <AppCta v-if="cta" v-bind="cta" class="page-header__cta" />
+      <div v-if="this.$slots.aside" class="page-header__aside">
+        <slot name="aside" />
+      </div>
     </div>
   </header>
 </template>
@@ -34,15 +39,26 @@ export default class ThePageHeader extends Vue {
     min-height: 28rem * 40 / 64;
   }
 
+  &__aside {
+    align-self: start;
+    max-width: 3 * $column-size-large;
+  }
+
   &__container {
     @include contained();
+    align-items: center;
     display: flex;
-    flex-direction: column;
-    justify-content: center;
+    flex-direction: row;
     min-height: 28rem;
 
     @include mq($until: large) {
       min-height: 28rem * 40 / 64;
+    }
+
+    @include mq($until: medium) {
+      align-items: initial;
+      flex-direction: column;
+      justify-content: center;
     }
   }
 
@@ -60,9 +76,7 @@ export default class ThePageHeader extends Vue {
     }
   }
 
-  &__title {
-    @include type-style('expressive-heading-05', true);
-    color: $white-text-01;
+  &__main {
     max-width: 10 * $column-size-large;
 
     @include mq($from: medium, $until: large) {
@@ -72,6 +86,11 @@ export default class ThePageHeader extends Vue {
     @include mq($until: medium) {
       max-width: 5 * $column-size-medium;
     }
+  }
+
+  &__title {
+    @include type-style('expressive-heading-05', true);
+    color: $white-text-01;
   }
 }
 </style>
