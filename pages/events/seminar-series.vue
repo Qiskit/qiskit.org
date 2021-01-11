4cb61cfa58a59e86383e4a340b888b0d8d68dd12
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-01-11 15:28:52 +0100

    Create base Seminar Series page (#1283)

---
+++ pages/events/seminar-series.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <!-- TODO: Add Seminar Series content -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import QiskitPage from '~/components/logic/QiskitPage.vue'
+
+@Component({
+  head () {
+    return {
+      title: 'Qiskit Seminar Series'
+    }
+  }
+})
+export default class SeminarSeriesPage extends QiskitPage {
+  routeName = 'seminar-series';
+}
+</script>
