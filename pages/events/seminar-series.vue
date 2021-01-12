6f37094d1388ef1b0acf337855d64dc7ac80bd9e
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-01-12 14:08:49 +0100

    feat: update mosaic images

--- pages/events/seminar-series.vue
+++ pages/events/seminar-series.vue
@@ -29,28 +29,28 @@ export default class SeminarSeriesPage extends QiskitPage {
       title: 'Learn from experts',
       description:
         'This series is a deep dive into various academic and research topics within the quantum community.',
-      image: '/images/library/circuit-01.png'
+      image: '/images/events/seminar-series/mosaic-experts.png'
     },
     {
       position: 'second',
       title: 'About the host',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: '/images/landing-page/feature-hardware.jpg'
+      image: '/images/events/seminar-series/mosaic-host.jpg'
     },
     {
       position: 'third',
       title: 'About the speakers',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: '/images/landing-page/feature-quantum-algorithms.png'
+      image: '/images/events/seminar-series/mosaic-speakers.jpg'
     },
     {
       position: 'fourth',
       title: 'Interactivity headline',
       description:
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: '/images/landing-page/feature-noise-mitigation.png'
+      image: '/images/events/seminar-series/mosaic-interactivity.png'
     }
   ];
 }
