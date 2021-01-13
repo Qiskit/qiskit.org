ac862097b579792c706c15dd436a61ef327e56bd
y4izus <yaiza.garcia.mm@gmail.com>
2021-01-13 17:34:03 +0100

    Uses AppDescriptionCard on TheCarefulExplanationForBeginners

--- components/learn/TheCarefulExplanationForBeginners.vue
+++ components/learn/TheCarefulExplanationForBeginners.vue
@@ -1,12 +1,7 @@
 <template>
   <CarefulExplanation v-bind="$attrs">
     <template #summary>
-      <h2 class="copy__subtitle">
-        A Carefully Worded Summary of Quantum Computing for Beginners
-      </h2>
-      <p class="copy__paragraph">
-        Physics is an attempt to describe the behaviour of the world around us. Around the early 20th century, through delicate experiments in laboratories, physicists saw strange behaviours which could not be explained by their current understanding of physics. These discoveries led to the development of the more complete ‘quantum’ physics, which was able to describe this behaviour very successfully.
-      </p>
+      <AppDescriptionCard :title="title" :description="preliminarDescription" />
     </template>
     <p class="copy__paragraph">
       Quantum physics explained behaviour previously thought impossible, and it was later discovered that this could be exploited to solve specific computational problems that were also previously thought impossible. Since this behaviour occurs only in delicate laboratory experiments, performing these new computations require delicate machines known as quantum computers. We are still learning how to build quantum computers, and it will be a while before they are large and stable enough to be useful, but progress is promising and the science is fascinating.
@@ -22,7 +17,10 @@ import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 
 @Component
-export default class TheCarefulExplanationForBeginners extends Vue {}
+export default class TheCarefulExplanationForBeginners extends Vue {
+  title = 'A Carefully Worded Summary of Quantum Computing for Beginners'
+  preliminarDescription = 'Physics is an attempt to describe the behaviour of the world around us. Around the early 20th century, through delicate experiments in laboratories, physicists saw strange behaviours which could not be explained by their current understanding of physics. These discoveries led to the development of the more complete ‘quantum’ physics, which was able to describe this behaviour very successfully.'
+}
 </script>
 
 <style lang="scss" scoped>
