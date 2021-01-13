45068797621903bef135042b32909baef95985b6
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-01-13 11:47:57 +0100

    Create "what is this event" section for Seminar Series (#1301)

---
+++ components/events/seminar-series/WhatIsThisEventSection.vue
@@ -0,0 +1,52 @@
+<template>
+  <section>
+    <h2 class="copy__title" v-text="title" />
+    <AppMosaic :mosaic-elements="mosaicElements" />
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import { MosaicElement } from '~/components/ui/AppMosaic.vue'
+
+@Component
+export default class WhatIsThisEventSection extends Vue {
+  title = 'What is the Quantum Information Science Seminar Series?';
+
+  mosaicElements: MosaicElement[] = [
+    {
+      position: 'first',
+      title: 'Learn from experts',
+      description:
+        'This series is a deep dive into various academic and research topics within the quantum community.',
+      image: '/images/events/seminar-series/mosaic-experts.png'
+    },
+    {
+      position: 'second',
+      title: 'About the host',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+      image: '/images/events/seminar-series/mosaic-host.jpg'
+    },
+    {
+      position: 'third',
+      title: 'About the speakers',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+      image: '/images/events/seminar-series/mosaic-speakers.jpg'
+    },
+    {
+      position: 'fourth',
+      title: 'Interactivity headline',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+      image: '/images/events/seminar-series/mosaic-interactivity.png'
+    }
+  ];
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/blocks/copy.scss";
+</style>
