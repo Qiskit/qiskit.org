0352c8c53a9ac09e7b234ede820e20af08e7e8cc
Yaiza <yaiza.garcia.mm@gmail.com>
2021-01-15 12:14:44 +0100

    Create seminar series data table component (#1305)

--- pages/events/seminar-series.vue
+++ pages/events/seminar-series.vue
@@ -1,6 +1,7 @@
 <template>
   <main class="seminar-series-page">
     <WhatIsThisEventSection class="seminar-series-page__section" />
+    <UpcomingSeminarSeriesSection class="seminar-series-page__section" />
   </main>
 </template>
 
@@ -23,11 +24,10 @@ export default class SeminarSeriesPage extends QiskitPage {
 <style lang="scss" scoped>
 .seminar-series-page {
   background-color: $white;
-  color: $white-text-01;
-  display: flex;
 
   &__section {
     @include contained();
+    margin-top: $layout-05;
     margin-bottom: $layout-03;
 
     @include mq($until: large) {
