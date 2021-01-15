c4bc46ac274e6678e633c8a097d2314f7fc65aca
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-01-15 12:44:20 +0100

    chore: restore ThePageHeader

--- components/ui/ThePageHeader.vue
+++ components/ui/ThePageHeader.vue
@@ -1,31 +1,19 @@
 <template>
   <header class="page-header">
     <div class="page-header__container">
-      <div class="page-header__main">
-        <h1 class="page-header__title">
-          <slot />
-        </h1>
-        <div v-if="this.$slots.description" class="page-header__description">
-          <slot name="description" />
-        </div>
-        <AppCta v-if="cta" v-bind="cta" class="page-header__cta" />
-      </div>
-      <div v-if="this.$slots.aside" class="page-header__aside">
-        <slot name="aside" />
-      </div>
+      <h1 class="page-header__title">
+        <slot />
+      </h1>
     </div>
   </header>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import { Component, Prop } from 'vue-property-decorator'
-import { GeneralLink } from '~/constants/appLinks.ts'
+import { Component } from 'vue-property-decorator'
 
 @Component
-export default class ThePageHeader extends Vue {
-  @Prop({ type: Object, required: false }) cta: GeneralLink | undefined;
-}
+export default class ThePageHeader extends Vue {}
 </script>
 
 <style lang="scss" scoped>
@@ -39,45 +27,21 @@ export default class ThePageHeader extends Vue {
     min-height: 28rem * 40 / 64;
   }
 
-  &__aside {
-    align-self: start;
-    width: 5 * $column-size-large;
-  }
-
   &__container {
     @include contained();
-    align-items: center;
     display: flex;
-    flex-direction: row;
-    justify-content: space-between;
-    min-height: 28rem;
+    flex-direction: column;
+    justify-content: center;
+    height: 28rem;
 
     @include mq($until: large) {
-      min-height: 28rem * 40 / 64;
-    }
-
-    @include mq($until: medium) {
-      align-items: initial;
-      flex-direction: column;
-      justify-content: center;
+      height: 28rem * 40 / 64;
     }
   }
 
-  &__cta {
-    margin: $layout-06 0 0;
-  }
-
-  &__description {
-    @include type-style('body-long-01');
-    margin: $layout-05 0 0;
-    max-width: 6 * $column-size-large;
-
-    p:not(:last-child) {
-      margin-bottom: $layout-03;
-    }
-  }
-
-  &__main {
+  &__title {
+    @include type-style('expressive-heading-05', true);
+    color: $white-text-01;
     max-width: 10 * $column-size-large;
 
     @include mq($from: medium, $until: large) {
@@ -87,11 +51,6 @@ export default class ThePageHeader extends Vue {
     @include mq($until: medium) {
       max-width: 5 * $column-size-medium;
     }
-  }
-
-  &__title {
-    @include type-style('expressive-heading-05', true);
-    color: $white-text-01;
   }
 }
 </style>
