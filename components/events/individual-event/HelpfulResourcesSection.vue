a8280d97c5206a1a7227df6bce24e5f3e564343c
y4izus <yaiza.garcia.mm@gmail.com>
2021-01-15 15:38:07 +0100

    Passes helpfulResources as props

--- components/events/individual-event/HelpfulResourcesSection.vue
+++ components/events/individual-event/HelpfulResourcesSection.vue
@@ -13,47 +13,14 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { Component } from 'vue-property-decorator'
+import { Component, Prop } from 'vue-property-decorator'
 import { DescriptionCard } from '~/components/ui/AppDescriptionCard.vue'
 
 @Component
 export default class HelpfulResourcesSection extends Vue {
-  title = 'Helpful Resources';
+  @Prop(Array) resources!: Array<DescriptionCard>
 
-  resources: DescriptionCard[] = [
-    {
-      title: 'Stay informed',
-      description: 'Want to keep up to date with upcoming seminars? Click here to subscribe to our calendar for all upcoming events. This section needs copy review.',
-      cta: {
-        url: '',
-        label: 'Get calendar updates'
-      }
-    },
-    {
-      title: 'Nominate',
-      description: 'If you or someone you know might be interested in speaking in a future seminar, we would love to include them. Please include your name, topic and available dates.',
-      cta: {
-        url: '',
-        label: 'Contact us'
-      }
-    },
-    {
-      title: 'Get up to speed',
-      description: 'If the content of the seminar series is too dense or technical, we have a host of content to help you get up to speed.',
-      cta: {
-        url: 'https://qiskit.org/learn',
-        label: 'Start learning'
-      }
-    },
-    {
-      title: 'Code of conduct',
-      description: 'Qiskit is dedicated to providing an enjoyable and safe experience for all participants. We have a code of conduct that all events adhere to.',
-      cta: {
-        url: 'https://github.com/Qiskit/qiskit/blob/master/CODE_OF_CONDUCT.md',
-        label: 'See code of  conduct'
-      }
-    }
-  ]
+  title = 'Helpful Resources';
 }
 </script>
 
