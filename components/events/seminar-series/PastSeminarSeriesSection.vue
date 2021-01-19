ec3c733b3bc415c0216c5938bf626bfdb71a1406
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-01-19 12:37:01 +0100

    feat: display past seminar series events

---
+++ components/events/seminar-series/PastSeminarSeriesSection.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="past-seminar-series-section">
+    <h2 class="copy__title">
+      Past Seminars
+    </h2>
+    <SeminarSeriesDataTable :columns="tableData.columns" :data-per-row="tableData.dataPerRow" />
+    <AppCta
+      class="past-seminar-series-section__cta"
+      kind="ghost"
+      v-bind="showMoreCta"
+    />
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import { SEMINAR_SERIES_ALL_EPISODES_CTA } from '~/constants/appLinks.ts'
+import events from '~/content/events/past-seminar-series-events.json'
+
+@Component
+export default class PastSeminarSeriesSection extends Vue {
+  tableDataPerRow = events.map(event => ([
+    {
+      component: 'span',
+      styles: 'min-width: 8rem; display: inline-block;',
+      data: event.speaker
+    },
+    {
+      component: 'span',
+      styles: 'min-width: 9rem; display: inline-block;',
+      data: event.institution
+    },
+    {
+      component: 'span',
+      styles: 'min-width: 19rem; display: inline-block;',
+      data: event.title
+    },
+    {
+      component: 'span',
+      styles: 'min-width: 8rem; display: inline-block;',
+      data: event.date
+    },
+    {
+      component: 'AppCta',
+      styles: 'min-width: 6rem;',
+      data: {
+        url: event.to,
+        label: 'Join the event'
+      }
+    }
+  ]));
+
+  tableData = {
+    columns: ['Speaker', 'Institution', 'Seminar title', 'Date of talk', 'Link to talk'],
+    dataPerRow: this.tableDataPerRow
+  }
+
+  showMoreCta = {
+    ...SEMINAR_SERIES_ALL_EPISODES_CTA,
+    label: 'Show more'
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/blocks/copy.scss";
+
+.past-seminar-series-section {
+  &__cta {
+    margin-top: $spacing-03;
+  }
+}
+</style>
