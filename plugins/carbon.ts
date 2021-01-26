af7326db959bc7b6a60db01b7ed17c2767e726bf
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-01-26 12:12:12 +0100

    Create Seminar Series header (#1339)
    * feat: use ThePageHeader header
    
    * feat: create custom Seminar Series header
    
    * feat: reuse EventCard component
    
    * feat: create AppCard vertical layout
    
    * feat: display next event from Airtable

--- plugins/carbon.ts
+++ plugins/carbon.ts
@@ -3,6 +3,7 @@ import CarbonComponentsVue from '@carbon/vue'
 import '~/assets/scss/carbon/components.scss'
 import { CarbonIconsVue } from '@carbon/icons-vue'
 import Calendar20 from '@carbon/icons-vue/lib/calendar/20'
+import Education20 from '@carbon/icons-vue/lib/education/20'
 import Map20 from '@carbon/icons-vue/lib/map/20'
 import ArrowRight20 from '@carbon/icons-vue/lib/arrow--right/20'
 import ArrowRight16 from '@carbon/icons-vue/lib/arrow--right/16'
@@ -21,6 +22,7 @@ Vue.use(CarbonComponentsVue)
 Vue.use(CarbonIconsVue, {
   components: {
     Calendar20,
+    Education20,
     Map20,
     ArrowRight20,
     ArrowRight16,
