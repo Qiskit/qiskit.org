be953031ebd42c0291a6f6205cf77bdfdf069d74
y4izus <yaiza.garcia.mm@gmail.com>
2021-01-27 10:12:14 +0100

    Updates mosaic content

--- components/events/seminar-series/WhatIsThisEventSection.vue
+++ components/events/seminar-series/WhatIsThisEventSection.vue
@@ -12,38 +12,38 @@ import { MosaicElement } from '~/components/ui/AppMosaic.vue'
 
 @Component
 export default class WhatIsThisEventSection extends Vue {
-  title = 'What is the Quantum Information Science Seminar Series?';
+  title = 'What is the Quantum Information Science Seminar Series?'
 
   mosaicElements: MosaicElement[] = [
     {
       position: 'first',
       title: 'Learn from experts',
       description:
-        'This series is a deep dive into various academic and research topics within the quantum community.',
+        'Learn from & interact directly with world-leading experts in quantum, from across the globe.',
       image: '/images/events/seminar-series/mosaic-experts.png'
     },
     {
       position: 'second',
-      title: 'About the host',
+      title: 'About the team',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: '/images/events/seminar-series/mosaic-host.jpg'
+        'Our savvy producer is Paul Searle and our seminar co-host is Dr. Olivia Lanes.',
+      image: '/images/events/seminar-series/mosaic-team.png'
     },
     {
       position: 'third',
-      title: 'About the speakers',
+      title: 'About the host',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
-      image: '/images/events/seminar-series/mosaic-speakers.jpg'
+        'Dr. Zlatko K. Minev, from IBM Quantum, is our lead host, whose quantum research earned him MIT Tech Review’s 35 under 35 Global Innovator award, a spot on Discover’s Top 50 Stories of the Year, and the JVA Presidential Award. Minev loves sharing science and is also the recipient of the Yale-Jefferson Award for Public Service.',
+      image: '/images/events/seminar-series/mosaic-host.png'
     },
     {
       position: 'fourth',
-      title: 'Interactivity headline',
+      title: 'Real time questions & discussion',
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+        'Discuss in real time with other researchers, students, and folks in quantum, while having the ability to ask questions of the speaker in real time via the comment chat box on youtube. ',
       image: '/images/events/seminar-series/mosaic-interactivity.png'
     }
-  ];
+  ]
 }
 </script>
 
