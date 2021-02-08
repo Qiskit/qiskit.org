062e5335be18b2cfd97e6367e00e5cab1bcc26a5
y4izus <yaiza.garcia.mm@gmail.com>
2021-02-08 10:59:01 +0100

    Adds segment for helpful-resources links

--- pages/events/seminar-series.vue
+++ pages/events/seminar-series.vue
@@ -37,7 +37,10 @@ export default class SeminarSeriesPage extends QiskitPage {
       description: 'Want to keep up to date with upcoming seminars? Click here to subscribe to our calendar for all upcoming events.',
       cta: {
         url: 'https://calendar.google.com/calendar/ical/c12g9fqo0mkvp9bo26dhm3u1rs%40group.calendar.google.com/public/basic.ics',
-        label: 'Get calendar updates'
+        label: 'Get calendar updates',
+        segment: {
+          action: `${this.routeName} > helpful-resources > get-calendar`
+        }
       }
     },
     {
@@ -45,7 +48,10 @@ export default class SeminarSeriesPage extends QiskitPage {
       description: 'If you or someone you know might be interested in speaking in a future seminar, we would love to include them. Please include your name, topic and available dates.',
       cta: {
         url: 'https://airtable.com/shrB5wy8SCaMMtKop',
-        label: 'Contact us'
+        label: 'Contact us',
+        segment: {
+          action: `${this.routeName} > helpful-resources > contact`
+        }
       }
     },
     {
@@ -53,7 +59,10 @@ export default class SeminarSeriesPage extends QiskitPage {
       description: 'If the content of the seminar series is too dense or technical, we have a host of content to help you get up to speed.',
       cta: {
         url: 'https://qiskit.org/learn',
-        label: 'Start learning'
+        label: 'Start learning',
+        segment: {
+          action: `${this.routeName} > helpful-resources > qiskit-org-learn`
+        }
       }
     },
     {
@@ -61,7 +70,10 @@ export default class SeminarSeriesPage extends QiskitPage {
       description: 'Qiskit is dedicated to providing an enjoyable and safe experience for all participants. We have a code of conduct that all events adhere to.',
       cta: {
         url: 'https://github.com/Qiskit/qiskit/blob/master/CODE_OF_CONDUCT.md',
-        label: 'See code of  conduct'
+        label: 'See code of  conduct',
+        segment: {
+          action: `${this.routeName} > helpful-resources > code-of-conduct`
+        }
       }
     }
   ]
