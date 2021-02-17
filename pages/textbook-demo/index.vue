6eba328c02e4219d992ebc44306f3725d0630955
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-02-17 16:10:06 +0100

    feat: create "Helpful Resources" section for Textbook LP (#1449)
    * feat: create "Textbook Demo" page
    
    * style: lint fixes
    
    * feat: create "Helpful Resources" section
    
    * feat: point placeholder links to "#"
    
    * style: code prettier
    
    * refactor: rename component classes

--- pages/textbook-demo/index.vue
+++ pages/textbook-demo/index.vue
@@ -1,12 +1,16 @@
 <template>
-  <main>
-    <!-- TODO: Add page content -->
+  <main class="textbook-demo-page">
+    <HelpfulResourcesSection
+      class="textbook-demo-page__section"
+      :resources="helpfulResources"
+    />
   </main>
 </template>
 
 <script lang="ts">
 import { Component } from 'vue-property-decorator'
 import QiskitPage from '~/components/logic/QiskitPage.vue'
+import { DescriptionCard } from '~/components/ui/AppDescriptionCard.vue'
 
 @Component({
   head () {
@@ -17,5 +21,54 @@ import QiskitPage from '~/components/logic/QiskitPage.vue'
 })
 export default class TextbookPage extends QiskitPage {
   routeName: string = 'textbook-demo'
+
+  helpfulResources: DescriptionCard[] = [
+    {
+      title: 'Other learning material',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie ipsum eget id id hendrerit quis euismod eleifend urna.',
+      cta: {
+        url: '#',
+        label: 'Lorem ipsum'
+      }
+    },
+    {
+      title: 'Docs',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus morbi sodales euismod faucibus ut turpis sem.',
+      cta: {
+        url: '#',
+        label: 'Lorem ipsum'
+      }
+    },
+    {
+      title: 'Slack',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mollis turpis pulvinar morbi consequat pulvinar sit.',
+      cta: {
+        url: '#',
+        label: 'Lorem ipsum'
+      }
+    },
+    {
+      title: 'Games and Demos',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo vestibulum urna sit est pellentesque tincidunt nisi, enim tincidunt.',
+      cta: {
+        url: '#',
+        label: 'Lorem ipsum'
+      }
+    }
+  ]
 }
 </script>
+
+<style lang="scss" scoped>
+.textbook-demo-page {
+  &__section {
+    @include contained();
+    margin-bottom: $layout-03;
+    margin-top: $layout-05;
+  }
+}
+</style>
