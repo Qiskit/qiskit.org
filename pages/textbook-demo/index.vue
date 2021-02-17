a2e69156ad8327e360b23ae8f6a29f11862417a6
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-02-17 14:25:41 +0100

    feat: create "Helpful Resources" section

--- pages/textbook-demo/index.vue
+++ pages/textbook-demo/index.vue
@@ -1,12 +1,16 @@
 <template>
-  <main>
-    <!-- TODO: Add page content -->
+  <main class="textbook-page">
+    <HelpfulResourcesSection
+      class="textbook-page__section"
+      :resources="helpfulResources"
+    />
   </main>
 </template>
 
 <script lang="ts">
 import { Component } from 'vue-property-decorator'
 import QiskitPage from '~/components/logic/QiskitPage.vue'
+import { DescriptionCard } from '~/components/ui/AppDescriptionCard.vue'
 
 @Component({
   head () {
@@ -17,5 +21,55 @@ import QiskitPage from '~/components/logic/QiskitPage.vue'
 })
 export default class TextbookPage extends QiskitPage {
   routeName: string = 'textbook-demo'
+
+  helpfulResources: DescriptionCard[] = [
+    {
+      title: 'Other learning material',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Molestie ipsum eget id id hendrerit quis euismod eleifend urna.',
+      cta: {
+        url:
+          'https://calendar.google.com/calendar/ical/c12g9fqo0mkvp9bo26dhm3u1rs%40group.calendar.google.com/public/basic.ics',
+        label: 'Lorem ipsum'
+      }
+    },
+    {
+      title: 'Docs',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Risus morbi sodales euismod faucibus ut turpis sem.',
+      cta: {
+        url: 'https://airtable.com/shrB5wy8SCaMMtKop',
+        label: 'Lorem ipsum'
+      }
+    },
+    {
+      title: 'Slack',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mollis turpis pulvinar morbi consequat pulvinar sit.',
+      cta: {
+        url: 'https://qiskit.org/learn',
+        label: 'Lorem ipsum'
+      }
+    },
+    {
+      title: 'Games and Demos',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Justo vestibulum urna sit est pellentesque tincidunt nisi, enim tincidunt.',
+      cta: {
+        url: 'https://github.com/Qiskit/qiskit/blob/master/CODE_OF_CONDUCT.md',
+        label: 'Lorem ipsum'
+      }
+    }
+  ]
 }
 </script>
+
+<style lang="scss" scoped>
+.textbook-page {
+  &__section {
+    @include contained();
+    margin-bottom: $layout-03;
+    margin-top: $layout-05;
+  }
+}
+</style>
