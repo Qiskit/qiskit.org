2a149c3f0a825e74e57f52a21efc4f1a1b585444
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-02-17 13:45:41 +0100

    feat: create "Textbook Demo" page

---
+++ pages/textbook-demo/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <!-- TODO: Add page content -->
+  </main>
+</template>
+
+<script lang="ts">
+import { Component } from "vue-property-decorator";
+import QiskitPage from "~/components/logic/QiskitPage.vue";
+
+@Component({
+  head() {
+    return {
+      title: "Qiskit Textbook",
+    };
+  },
+})
+export default class TextbookPage extends QiskitPage {
+  routeName: string = "textbook-demo";
+}
+</script>
