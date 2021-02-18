e607f9729167e6453ad23c3ec79244399cbc3157
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-02-18 17:22:34 +0100

    feat: create TextbookDemoHeader component

---
+++ components/textbook-demo/TextbookDemoHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <!-- TODO: Create header -->
+  </header>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class TextbookDemoHeader extends Vue { }
+</script>
+
+<style lang="scss" scoped>
+</style>
