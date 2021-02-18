41886705b445e83a368e35a92351da4a7fc98047
y4izus <yaiza.garcia.mm@gmail.com>
2021-02-18 12:36:32 +0100

    Adds helpful-resources section

--- pages/events/mit-endicott.vue
+++ pages/events/mit-endicott.vue
@@ -1,12 +1,13 @@
 <template>
   <main class="mit-endicott-page">
-    <!-- TODO add sections here -->
+    <HelpfulResourcesSection class="mit-endicott-page__section" :resources="helpfulResources" />
   </main>
 </template>
 
 <script lang="ts">
 import { Component } from 'vue-property-decorator'
 import QiskitPage from '~/components/logic/QiskitPage.vue'
+import { DescriptionCard } from '~/components/ui/AppDescriptionCard.vue'
 
 @Component({
   head () {
@@ -17,11 +18,69 @@ import QiskitPage from '~/components/logic/QiskitPage.vue'
 })
 export default class SeminarSeriesPage extends QiskitPage {
   routeName = 'mit-endicott'
+
+  // TODO: Replace with the final content
+  helpfulResources: DescriptionCard[] = [
+    {
+      title: 'Stay informed',
+      description: 'Want to keep up to date with upcoming seminars? Click here to subscribe to our calendar for all upcoming events.',
+      cta: {
+        url: '/',
+        label: 'Get calendar updates',
+        segment: {
+          action: `${this.routeName} > helpful-resources > get-calendar`
+        }
+      }
+    },
+    {
+      title: 'Nominate',
+      description: 'If you or someone you know might be interested in speaking in a future seminar, we would love to include them. Please include your name, topic and available dates.',
+      cta: {
+        url: '/',
+        label: 'Contact us',
+        segment: {
+          action: `${this.routeName} > helpful-resources > contact`
+        }
+      }
+    },
+    {
+      title: 'Get up to speed',
+      description: 'If the content of the seminar series is too dense or technical, we have a host of content to help you get up to speed.',
+      cta: {
+        url: '/',
+        label: 'Start learning',
+        segment: {
+          action: `${this.routeName} > helpful-resources > qiskit-org-learn`
+        }
+      }
+    },
+    {
+      title: 'Code of conduct',
+      description: 'Qiskit is dedicated to providing an enjoyable and safe experience for all participants. We have a code of conduct that all events adhere to.',
+      cta: {
+        url: '/',
+        label: 'See code of conduct',
+        segment: {
+          action: `${this.routeName} > helpful-resources > code-of-conduct`
+        }
+      }
+    }
+  ]
 }
 </script>
 
 <style lang="scss" scoped>
 .mit-endicott-page {
   color: $white-text-01;
+
+  &__section {
+    @include contained();
+    margin-top: $layout-05;
+    margin-bottom: $layout-03;
+
+    @include mq($until: large) {
+      margin-bottom: $layout-01;
+    }
+  }
 }
 </style>
