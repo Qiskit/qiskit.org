0aea09712121c039b89ee06befd0bcba9b858fd4
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-02-18 17:47:36 +0100

    feat: position elements correctly

--- components/textbook-demo/TextbookDemoHeader.vue
+++ components/textbook-demo/TextbookDemoHeader.vue
@@ -1,11 +1,13 @@
 <template>
   <header class="textbook-demo-header">
     <div class="textbook-demo-header__container">
-      <AppPageHeaderTitle>
-        Qiskit Textbook (beta)
-      </AppPageHeaderTitle>
-      <div class="textbook-demo-header__dropdown-container">
-        "Browse Content Dropdown"
+      <div>
+        <AppPageHeaderTitle>
+          Qiskit Textbook (beta)
+        </AppPageHeaderTitle>
+        <div class="textbook-demo-header__dropdown-container">
+          "Browse Content Dropdown"
+        </div>
       </div>
       <div class="textbook-demo-header__cta-container">
         <AppCta v-bind="cta" />
@@ -28,10 +30,19 @@ export default class TextbookDemoHeader extends Vue {
 <style lang="scss" scoped>
 .textbook-demo-header {
   background-color: $cool-gray-10;
+  height: 34rem;
 
   &__container {
     @include contained();
-    padding-top: $spacing-07;
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: space-between;
+    padding-top: $spacing-09;
+  }
+
+  &__dropdown-container {
+    margin-top: $layout-03;
   }
 
   &__cta-container {
