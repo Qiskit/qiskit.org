deadf159c276d0fba980ea1529c159d6711b74f5
y4izus <yaiza.garcia.mm@gmail.com>
2021-02-18 14:59:17 +0100

    Creates AppDataTableSection

---
+++ components/ui/AppDataTableSection.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="data-table-section">
+    <h2 class="copy__title">
+      {{ sectionTitle }}
+    </h2>
+    <AppDataTable :columns="dataTableColumns" :elements="dataTableElements" />
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+import { TableRowElement } from '~/components/ui/AppDataTable.vue'
+
+@Component
+export default class UpcomingSeminarSeriesSection extends Vue {
+  @Prop({ type: String, required: true }) sectionTitle!: string
+  @Prop({ type: Array, default: () => [] }) dataTableColumns!: string[]
+  @Prop({ type: Array, default: () => [] }) dataTableElements!: TableRowElement[]
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/blocks/copy.scss";
+
+.data-table-section {
+  .copy__title {
+    max-width: initial;
+  }
+}
+</style>
