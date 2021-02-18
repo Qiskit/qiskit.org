01ee5584572971050b5e482bd9db8ecef9f0b101
y4izus <yaiza.garcia.mm@gmail.com>
2021-02-18 16:24:04 +0100

    Reuse the AppMosaicSection component on the Seminar Series page

--- pages/events/seminar-series.vue
+++ pages/events/seminar-series.vue
@@ -1,6 +1,11 @@
 <template>
   <main class="event-page seminar-series-page">
     <SeminarSeriesHeader class="seminar-series-page__header" :next-event="nextEvent" :past-events="pastEvents" />
+    <AppMosaicSection
+      class="seminar-series-page__section"
+      :title="mosaicSectionTitle"
+      :elements="mosaicElements"
+    />
     <WhatIsThisEventSection class="seminar-series-page__section" />
     <AppDataTableSection
       v-if="hasUpcomingEvents"
@@ -29,6 +34,7 @@
 <script lang="ts">
 import { Component } from 'vue-property-decorator'
 import QiskitPage from '~/components/logic/QiskitPage.vue'
+import { MosaicElement } from '~/components/ui/AppMosaic.vue'
 import { DescriptionCard } from '~/components/ui/AppDescriptionCard.vue'
 import { SeminarSeriesEvent } from '~/hooks/event-conversion-utils'
 import { TableRowElement } from '~/components/ui/AppDataTable.vue'
@@ -46,6 +52,40 @@ import { SEMINAR_SERIES_FULL_ARCHIVE_CTA } from '~/constants/appLinks.ts'
 export default class SeminarSeriesPage extends QiskitPage {
   routeName = 'seminar-series'
 
+  // Data for the mosaic section
+  mosaicSectionTitle = 'What is the Quantum Information Science Seminar Series?'
+  mosaicElements: MosaicElement[] = [
+    {
+      position: 'first',
+      title: 'Learn from experts',
+      description:
+        'Learn from & interact directly with world-leading experts in quantum, from across the globe.',
+      image: '/images/events/seminar-series/mosaic-experts.png'
+    },
+    {
+      position: 'second',
+      title: 'About the hosts',
+      description:
+        'Dr. Zlatko K. Minev, research staff member at IBM Quantum and recipient of MIT Tech Review’s 35 under 35 Global Innovator award, is our lead host. He is joined by Dr. Olivia Lanes, an experimental researcher and education developer at IBM, working to bridge the gap between the hardware and software communities.',
+      image: '/images/events/seminar-series/mosaic-hosts.png'
+    },
+    {
+      position: 'third',
+      title: 'The latest in quantum computing',
+      description:
+        'This series will discuss all the most current research and new developements across the field of quantum computing.',
+      image: '/images/events/seminar-series/mosaic-team.png'
+    },
+    {
+      position: 'fourth',
+      title: 'Real time questions & discussion',
+      description:
+        'Discuss in real time with other researchers, students, and folks in quantum, while having the ability to ask questions of the speaker in real time via the comment chat box on YouTube.',
+      image: '/images/events/seminar-series/mosaic-interactivity.png'
+    }
+  ]
+
+  // Data for the data table's sections
   upcomingEventsSectionTitle = 'Upcoming Quantum Seminar Schedule'
   pastEventsSectionTitle = 'Past Quantum Seminars'
   seminarSeriesDataTableColumns = ['Speaker', 'Institution', 'Name of talk', 'Date of talk', 'Link to talk']
