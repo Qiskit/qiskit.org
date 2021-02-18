c70146392a7f2f7911a5f2c487ca8f0286d67683
y4izus <yaiza.garcia.mm@gmail.com>
2021-02-18 12:31:58 +0100

    Creates the base for endicotts page

---
+++ pages/events/mit-endicott.vue
@@ -0,0 +1,27 @@
+<template>
+  <main class="mit-endicott-page">
+    <!-- TODO add sections here -->
+  </main>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import QiskitPage from '~/components/logic/QiskitPage.vue'
+
+@Component({
+  head () {
+    return {
+      title: 'MIT Endicott Event'
+    }
+  }
+})
+export default class SeminarSeriesPage extends QiskitPage {
+  routeName = 'mit-endicott'
+}
+</script>
+
+<style lang="scss" scoped>
+.mit-endicott-page {
+  color: $white-text-01;
+}
+</style>
