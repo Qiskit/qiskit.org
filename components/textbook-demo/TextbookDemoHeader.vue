da69a358595b85725ac7b89c62fe763b55955bd7
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-03-03 13:06:50 +0100

    chore: remove explicit component registration

--- components/textbook-demo/TextbookDemoHeader.vue
+++ components/textbook-demo/TextbookDemoHeader.vue
@@ -21,13 +21,8 @@ import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
 import { TEXTBOOK_DEMO_START_LEARNING } from '~/constants/appLinks'
 import { TEXTBOOK_DEMO_MEGA_MENU } from '~/constants/megaMenuLinks'
-import AppMegaDropdownMenu from '~/components/ui/AppMegaDropdownMenu.vue'
 
-@Component({
-  components: {
-    AppMegaDropdownMenu
-  }
-})
+@Component
 export default class TextbookDemoHeader extends Vue {
   startLearningCTA = TEXTBOOK_DEMO_START_LEARNING
   dropdownMenuPlaceholder = 'Browse content'
