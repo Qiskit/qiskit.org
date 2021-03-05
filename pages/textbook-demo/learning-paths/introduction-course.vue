e6d70864beac9ae1a88a6e4dafcb2b61688d4ca5
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-03-05 13:10:42 +0100

    feat: create page

---
+++ pages/textbook-demo/learning-paths/introduction-course.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="learning-path-introduction-course-page">
+  </main>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import QiskitPage from '~/components/logic/QiskitPage.vue'
+
+@Component({
+  head () {
+    return {
+      title: 'Introduction course'
+    }
+  }
+})
+export default class LearningPathIntroductionCoursePage extends QiskitPage {
+  routeName: string = 'introduction-course'
+}
+</script>
