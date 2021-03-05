e9de80b8ea5399b0569088e306a92f602560e426
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-03-05 13:51:14 +0100

    feat: create ExternalRecommendedReadings component

---
+++ components/textbook-demo/LearningPathExternalRecommendedReadings.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="learning-path-external-recommended-readings">
+    <h2 class="learning-path-external-recommended-readings__headline">
+      External recommended readings
+    </h2>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class LearningPathExternalRecommendedReadings extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import "~carbon-components/scss/globals/scss/typography";
+
+.learning-path-external-recommended-readings {
+  &__headline {
+    @include type-style("productive-heading-01");
+    color: $cool-gray-80;
+
+    @include mq($until: x-large) {
+      @include type-style("expressive-heading-04");
+    }
+  }
+}
+</style>
---
+++ pages/textbook-demo/learning-paths/introduction-course.vue
@@ -0,0 +1,31 @@
+<template>
+  <main class="learning-path-introduction-course-page">
+    <LearningPathExternalRecommendedReadings class="learning-path-introduction-course-page__section" />
+  </main>
+</template>
+
+<script lang="ts">
+import { Component } from 'vue-property-decorator'
+import QiskitPage from '~/components/logic/QiskitPage.vue'
+
+@Component({
+  head () {
+    return {
+      title: 'Introduction course'
+    }
+  }
+})
+export default class LearningPathIntroductionCoursePage extends QiskitPage {
+  routeName: string = 'introduction-course'
+}
+</script>
+
+<style lang="scss" scoped>
+.learning-path-introduction-course-page {
+  &__section {
+    @include contained();
+    margin-bottom: $layout-03;
+    margin-top: $layout-05;
+  }
+}
+</style>
