692ab1626b3366958201f38234fd49ae6002131f
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-03-09 14:57:47 +0100

    feat: create grid structure

--- components/textbook-demo/LearningPathPagesSection.vue
+++ components/textbook-demo/LearningPathPagesSection.vue
@@ -1,8 +1,14 @@
 <template>
-  <section>
-    <h2 class="copy__title">
-      Learning Path Pages
-    </h2>
+  <section class="learning-path-pages-section">
+    <div>
+      <h2 class="copy__title">
+        Learning path pages
+      </h2>
+      <nav class="learning-path-pages-section__nav">
+        Paths nav
+      </nav>
+    </div>
+    <main class="learning-path-pages-section__main">Right side</main>
   </section>
 </template>
 
@@ -14,3 +20,22 @@ import { Component } from 'vue-property-decorator'
 export default class LearningPathPagesSection extends Vue {
 }
 </script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/blocks/copy.scss';
+
+.learning-path-pages-section {
+  display: grid;
+  column-gap: $spacing-07;
+  grid-template-areas: '. main main main';
+  grid-template-columns: repeat(4, 1fr) ;
+
+  &__main {
+    grid-area: main;
+  }
+
+  &__nav {
+    margin-right: -$spacing-07;
+  }
+}
+</style>
