0cb7c26082de1f85e4ad31354ebf79c98c141ba9
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-03-09 13:05:09 +0100

    feat: create LearningPathPages component

---
+++ components/textbook-demo/LearningPathPagesSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <h2 class="copy__title">
+      Learning Path Pages
+    </h2>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class LearningPathPagesSection extends Vue {
+}
+</script>
