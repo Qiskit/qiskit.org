f0deaf45db29b80e5037ac81cf4d559760a5d438
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-03-26 15:19:36 +0100

    feat: adjust layout of blue section

--- pages/photon.vue
+++ pages/photon.vue
@@ -7,22 +7,29 @@
     <section class="photon__get-it-section">
       <div class="photon__container">
         <h2 class="copy__title">Lorem ipsum</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum ut
-          aliquet aliquam quis. Pellentesque pulvinar consectetur cras proin
-          nulla. Sollicitudin ut felis, erat in lacus eleifend non faucibus
-          enim. Vestibulum libero elementum amet, faucibus lorem convallis
-          pellentesque diam. Lorem ipsum dolor sit amet, consectetur adipiscing
-          elit. Fermentum ut aliquet aliquam quis. Pellentesque pulvinar
-          consectetur cras proin nulla. Sollicitudin ut felis, erat in lacus
-          eleifend non faucibus enim. Vestibulum libero elementum amet, faucibus
-          lorem convallis pellentesque diam.
-        </p>
-        <AppCta
-          class="photon__get-it-section__cta"
-          label="Get a recommendation"
-          url="/"
-        />
+        <div class="bx--row">
+          <div class="bx--col-lg-6">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum
+              ut aliquet aliquam quis. Pellentesque pulvinar consectetur cras
+              proin nulla. Sollicitudin ut felis, erat in lacus eleifend non
+              faucibus enim. Vestibulum libero elementum amet, faucibus lorem
+              convallis pellentesque diam. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Fermentum ut aliquet aliquam quis.
+              Pellentesque pulvinar consectetur cras proin nulla. Sollicitudin
+              ut felis, erat in lacus eleifend non faucibus enim. Vestibulum
+              libero elementum amet, faucibus lorem convallis pellentesque diam.
+            </p>
+            <AppCta
+              class="photon__get-it-section__cta"
+              label="Get a recommendation"
+              url="/"
+            />
+          </div>
+          <div class="bx--col-lg-6">
+            <img src="http://via.placeholder.com/480x270" />
+          </div>
+        </div>
       </div>
     </section>
   </div>
