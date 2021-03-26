baa1de0ea4998a5dcd1a013ccb4134d582772336
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-03-26 14:32:37 +0100

    feat: create page

---
+++ pages/photon.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>Content goes here</div>
+</template>
+
+<script lang="ts">
+import { Component } from "vue-property-decorator";
+import QiskitPage from "~/components/logic/QiskitPage.vue";
+
+@Component({
+  head() {
+    return {
+      title: "Photon",
+    };
+  },
+})
+export default class PhotonPage extends QiskitPage {
+  routeName = "photon";
+}
+</script>
