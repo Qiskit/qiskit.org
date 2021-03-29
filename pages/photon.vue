b8c92b8d3e2f0a44103694406684c3baae212844
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-03-29 10:47:54 +0200

    feat: add white section

--- pages/photon.vue
+++ pages/photon.vue
@@ -1,14 +1,31 @@
 <template>
   <div>
     <header>Header</header>
-    <section class="photon__white-section">
-      <h2 class="copy__title">Lorem ipsum</h2>
-      <div class="bx--row">
-        <div class="bx--col-lg-6">left</div>
-        <div class="bx--col-lg-6">right</div>
+    <section class="photon__section">
+      <div class="photon__container">
+        <h2 class="copy__title">Lorem ipsum</h2>
+        <div class="bx--row">
+          <div class="bx--col-lg-6">
+            <img src="http://via.placeholder.com/480x270" />
+          </div>
+          <div class="bx--col-lg-6">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum
+              ut aliquet aliquam quis. Pellentesque pulvinar consectetur cras
+              proin nulla. Sollicitudin ut felis, erat in lacus eleifend non
+              faucibus enim. Vestibulum libero elementum amet, faucibus lorem
+              convallis pellentesque diam. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Fermentum ut aliquet aliquam quis.
+              Pellentesque pulvinar consectetur cras proin nulla. Sollicitudin
+              ut felis, erat in lacus eleifend non faucibus enim. Vestibulum
+              libero elementum amet, faucibus lorem convallis pellentesque diam.
+            </p>
+            <AppCta class="photon__cta" label="Get movie" url="/" />
+          </div>
+        </div>
       </div>
     </section>
-    <section class="photon__blue-section">
+    <section class="photon__section photon__section_blue">
       <div class="photon__container">
         <h2 class="copy__title">Lorem ipsum</h2>
         <div class="bx--row">
@@ -25,7 +42,7 @@
               libero elementum amet, faucibus lorem convallis pellentesque diam.
             </p>
             <AppCta
-              class="photon__blue-section__cta"
+              class="photon__cta photon__cta_white"
               label="Get a recommendation"
               url="/"
             />
@@ -61,20 +78,26 @@ export default class PhotonPage extends QiskitPage {
     @include contained();
   }
 
-  &__blue-section {
-    background: $carbon--blue-70;
-    padding-bottom: $spacing-09;
-    padding-top: $spacing-09;
+  &__cta {
+    margin-top: $spacing-07;
 
-    &__cta {
+    &_white {
       background: $carbon--cool-gray-10;
       background-image: none !important;
       color: $carbon--blue-80 !important;
-      margin-top: $spacing-07;
     }
+  }
 
-    .copy__title {
-      color: $text-04;
+  &__section {
+    padding-bottom: $spacing-09;
+    padding-top: $spacing-09;
+
+    &_blue {
+      background: $carbon--blue-70;
+
+      .copy__title {
+        color: $text-04;
+      }
     }
   }
 }
