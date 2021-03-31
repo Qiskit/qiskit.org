02d384547e24d2ff52d7a1fa4f5defa2a3fcae49
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-03-31 10:58:40 +0200

    refactor: extract PhotonRecommendationSection

---
+++ components/photon/PhotonRecommendationSection.vue
@@ -0,0 +1,58 @@
+<template>
+  <section>
+    <div class="photon__container">
+      <h2 class="copy__title">
+        Lorem ipsum
+      </h2>
+      <div class="bx--row">
+        <div class="bx--col-lg-6 bx--col-md-4 photon__section-mobile-spacing">
+          <img
+            class="photon__img-responsive"
+            src="http://via.placeholder.com/480x270"
+          >
+        </div>
+        <div class="bx--col-lg-6 bx--col-md-4">
+          <p class="copy__paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum
+            ut aliquet aliquam quis. Pellentesque pulvinar consectetur cras
+            proin nulla. Sollicitudin ut felis, erat in lacus eleifend non
+            faucibus enim. Vestibulum libero elementum amet, faucibus lorem
+            convallis pellentesque diam. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Fermentum ut aliquet aliquam quis. Pellentesque
+            pulvinar consectetur cras proin nulla. Sollicitudin ut felis, erat
+            in lacus eleifend non faucibus enim. Vestibulum libero elementum
+            amet, faucibus lorem convallis pellentesque diam.
+          </p>
+          <AppCta
+            class="photon__cta photon__cta_dark"
+            label="Get movie"
+            url="#get-movie"
+          />
+          <AppLink class="photon-recommendation-section__request-recommendation-link" url="/">
+            Request new recommendation
+          </AppLink>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class PhotonRecommendationSection extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import "~/assets/scss/blocks/copy.scss";
+
+.photon-recommendation-section {
+  &__request-recommendation-link {
+    color: $carbon--blue-70 !important;
+    display: inline-block;
+    margin-top: $spacing-06;
+  }
+}
+</style>
