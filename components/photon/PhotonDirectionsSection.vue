4f575952f9c67c8fe3a64c9fc7c31a9a080982ec
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-03-31 14:28:55 +0200

    refactor: extract PhotonDirectionsSection

---
+++ components/photon/PhotonDirectionsSection.vue
@@ -0,0 +1,47 @@
+<template>
+  <section id="get-movie">
+    <div class="photon__container">
+      <h2 class="copy__title">
+        Lorem ipsum
+      </h2>
+      <div class="bx--row">
+        <div class="bx--col-lg-6 bx--col-md-4 photon__section-mobile-spacing">
+          <p class="copy__paragraph">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum
+            ut aliquet aliquam quis. Pellentesque pulvinar consectetur cras
+            proin nulla. Sollicitudin ut felis, erat in lacus eleifend non
+            faucibus enim. Vestibulum libero elementum amet, faucibus lorem
+            convallis pellentesque diam. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Fermentum ut aliquet aliquam quis. Pellentesque
+            pulvinar consectetur cras proin nulla. Sollicitudin ut felis, erat
+            in lacus eleifend non faucibus enim. Vestibulum libero elementum
+            amet, faucibus lorem convallis pellentesque diam.
+          </p>
+          <AppCta
+            class="photon__cta photon__cta_light"
+            label="Print directions to Photon"
+            url="/"
+          />
+        </div>
+        <div class="bx--col-lg-6 bx--col-md-4">
+          <img
+            class="photon__img-responsive"
+            src="http://via.placeholder.com/480x270"
+          >
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class PhotonDirectionsSection extends Vue {}
+</script>
+
+<style lang="scss">
+@import "~/assets/scss/blocks/copy.scss";
+</style>
--- pages/photon.vue
+++ pages/photon.vue
@@ -4,36 +4,7 @@
     <PhotonRecommendationSection
       class="photon__section photon__section_light"
     />
-    <section id="get-movie" class="photon__section photon__section_dark">
-      <div class="photon__container">
-        <h2 class="copy__title">
-          Lorem ipsum
-        </h2>
-        <div class="bx--row">
-          <div class="bx--col-lg-6 bx--col-md-4 photon__section-mobile-spacing">
-            <p class="copy__paragraph">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fermentum
-              ut aliquet aliquam quis. Pellentesque pulvinar consectetur cras
-              proin nulla. Sollicitudin ut felis, erat in lacus eleifend non
-              faucibus enim. Vestibulum libero elementum amet, faucibus lorem
-              convallis pellentesque diam. Lorem ipsum dolor sit amet,
-              consectetur adipiscing elit. Fermentum ut aliquet aliquam quis.
-              Pellentesque pulvinar consectetur cras proin nulla. Sollicitudin
-              ut felis, erat in lacus eleifend non faucibus enim. Vestibulum
-              libero elementum amet, faucibus lorem convallis pellentesque diam.
-            </p>
-            <AppCta
-              class="photon__cta photon__cta_light"
-              label="Print directions to Photon"
-              url="/"
-            />
-          </div>
-          <div class="bx--col-lg-6 bx--col-md-4">
-            <img class="photon__img-responsive" src="http://via.placeholder.com/480x270">
-          </div>
-        </div>
-      </div>
-    </section>
+    <PhotonDirectionsSection class="photon__section photon__section_dark" />
     <section class="photon__section">
       <div class="photon__container">
         <div class="bx--row">
@@ -133,6 +104,8 @@ export default class PhotonPage extends QiskitPage {
 </script>
 
 <style lang="scss">
+@import "~/assets/scss/blocks/copy.scss";
+
 .photon {
   &__container {
     @include contained();
@@ -157,18 +130,6 @@ export default class PhotonPage extends QiskitPage {
     max-width: 100%;
   }
 
-  &__section-mobile-spacing {
-    @include mq($until: medium) {
-      margin-bottom: $spacing-09;
-    }
-  }
-}
-</style>
-
-<style lang="scss" scoped>
-@import "~/assets/scss/blocks/copy.scss";
-
-.photon {
   &__section {
     padding-bottom: $spacing-09;
     padding-top: $spacing-09;
@@ -184,6 +145,12 @@ export default class PhotonPage extends QiskitPage {
 
     &_light {
       background: $carbon--cool-gray-10;
+    }
+  }
+
+  &__section-mobile-spacing {
+    @include mq($until: medium) {
+      margin-bottom: $spacing-09;
     }
   }
 }
