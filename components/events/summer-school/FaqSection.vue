ad766d860022c3113c35b42f34998dcadfc338c7
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-04-28 12:58:26 +0200

    fix: CSS import and lint fixes

--- components/events/summer-school/FaqSection.vue
+++ components/events/summer-school/FaqSection.vue
@@ -292,7 +292,7 @@ export default class FaqSection extends Vue {
 </script>
 
 <style lang="scss" scoped>
-@import "~/assets/scss/layout/_copy.scss";
+@import "~/assets/scss/layout/_typography.scss";
 
 .faq-section {
   ::v-deep {
