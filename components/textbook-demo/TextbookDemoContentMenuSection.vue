47c41ae1883cd8f5b1a242d7fd6ff86df7484164
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-06-04 14:07:56 +0200

    feat(learning-paths): create "mega menu wrapper" section

---
+++ components/textbook-demo/TextbookDemoContentMenuSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="content-menu-section">
+    <div class="bx--grid">
+      <AppMegaDropdownMenu :content="dropdownMenuContent" />
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import { TEXTBOOK_DEMO_MEGA_MENU } from '~/constants/megaMenuLinks'
+
+@Component
+export default class TextbookDemoContentMenuSection extends Vue {
+  dropdownMenuContent = TEXTBOOK_DEMO_MEGA_MENU
+}
+</script>
+
+<style lang="scss" scoped>
+.content-menu-section {
+  background: $background-color-lighter;
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+}
+</style>
