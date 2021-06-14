dafe40433ccf79c63517618be0185020b0709418
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-06-14 12:01:44 +0200

    Display fixed mega menu on scroll (#1888)
    * feat(textbook): check whether the mega menu is visible
    
    * feat(textbook): display fixed mega menu on scroll
    
    * feat(textbook): fade in/out mega menu on scroll
    
    * feat(textbook): fade out fixed mega menu only when leaving

--- components/textbook-demo/TextbookDemoHeader.vue
+++ components/textbook-demo/TextbookDemoHeader.vue
@@ -7,6 +7,7 @@
             Qiskit Textbook (beta)
           </h1>
           <AppMegaDropdownMenu
+            :id="appMegaDropdownMenuId"
             class="textbook-demo-header__dropdown"
             kind="secondary"
             :content="dropdownMenuContent"
@@ -15,19 +16,65 @@
         <AppCta v-bind="startLearningCTA" class="textbook-demo-header__cta" />
       </div>
     </div>
+    <transition name="scroll-in">
+      <TextbookDemoContentMenuSection
+        v-if="!appMegaDropdownMenuIsVisible"
+        class="textbook-demo-header__dropdown_fixed"
+      />
+    </transition>
   </header>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import Vue, { VueConstructor } from 'vue'
 import { Component } from 'vue-property-decorator'
 import { TEXTBOOK_DEMO_START_LEARNING } from '~/constants/appLinks'
 import { TEXTBOOK_DEMO_MEGA_MENU } from '~/constants/megaMenuLinks'
 
+interface VueComponent extends Vue {
+  $el: HTMLElement
+  _uid: number
+}
+
 @Component
-export default class TextbookDemoHeader extends Vue {
+export default class TextbookDemoHeader extends (Vue as VueConstructor<VueComponent>) {
   startLearningCTA = TEXTBOOK_DEMO_START_LEARNING
   dropdownMenuContent = TEXTBOOK_DEMO_MEGA_MENU
+  appMegaDropdownMenuIsVisible = true
+  appMegaDropdownMenuObserver: IntersectionObserver | undefined
+
+  get appMegaDropdownMenuId () {
+    return `app-mega-dropdown-menu-${this._uid}`
+  }
+
+  mounted () {
+    this.connectAppMegaDropdownMenuObserver()
+  }
+
+  beforeDestroy () {
+    this.disconnectAppMegaDropdownMenuObserver()
+  }
+
+  connectAppMegaDropdownMenuObserver () {
+    const appMegaDropdownMenuElement = this.$el.querySelector(`#${this.appMegaDropdownMenuId}`)
+
+    if (appMegaDropdownMenuElement) {
+      this.appMegaDropdownMenuObserver = new IntersectionObserver(this.updateAppMegaDropdownMenuLayout)
+      this.appMegaDropdownMenuObserver.observe(appMegaDropdownMenuElement)
+    }
+  }
+
+  disconnectAppMegaDropdownMenuObserver () {
+    if (this.appMegaDropdownMenuObserver) {
+      this.appMegaDropdownMenuObserver.disconnect()
+    }
+  }
+
+  updateAppMegaDropdownMenuLayout (entries: Array<IntersectionObserverEntry>) {
+    entries.forEach(({ isIntersecting }) => {
+      this.appMegaDropdownMenuIsVisible = isIntersecting
+    })
+  }
 }
 </script>
 
@@ -75,10 +122,26 @@ export default class TextbookDemoHeader extends Vue {
 
   &__dropdown {
     margin-top: $layout-03;
+
+    &_fixed {
+      position: fixed;
+      transition: .3s ease-in-out;
+      top: 0;
+      width: 100%;
+    }
   }
 
   &__cta {
     align-self: flex-end;
   }
+}
+
+.scroll-in-enter,
+.scroll-in-leave-to {
+  margin-top: -40px;
+}
+
+.scroll-in-leave-to {
+  opacity: 0;
 }
 </style>
