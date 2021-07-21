6cf019ef0d6df61af570cc1b9b184bcea289a47a
Abdón Rodríguez Davila <a@abdonrd.com>
2021-07-21 16:16:53 +0200

    Get the Qiskit schemas from the new repository (#2017)

--- deploy/deploy_cos.sh
+++ deploy/deploy_cos.sh
@@ -10,7 +10,7 @@ CONFIG_PATH=$(rclone config file | tail -1)
 # Decrypt config
 openssl aes-256-cbc -K $encrypted_rclone_key -iv $encrypted_rclone_iv -in rclone.conf.enc -out $CONFIG_PATH -d
 
-git clone --depth=1 https://github.com/Qiskit/qiskit-terra.git
-cp -r qiskit-terra/qiskit/schemas ../dist/.
+git clone --depth=1 https://github.com/Qiskit/ibm-quantum-schemas.git
+cp -r ibm-quantum-schemas/schemas ../dist/.
 
 rclone sync --exclude-from ./static-sites.txt ../dist IBMCOS:qiskit-org-web-resources
