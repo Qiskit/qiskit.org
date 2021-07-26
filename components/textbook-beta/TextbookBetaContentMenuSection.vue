888fac5ed1b55f363a0a0bbfdc8c2cd3b39598fb
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2021-07-26 16:27:50 +0200

    refactor(mega-menu-dropdown): use `qiskit-vue` component

--- components/textbook-beta/TextbookBetaContentMenuSection.vue
+++ components/textbook-beta/TextbookBetaContentMenuSection.vue
@@ -1,9 +1,11 @@
 <template>
   <section class="content-menu-section">
     <div class="bx--grid">
-      <AppMegaDropdownMenu
+      <QiskitMegaMenuDropdown
         :content="dropdownMenuContent"
+        input-field-classes="bx--col-md-4 bx--col-lg-4"
         segment-component-name="Textbook mega menu"
+        :track-performed-search="trackPerformedSearch"
       />
     </div>
   </section>
@@ -12,11 +14,18 @@
 <script lang="ts">
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
+import QiskitMegaMenuDropdown from '@qiskit-community/qiskit-vue/src/components/MegaMenuDropdown/index.vue'
 import { TEXTBOOK_BETA_MEGA_MENU } from '~/constants/megaMenuLinks'
 
-@Component
+@Component({
+  components: { QiskitMegaMenuDropdown }
+})
 export default class TextbookBetaContentMenuSection extends Vue {
   dropdownMenuContent = TEXTBOOK_BETA_MEGA_MENU
+
+  trackPerformedSearch (uiElement: string, field: string) {
+    this.$trackPerformedSearch(uiElement, field)
+  }
 }
 </script>
 
