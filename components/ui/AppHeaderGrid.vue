33d4f37ac88a5c72047439ec08759ecf3cfc55a0
y4izus <yaiza.garcia.mm@gmail.com>
2021-08-31 16:20:47 +0200

    Creates svg grid

---
+++ components/ui/AppHeaderGrid.vue
@@ -0,0 +1,68 @@
+<template>
+  <svg width="1584" height="898" viewBox="0 0 1584 898" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <!-- Vertical lines -->
+    <line x1="-40" y1="1" x2="1592" y2="1" stroke="#DDE1E6" />
+    <line x1="-40" y1="65" x2="1592" y2="65" stroke="#DDE1E6" />
+    <line x1="-40" y1="129" x2="1592" y2="129" stroke="#DDE1E6" />
+    <line x1="-40" y1="193" x2="1592" y2="193" stroke="#DDE1E6" />
+    <line x1="-40" y1="257" x2="1592" y2="257" stroke="#DDE1E6" />
+    <line x1="-40" y1="321" x2="1592" y2="321" stroke="#DDE1E6" />
+    <line x1="-40" y1="385" x2="1592" y2="385" stroke="#DDE1E6" />
+    <line x1="-40" y1="449" x2="1592" y2="449" stroke="#DDE1E6" />
+    <line x1="-40" y1="513" x2="1592" y2="513" stroke="#DDE1E6" />
+    <line x1="-40" y1="577" x2="1592" y2="577" stroke="#DDE1E6" />
+    <line x1="-40" y1="641" x2="1592" y2="641" stroke="#DDE1E6" />
+    <line x1="-40" y1="705" x2="728" y2="705" stroke="#DDE1E6" />
+    <line x1="856" y1="705" x2="984" y2="705" stroke="#DDE1E6" />
+    <line x1="1048" y1="705" x2="1176" y2="705" stroke="#DDE1E6" />
+    <line x1="1240" y1="705" x2="1592" y2="705" stroke="#DDE1E6" />
+    <line x1="-40" y1="769" x2="536" y2="769" stroke="#DDE1E6" />
+    <line x1="600" y1="769" x2="664" y2="769" stroke="#DDE1E6" />
+    <line x1="728" y1="769" x2="792" y2="769" stroke="#DDE1E6" />
+    <line x1="856" y1="769" x2="920" y2="769" stroke="#DDE1E6" />
+    <line x1="1112" y1="769" x2="1176" y2="769" stroke="#DDE1E6" />
+    <line x1="1304" y1="769" x2="1592" y2="769" stroke="#DDE1E6" />
+    <line x1="408" y1="833" x2="536" y2="833" stroke="#DDE1E6" />
+    <line x1="728" y1="833" x2="792" y2="833" stroke="#DDE1E6" />
+    <line x1="472" y1="897" x2="536" y2="897" stroke="#DDE1E6" />
+
+    <!-- Horizontal lines -->
+    <line x1="24" y1="1" x2="24" y2="769" stroke="#DDE1E6" />
+    <line x1="88" y1="1" x2="88" y2="769" stroke="#DDE1E6" />
+    <line x1="152" y1="1" x2="152" y2="769" stroke="#DDE1E6" />
+    <line x1="216" y1="1" x2="216" y2="769" stroke="#DDE1E6" />
+    <line x1="280" y1="1" x2="280" y2="769" stroke="#DDE1E6" />
+    <line x1="344" y1="1" x2="344" y2="641" stroke="#DDE1E6" />
+    <line x1="344" y1="705" x2="344" y2="769" stroke="#DDE1E6" />
+    <line x1="408" y1="1" x2="408" y2="833" stroke="#DDE1E6" />
+    <line x1="472" y1="1" x2="472" y2="897" stroke="#DDE1E6" />
+    <line x1="536" y1="1" x2="536" y2="769" stroke="#DDE1E6" />
+    <line x1="536" y1="833" x2="536" y2="897" stroke="#DDE1E6" />
+    <line x1="600" y1="1" x2="600" y2="769" stroke="#DDE1E6" />
+    <line x1="664" y1="1" x2="664" y2="769" stroke="#DDE1E6" />
+    <line x1="728" y1="1" x2="728" y2="705" stroke="#DDE1E6" />
+    <line x1="728" y1="769" x2="728" y2="833" stroke="#DDE1E6" />
+    <line x1="792" y1="1" x2="792" y2="641" stroke="#DDE1E6" />
+    <line x1="792" y1="769" x2="792" y2="833" stroke="#DDE1E6" />
+    <line x1="856" y1="1" x2="856" y2="769" stroke="#DDE1E6" />
+    <line x1="920" y1="1" x2="920" y2="769" stroke="#DDE1E6" />
+    <line x1="984" y1="1" x2="984" y2="705" stroke="#DDE1E6" />
+    <line x1="1048" y1="1" x2="1048" y2="705" stroke="#DDE1E6" />
+    <line x1="1112" y1="1" x2="1112" y2="769" stroke="#DDE1E6" />
+    <line x1="1176" y1="1" x2="1176" y2="769" stroke="#DDE1E6" />
+    <line x1="1240" y1="1" x2="1240" y2="705" stroke="#DDE1E6" />
+    <line x1="1304" y1="1" x2="1304" y2="769" stroke="#DDE1E6" />
+    <line x1="1368" y1="1" x2="1368" y2="769" stroke="#DDE1E6" />
+    <line x1="1432" y1="1" x2="1432" y2="769" stroke="#DDE1E6" />
+    <line x1="1496" y1="1" x2="1496" y2="769" stroke="#DDE1E6" />
+    <line x1="1560" y1="1" x2="1560" y2="769" stroke="#DDE1E6" />
+  </svg>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class AppHeaderGrid extends Vue {}
+</script>
