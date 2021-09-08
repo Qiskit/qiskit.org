f29fcca9e990a479381f223747e38a5aed752710
y4izus <yaiza.garcia.mm@gmail.com>
2021-09-08 12:07:57 +0200

    Improves code and animation

--- components/ui/AppNarrowHeaderGrid.vue
+++ components/ui/AppNarrowHeaderGrid.vue
@@ -7,60 +7,16 @@
     fill="none"
     xmlns="http://www.w3.org/2000/svg">
 
-    <!-- Horizontal lines -->
-    <line x1="0" y1="0" x2="1860" y2="0" stroke="#DDE1E6" />
-    <line x1="0" y1="64" x2="1860" y2="64" stroke="#DDE1E6" />
-    <line x1="0" y1="128" x2="1860" y2="128" stroke="#DDE1E6" />
-    <line x1="0" y1="192" x2="1860" y2="192" stroke="#DDE1E6" />
-    <line x1="0" y1="256" x2="772" y2="256" stroke="#DDE1E6" />
-    <line x1="900" y1="256" x2="1028" y2="256" stroke="#DDE1E6" />
-    <line x1="1092" y1="256" x2="1220" y2="256" stroke="#DDE1E6" />
-    <line x1="1284" y1="256" x2="1860" y2="256" stroke="#DDE1E6" />
-    <line x1="0" y1="320" x2="580" y2="320" stroke="#DDE1E6" />
-    <line x1="644" y1="320" x2="708" y2="320" stroke="#DDE1E6" />
-    <line x1="772" y1="320" x2="836" y2="320" stroke="#DDE1E6" />
-    <line x1="900" y1="320" x2="964" y2="320" stroke="#DDE1E6" />
-    <line x1="1156" y1="320" x2="1220" y2="320" stroke="#DDE1E6" />
-    <line x1="1348" y1="320" x2="1860" y2="320" stroke="#DDE1E6" />
-    <line x1="452" y1="384" x2="580" y2="384" stroke="#DDE1E6" />
-    <line x1="772" y1="384" x2="836" y2="384" stroke="#DDE1E6" />
-    <line x1="516" y1="448" x2="580" y2="448" stroke="#DDE1E6" />
-
-    <!-- Vertical lines -->
-    <line x1="4" y1="0" x2="4" y2="320" stroke="#DDE1E6" />
-    <line x1="68" y1="0" x2="68" y2="320" stroke="#DDE1E6" />
-    <line x1="132" y1="0" x2="132" y2="320" stroke="#DDE1E6" />
-    <line x1="196" y1="0" x2="196" y2="320" stroke="#DDE1E6" />
-    <line x1="260" y1="0" x2="260" y2="320" stroke="#DDE1E6" />
-    <line x1="324" y1="0" x2="324" y2="320" stroke="#DDE1E6" />
-    <line x1="388" y1="0" x2="388" y2="192" stroke="#DDE1E6" />
-    <line x1="388" y1="256" x2="388" y2="320" stroke="#DDE1E6" />
-    <line x1="452" y1="0" x2="452" y2="384" stroke="#DDE1E6" />
-    <line x1="516" y1="0" x2="516" y2="448" stroke="#DDE1E6" />
-    <line x1="580" y1="0" x2="580" y2="320" stroke="#DDE1E6" />
-    <line x1="580" y1="384" x2="580" y2="448" stroke="#DDE1E6" />
-    <line x1="644" y1="0" x2="644" y2="320" stroke="#DDE1E6" />
-    <line x1="708" y1="0" x2="708" y2="320" stroke="#DDE1E6" />
-    <line x1="772" y1="0" x2="772" y2="256" stroke="#DDE1E6" />
-    <line x1="772" y1="320" x2="772" y2="384" stroke="#DDE1E6" />
-    <line x1="836" y1="0" x2="836" y2="192" stroke="#DDE1E6" />
-    <line x1="836" y1="320" x2="836" y2="384" stroke="#DDE1E6" />
-    <line x1="900" y1="0" x2="900" y2="320" stroke="#DDE1E6" />
-    <line x1="964" y1="0" x2="964" y2="320" stroke="#DDE1E6" />
-    <line x1="1028" y1="0" x2="1028" y2="256" stroke="#DDE1E6" />
-    <line x1="1092" y1="0" x2="1092" y2="256" stroke="#DDE1E6" />
-    <line x1="1156" y1="0" x2="1156" y2="320" stroke="#DDE1E6" />
-    <line x1="1220" y1="0" x2="1220" y2="320" stroke="#DDE1E6" />
-    <line x1="1284" y1="0" x2="1284" y2="256" stroke="#DDE1E6" />
-    <line x1="1348" y1="0" x2="1348" y2="320" stroke="#DDE1E6" />
-    <line x1="1412" y1="0" x2="1412" y2="320" stroke="#DDE1E6" />
-    <line x1="1476" y1="0" x2="1476" y2="320" stroke="#DDE1E6" />
-    <line x1="1540" y1="0" x2="1540" y2="320" stroke="#DDE1E6" />
-    <line x1="1604" y1="0" x2="1604" y2="320" stroke="#DDE1E6" />
-    <line x1="1668" y1="0" x2="1668" y2="320" stroke="#DDE1E6" />
-    <line x1="1732" y1="0" x2="1732" y2="320" stroke="#DDE1E6" />
-    <line x1="1796" y1="0" x2="1796" y2="320" stroke="#DDE1E6" />
-    <line x1="1860" y1="0" x2="1860" y2="320" stroke="#DDE1E6" />
+    <path
+      class="app-narrow-header-grid__lines"
+      stroke="#DDE1E6"
+      d="M0 0h1860M0 64h1860M0 128h1860M0 192h1860M0 256h772M900 256h128M1092 256h128M1284 256h576M0
+      320h580M644 320h64M772 320h64M900 320h64M1156 320h64M1348 320h512M452 384h128M772 384h64M516
+      448h64M4 0v320M68 0v320M132 0v320M196 0v320M260 0v320M324 0v320M388 0v192M388 256v64M452
+      0v384M516 0v448M580 0v320M580 384v64M644 0v320M708 0v320M772 0v256M772 320v64M836 0v192M836
+      320v64M900 0v320M964 0v320M1028 0v256M1092 0v256M1156 0v320M1220 0v320M1284 0v256M1348 0v320M1412
+      0v320M1476 0v320M1540 0v320M1604 0v320M1668 0v320M1732 0v320M1796 0v320M1860 0v320"
+    />
   </svg>
 </template>
 
@@ -79,5 +35,21 @@ export default class AppNarrowHeaderGrid extends Vue {}
   top: 0;
   transform: translate(-50%, 0);
   z-index:-1;
+
+  &__lines {
+    stroke-dasharray: 2000;
+    stroke-dashoffset: -2000;
+    animation: draw-lines 3s;
+    animation-fill-mode: forwards;
+  }
+}
+
+@keyframes draw-lines{
+  from {
+    stroke-dashoffset: -2000;
+  }
+  to {
+    stroke-dashoffset: 0;
+  }
 }
 </style>
