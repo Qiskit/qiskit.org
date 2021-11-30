8e23825f3350c12cbbe8bf2446140004d8d8a4dd
randytolentino <randytolentino@ibm.com>
2021-11-30 11:52:39 -0800

    update homepage layouot

---
+++ components/landing/TheQiskitCapabilitiesSection/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <article class="bx--grid page-section qiskit-capabilities-section">
+    <h2 v-text="title" />
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component, Prop } from 'vue-property-decorator'
+
+
+@Component
+export default class TheQiskitCapabilitiesSection extends Vue {
+  @Prop(String) title!: string
+  @Prop(String) description!: string
+}
+</script>
+
+<style lang="scss" scoped>
+.qiskit-capabilities-section {
+  // todo: add styles
+}
+</style>
