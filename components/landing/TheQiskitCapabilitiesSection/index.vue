1d5cf39d66ac65fb0c688d61d85219237b073b55
Randy <randytolentino@ibm.com>
2021-12-01 06:06:01 -0800

    Update components/landing/TheQiskitCapabilitiesSection/index.vue
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>

--- components/landing/TheQiskitCapabilitiesSection/index.vue
+++ components/landing/TheQiskitCapabilitiesSection/index.vue
@@ -6,7 +6,7 @@
 
 <script lang="ts">
 import Vue from 'vue'
-import { Component, Prop } from 'vue-property-decorator'
+import { Component } from 'vue-property-decorator'
 
 @Component
 export default class TheQiskitCapabilitiesSection extends Vue {
