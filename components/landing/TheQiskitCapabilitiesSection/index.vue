450a9467973a35e2f9432f159006d9060131f732
Randy <randytolentino@ibm.com>
2021-12-02 02:30:16 -0800

    feat: update homepage sections layout (#2339)
    * update homepage sections and layout
    
    * Update components/landing/TheQiskitCapabilitiesSection/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update components/landing/TheQiskitCapabilitiesSection/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update components/landing/TheQiskitCapabilitiesSection/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update components/landing/TheQiskitCapabilitiesSection/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update components/landing/TheQiskitCapabilitiesSection/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update components/landing/TheQiskitCapabilitiesSection/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update pages/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update pages/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * remove unused code
    
    * revert change to bg
    
    * update title casing
    
    * remove unused asset
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>

---
+++ components/landing/TheQiskitCapabilitiesSection/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <article class="bx--grid page-section">
+    <h2>What Can Qiskit Do</h2>
+  </article>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class TheQiskitCapabilitiesSection extends Vue {
+}
+</script>
