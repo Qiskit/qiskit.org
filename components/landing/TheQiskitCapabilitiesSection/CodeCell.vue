7ce8df7a0c537cbc9d240aa8f24eddbecfd3590b
Randy <randytolentino@ibm.com>
2021-12-21 05:56:15 -0800

    Add new qiskit capabilities section (#2361)
    * update homepage sections and layout
    
    * Update components/landing/TheQiskitCapabilitiesSection/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update components/landing/TheQiskitCapabilitiesSection/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update components/landing/TheQiskitCapabilitiesSection/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update components/landing/TheQiskitCapabilitiesSection/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update components/landing/TheQiskitCapabilitiesSection/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update components/landing/TheQiskitCapabilitiesSection/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update pages/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update pages/index.vue
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * remove unused code
    
    * revert change to bg
    
    * update title casing
    
    * remove unused asset
    
    * add landing page capabilities scroll section
    
    * add capability card link
    
    * remove unused method
    
    * add try section
    
    * add try card
    
    * update links
    
    * detect active section
    
    * update section with codcell
    
    * add dynamic highlighting
    
    * clean up responsiveness
    
    * update links
    
    * update code cell responsive styles
    
    * update styles per breakpoint
    
    * add cta section
    
    * update copy code cta
    
    * align items better
    
    * update trigger margin
    
    * update spacing
    
    * add copy icon
    
    * update copy button
    
    * update responsive styles
    
    * update spacing
    
    * update copy
    
    * fix: 1 instead of 3 non-breaking spaces
    
    * refactor: set only the property that changes
    
    * refactor: follow our conventions for `prop` definitions
    
    * style: minor spaces adjustments
    
    * Apply suggestions from code review
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * update responsiveness for code lines
    
    * remove unused style
    
    * define and reuse img width
    
    * clean styles
    
    * address PR comments
    
    * style: minor space adjustments
    
    * chore: remove unused code
    
    * chore: add prop default
    
    * chore: remove unnecessary attribute
    
    The width is already inherited from the "media" image child.
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>

--- components/landing/TheQiskitCapabilitiesSection/CodeCell.vue
+++ components/landing/TheQiskitCapabilitiesSection/CodeCell.vue
@@ -1,25 +1,27 @@
 <template>
   <code class="code-cell">
     <div class="code-cell__block" :class="{'code-cell__block_active' : activeBlocks[0]}">
+      <pre>import qiskit</pre>
+      <pre>&nbsp;</pre>
       <pre><span class="code-cell__comment"># Qiskit quantum circuits libraries</span></pre>
-      <pre>quantum_circuit = qiskit.circuit.library.QuantumVolume(5) <span class="code-cell__comment">#1</span> <span class="code-cell__comment">#3</span></pre>
-      <pre>quantum_circuit.draw()</pre>
+      <pre class="code-cell__line">quantum_circuit = qiskit.circuit.library.QuantumVolume(5) <span class="code-cell__comment">#1</span> <span class="code-cell__comment">#3</span></pre>
+      <pre class="code-cell__line">quantum_circuit.draw()</pre>
     </div>
     <div class="code-cell__block" :class="{'code-cell__block_active' : activeBlocks[1]}">
       <pre><span class="code-cell__comment"># prepare your circuit to run</span></pre>
       <pre>from qiskit import IBMQ <span class="code-cell__comment">#2</span></pre>
-      <pre> &nbsp; </pre>
-      <pre>provider = IBMQ.load_account() <span class="code-cell__comment">#3</span></pre>
-      <pre>backend = provider.get_backend(<span class="code-cell__string">'ibmq_santiago'</span>) <span class="code-cell__comment">#4</span></pre>
-      <pre> &nbsp; </pre>
-      <pre>optimized_circuit = qiskit.transpile(quantum_circuit, backend) <span class="code-cell__comment">#5</span></pre>
-      <pre>optimized_circuit.draw()</pre>
+      <pre>&nbsp;</pre>
+      <pre class="code-cell__line">provider = IBMQ.load_account() <span class="code-cell__comment">#3</span></pre>
+      <pre class="code-cell__line">backend = provider.get_backend(<span class="code-cell__string">'ibmq_santiago'</span>) <span class="code-cell__comment">#4</span></pre>
+      <pre>&nbsp;</pre>
+      <pre class="code-cell__line">optimized_circuit = qiskit.transpile(quantum_circuit, backend) <span class="code-cell__comment">#5</span></pre>
+      <pre class="code-cell__line">optimized_circuit.draw()</pre>
     </div>
     <div class="code-cell__block" :class="{'code-cell__block_active' : activeBlocks[2]}">
       <pre><span class="code-cell__comment"># run in real hardware</span></pre>
-      <pre>job = backend.run(optimized_circuit)</pre>
-      <pre>retrieved_job = backend.retrieve_job(job.job_id())</pre>
-      <pre>result = retrieved_job.result() <span class="code-cell__comment">#6</span></pre>
+      <pre class="code-cell__line">job = backend.run(optimized_circuit)</pre>
+      <pre class="code-cell__line">retrieved_job = backend.retrieve_job(job.job_id())</pre>
+      <pre class="code-cell__line">result = retrieved_job.result() <span class="code-cell__comment">#6</span></pre>
     </div>
   </code>
 </template>
@@ -50,7 +52,11 @@ export default class CodeCell extends Vue {
 
     &_active {
       opacity: 1;
-      border-left: 0.125rem solid $border-active-color;
+      border-color: $border-active-color;
+    }
+
+    @include mq($until: medium) {
+      border-color: transparent;
     }
   }
 
@@ -58,6 +64,10 @@ export default class CodeCell extends Vue {
   &__string {
     color: $text-active-color;
   }
-}
 
+  &__line {
+    word-break: break-word;
+    white-space: normal;
+  }
+}
 </style>
