d11eff63bd13cf77e732c5a9e1293d8d2454f383
lerongil <leron_1234@yahoo.com>
2022-03-02 07:14:56 -0800

    feat: update code cell in home page (#2502)
    Fix issue #2501

--- components/landing/TheQiskitCapabilitiesSection/CodeCell.vue
+++ components/landing/TheQiskitCapabilitiesSection/CodeCell.vue
@@ -5,14 +5,16 @@
       <pre>&nbsp;</pre>
       <pre><span class="code-cell__comment"># Qiskit quantum circuits libraries</span></pre>
       <pre class="code-cell__line">quantum_circuit = qiskit.circuit.library.QuantumVolume(5) <span class="code-cell__comment">#1</span> <span class="code-cell__comment">#3</span></pre>
+      <pre class="code-cell__line">quantum_circuit.measure_all()</pre>
       <pre class="code-cell__line">quantum_circuit.draw()</pre>
     </div>
     <div class="code-cell__block" :class="{'code-cell__block_active' : activeBlocks[1]}">
       <pre><span class="code-cell__comment"># prepare your circuit to run</span></pre>
       <pre>from qiskit import IBMQ <span class="code-cell__comment">#2</span></pre>
+      <pre>IBMQ.save_account("YOUR TOKEN")<span class="code-cell__comment">#Get the API token in https://quantum-computing.ibm.com/</span></pre>
       <pre>&nbsp;</pre>
       <pre class="code-cell__line">provider = IBMQ.load_account() <span class="code-cell__comment">#3</span></pre>
-      <pre class="code-cell__line">backend = provider.get_backend(<span class="code-cell__string">'ibmq_santiago'</span>) <span class="code-cell__comment">#4</span></pre>
+      <pre class="code-cell__line">backend = provider.get_backend(<span class="code-cell__string">'ibmq_quito'</span>) <span class="code-cell__comment">#4</span></pre>
       <pre>&nbsp;</pre>
       <pre class="code-cell__line">optimized_circuit = qiskit.transpile(quantum_circuit, backend) <span class="code-cell__comment">#5</span></pre>
       <pre class="code-cell__line">optimized_circuit.draw()</pre>
@@ -22,6 +24,7 @@
       <pre class="code-cell__line">job = backend.run(optimized_circuit)</pre>
       <pre class="code-cell__line">retrieved_job = backend.retrieve_job(job.job_id())</pre>
       <pre class="code-cell__line">result = retrieved_job.result() <span class="code-cell__comment">#6</span></pre>
+      <pre class="code-cell__line">print(result.get_counts()) <span class="code-cell__comment">#7</span></pre>
     </div>
   </code>
 </template>
