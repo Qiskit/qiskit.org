de9df7abc16a38e8ee726f298a04a2a0f3c1706b
techtolentino <makertolentino@gmail.com>
2022-05-06 06:55:22 -0700

    extend external recommended readings component

--- components/textbook-beta/ExternalRecommendedReadings.vue
+++ components/textbook-beta/ExternalRecommendedReadings.vue
@@ -3,6 +3,13 @@
     <h2>
       External recommended readings
     </h2>
+    <p
+      v-for="reference in references"
+      :key="reference.label"
+      class="external-recommended-readings__reference"
+    >
+      {{ reference }}
+    </p>
     <AppLink
       v-for="link in links"
       :key="link.label"
@@ -23,15 +30,22 @@ import { GeneralLink } from '~/constants/appLinks'
 @Component
 export default class ExternalRecommendedReadings extends Vue {
   @Prop({ type: Array, required: true }) links!: GeneralLink[]
+  @Prop({ type: Array, required: false }) references!: GeneralLink[]
 }
 </script>
 
 <style lang="scss" scoped>
+@import '~carbon-components/scss/globals/scss/typography';
+
 .external-recommended-readings {
   &__link {
     display: block;
     margin-bottom: $spacing-01;
     width: fit-content;
+  }
+
+  &__reference {
+    margin-bottom: $spacing-01;
   }
 }
 </style>
