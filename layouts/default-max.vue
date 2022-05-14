9a62c4af246ffde2e7c206481429e11540c7d2e3
va barbosa <va@us.ibm.com>
2022-05-14 06:34:22 -0400

    banner on home page only
    update front page banner

--- layouts/default-max.vue
+++ layouts/default-max.vue
@@ -3,18 +3,6 @@
   <div tabindex="-1">
     <header id="navigation">
       <TheMenu @change-visibility="isMenuShown = $event === 'shown'" />
-      <QiskitBanner padding-x-none class="qiskit-runtime">
-        <div class="bx--grid">
-          <span class="banner-title">Start building with Qiskit runtime.</span>
-          <span class="banner-message">Leverage the new programming model and execution framework to efficiently execute circuits.</span>
-          <AppLink
-            :segment="{ action: `banner > qiskit-runtime` }"
-            url="https://qiskit.org/documentation/partners/qiskit_ibm_runtime/"
-          >
-            Learn more
-          </AppLink>
-        </div>
-      </QiskitBanner>
     </header>
     <nuxt />
     <PageFooter theme="light" />
@@ -24,36 +12,9 @@
 <script lang="ts">
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
-import QiskitBanner from '@qiskit-community/qiskit-vue/src/components/banner/Banner.vue'
 
-@Component({
-  components: { QiskitBanner }
-})
+@Component
 export default class MaxLayout extends Vue {
   isMenuShown: boolean = false
 }
 </script>
-
-<style lang="scss">
-@import "@carbon/colors/scss/colors";
-
-.qiskit-banner.qiskit-runtime {
-  background-color: $background-color-lighter !important;
-
-  .banner-title {
-    color: $cool-gray-100;
-    font-weight: 600;
-  }
-
-  .banner-message {
-    color: $cool-gray-100;
-  }
-
-  .app-link,
-  .app-link:visited {
-    color: $active-color !important;
-    text-decoration: none;
-    float: right;
-  }
-}
-</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
   <main class="landing-page">
+    <QiskitBanner padding-x-none class="qiskit-runtime">
+      <div class="bx--grid">
+        <span class="banner-title">Start building with Qiskit runtime.</span>
+        <span class="banner-message">Leverage the new programming model and execution framework to efficiently execute circuits.</span>
+        <AppLink
+          :segment="{ action: `banner > qiskit-runtime` }"
+          url="https://qiskit.org/documentation/partners/qiskit_ibm_runtime/"
+        >
+          Learn more
+        </AppLink>
+      </div>
+    </QiskitBanner>
     <TheHeroMoment :version="qiskitVersion" />
     <TheQuickStart />
     <TheQiskitCapabilitiesSection />
@@ -11,6 +23,7 @@
 import axios from 'axios'
 import { Component } from 'vue-property-decorator'
 import QiskitPage from '~/components/logic/QiskitPage.vue'
+import QiskitBanner from '@qiskit-community/qiskit-vue/src/components/banner/Banner.vue'
 
 @Component({
   head () {
@@ -23,9 +36,34 @@ import QiskitPage from '~/components/logic/QiskitPage.vue'
     return {
       qiskitVersion: packageInfo.info.version
     }
-  }
+  },
+  components: { QiskitBanner }
 })
 export default class LandingPage extends QiskitPage {
   routeName = 'qiskit-landing-page'
 }
 </script>
+
+<style lang="scss">
+@import "@carbon/colors/scss/colors";
+
+.qiskit-banner.qiskit-runtime {
+  background-color: $background-color-lighter !important;
+
+  .banner-title {
+    color: $cool-gray-100;
+    font-weight: 600;
+  }
+
+  .banner-message {
+    color: $cool-gray-100;
+  }
+
+  .app-link,
+  .app-link:visited {
+    color: $active-color !important;
+    text-decoration: none;
+    float: right;
+  }
+}
+</style>
