1b12f1daf0206d6622f869cd4b97602c3d3fa046
//va <vabarbosa@users.noreply.github.com>
2022-05-16 09:48:11 -0400

    feat: runtime banner (#2594)
    * add runtime banner
    
    * background color
    
    * banner on home page only
    
    update front page banner
    
    * lint error
    
    * update classname attribute

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
   <main class="landing-page">
+    <QiskitBanner padding-x-none class="qiskit-runtime">
+      <div class="bx--grid">
+        <span class="banner__title">Start building with Qiskit runtime.</span>
+        <span class="banner__message">Leverage the new programming model and execution framework to efficiently execute circuits.</span>
+        <AppLink
+          :segment="{ action: `banner > qiskit-runtime` }"
+          url="https://qiskit.org/documentation/partners/qiskit_ibm_runtime/"
+        >
+          Learn more
+        </AppLink>
+      </div>
+    </QiskitBanner>
     <TheHeroMoment :version="qiskitVersion" />
     <TheQuickStart />
     <TheQiskitCapabilitiesSection />
@@ -10,6 +22,7 @@
 <script lang="ts">
 import axios from 'axios'
 import { Component } from 'vue-property-decorator'
+import QiskitBanner from '@qiskit-community/qiskit-vue/src/components/banner/Banner.vue'
 import QiskitPage from '~/components/logic/QiskitPage.vue'
 
 @Component({
@@ -23,9 +36,34 @@ import QiskitPage from '~/components/logic/QiskitPage.vue'
     return {
       qiskitVersion: packageInfo.info.version
     }
-  }
+  },
+  components: { QiskitBanner }
 })
 export default class LandingPage extends QiskitPage {
   routeName = 'qiskit-landing-page'
 }
 </script>
+
+<style lang="scss">
+@import "@carbon/colors/scss/colors";
+
+.qiskit-banner.qiskit-runtime {
+  background-color: $background-color-lighter !important;
+
+  .banner__title {
+    color: $cool-gray-100;
+    font-weight: 600;
+  }
+
+  .banner__message {
+    color: $cool-gray-100;
+  }
+
+  .app-link,
+  .app-link:visited {
+    color: $active-color !important;
+    text-decoration: none;
+    float: right;
+  }
+}
+</style>
