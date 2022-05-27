ca4eeb7ef837ed08fd171f7138e67c948b27e430
techtolentino <makertolentino@gmail.com>
2022-05-27 11:51:58 -0700

    implement mega-menu-dropdown web component

--- components/learn/LearnContentMenuSection.vue
+++ components/learn/LearnContentMenuSection.vue
@@ -1,12 +1,9 @@
 <template>
   <section class="content-menu-section">
     <div class="bx--grid">
-      <QiskitMegaMenuDropdown
-        :content="dropdownMenuContent"
-        input-field-classes="bx--col-md-4 bx--col-lg-4"
-        segment-component-name="Textbook mega menu"
-        :track-performed-search="trackPerformedSearch"
-      />
+      <div class="bx--row">
+        <qiskit-mega-menu-dropdown :content.prop="dropdownMenuContent" class="learn-header__dropdown bx--col-md-4 bx--col-lg-4 bx--no-gutter" />
+      </div>
     </div>
   </section>
 </template>
@@ -14,12 +11,10 @@
 <script lang="ts">
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
-import QiskitMegaMenuDropdown from '@qiskit-community/qiskit-vue/src/components/mega-menu-dropdown/MegaMenuDropdown.vue'
+import '@qiskit/web-components/components/mega-menu-dropdown'
 import { LEARN_MEGA_MENU } from '~/constants/megaMenuLinks'
 
-@Component({
-  components: { QiskitMegaMenuDropdown }
-})
+@Component({})
 export default class LearnContentMenuSection extends Vue {
   dropdownMenuContent = LEARN_MEGA_MENU
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -44,6 +44,12 @@ md.use(miAnchor, {
 const config: NuxtConfig = {
   target: 'static',
 
+  // Disable Server Side rendering
+  ssr: false,
+
+  // Inline server bundle dependencies
+  standalone: true,
+
   env: {
     analyticsScriptUrl: IS_PRODUCTION
       ? 'https://cloud.ibm.com/analytics/build/bluemix-analytics.min.js'
@@ -167,6 +173,10 @@ const config: NuxtConfig = {
             return md.render(body)
           }
         }
+      })
+      config.module.rules.push({
+        test: /\.js$/,
+        loader: require.resolve('@open-wc/webpack-import-meta-loader')
       })
     },
 
