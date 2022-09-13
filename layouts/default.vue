8f44e4455c7d11e52f917879b224edfc6ea1a690
Yoshie Muranaka <yoshiemuranaka@gmail.com>
2022-09-13 09:47:26 -0700

    feat: update layouts to use ui-shell
    Updates default and default-max layouts to use ui-shell
    instead of TheMenu component
    Removes TheMenu component since no longer used

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,10 @@
 <template>
   <!-- tabindex is needed to allow hiding the menu in iOS Safari -->
   <div tabindex="-1">
-    <header id="navigation">
-      <TheMenu
-        old-container
-        @change-visibility="isMenuShown = $event === 'shown'"
-      />
-    </header>
-    <nuxt />
+    <qiskit-ui-shell variant="hide-account" />
+    <div class="main-container">
+      <nuxt />
+    </div>
     <PageFooter old-container theme="light" />
   </div>
 </template>
@@ -15,9 +12,14 @@
 <script lang="ts">
 import Vue from 'vue'
 import { Component } from 'vue-property-decorator'
+import '@qiskit/web-components/components/ui-shell'
 
 @Component
-export default class DefaultLayout extends Vue {
-  isMenuShown: boolean = false
-}
+export default class DefaultLayout extends Vue {}
 </script>
+
+<style lang="scss">
+.main-container {
+  margin-top: 3.5rem;
+}
+</style>
