92ac441d9e748f1218893c030d6c11845430e7f4
techtolentino <makertolentino@gmail.com>
2022-09-16 12:48:43 -0700

    add details to university event cards

--- components/events/fall-fest/UniversityDirectorySection.vue
+++ components/events/fall-fest/UniversityDirectorySection.vue
@@ -8,7 +8,7 @@
       <div class="bx--col-xlg-12 bx--col-lg-12">
         <div class="bx--row">
           <div
-            v-for="{ image, title, description, cta } in universities"
+            v-for="{ image, title, date, cta, detail } in universities"
             :key="title"
             class="bx--col-xlg-8"
           >
@@ -21,7 +21,12 @@
               class="university-directory-section__card"
               :description-whole-size="true"
             >
-              {{ description }}
+              <p>
+                {{ date }}
+                <!-- eslint-disable-next-line vue/html-self-closing -->
+                <br />
+                {{ detail }}
+              </p>
             </AppCard>
           </div>
         </div>
@@ -38,7 +43,8 @@ import { GeneralLink } from '~/constants/appLinks'
 type University = {
   image: string,
   title: string,
-  description: string,
+  detail: string,
+  date: string,
   cta: GeneralLink
 }
 
@@ -48,9 +54,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Uttar Pradesh',
       image: '/images/events/fall-fest/image30.png',
-      description: 'October 22, 2022',
+      date: 'October 22, 2022',
+      detail: 'Online',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'uttar-pradesh', location: 'fall-fest-page' }
       }
@@ -58,9 +65,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'The Higher National School of Computer Science',
       image: '/images/events/fall-fest/image27.png',
-      description: 'October 21, 2022',
+      date: 'October 21, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'the-higher-national-school-of-computer-science', location: 'fall-fest-page' }
       }
@@ -68,9 +76,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'RV PU College',
       image: '/images/events/fall-fest/image18.png',
-      description: 'October 30, 2022',
+      date: 'October 30, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'rv-pu-college', location: 'fall-fest-page' }
       }
@@ -78,9 +87,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'University of Victoria',
       image: '/images/events/fall-fest/image29.png',
-      description: 'October 22, 2022',
+      date: 'October 22, 2022',
+      detail: 'Hybrid',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'university-of-victoria', location: 'fall-fest-page' }
       }
@@ -88,9 +98,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Universidad Simón Bolívar',
       image: '/images/events/fall-fest/image24.png',
-      description: 'October 21, 2022',
+      date: 'October 21, 2022',
+      detail: 'Online',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'universidad-simón-bolívar', location: 'fall-fest-page' }
       }
@@ -98,9 +109,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Coimbatore Institute of Technology, India',
       image: '/images/events/fall-fest/image41.png',
-      description: 'October 6, 2022',
+      date: 'October 6, 2022',
+      detail: 'Online',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'coimbatore-institute-of-Technology-india', location: 'fall-fest-page' }
       }
@@ -108,9 +120,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Quantum Fall Fest QColombia',
       image: '/images/events/fall-fest/image20.png',
-      description: 'October 10, 2022',
+      date: 'October 10, 2022',
+      detail: 'Online',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'quantum-fall-fest-qcolombia', location: 'fall-fest-page' }
       }
@@ -118,9 +131,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Yale University',
       image: '/images/events/fall-fest/image31.png',
-      description: 'October 3, 2022',
+      date: 'October 3, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'yale-university', location: 'fall-fest-page' }
       }
@@ -128,9 +142,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'QickStart : A Qiskit Powered Intro to Quantum Computing',
       image: '/images/events/fall-fest/image12.png',
-      description: 'October 3, 2022',
+      date: 'October 3, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'qickStart-a-qiskit-powered-intro-to-quantum-computing', location: 'fall-fest-page' }
       }
@@ -138,9 +153,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'The University of Hong Kong',
       image: '/images/events/fall-fest/image26.png',
-      description: 'October 25, 2022',
+      date: 'October 25, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'the-university-of-hong-kong', location: 'fall-fest-page' }
       }
@@ -148,9 +164,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Purdue University',
       image: '/images/events/fall-fest/image13.png',
-      description: 'September 23, 2022',
+      date: 'September 23, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'purdue-university', location: 'fall-fest-page' }
       }
@@ -158,9 +175,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Qiskit Fall Fest Kolkata Chapter',
       image: '/images/events/fall-fest/image11.png',
-      description: 'October 17, 2022',
+      date: 'October 17, 2022',
+      detail: 'Online',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'qiskit-fall-fest-kolkata-chapter', location: 'fall-fest-page' }
       }
@@ -168,9 +186,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Qiskit Fall Fest Mexico-IPN',
       image: '/images/events/fall-fest/image22.png',
-      description: 'October 17, 2022',
+      date: 'October 17, 2022',
+      detail: 'Online',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: '', location: 'fall-fest-page' }
       }
@@ -178,9 +197,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Case Western Reserve University',
       image: '/images/events/fall-fest/image40.png',
-      description: 'October 10, 2022',
+      date: 'October 10, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'qiskit-fall-fest-mexico-ipn', location: 'fall-fest-page' }
       }
@@ -188,9 +208,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Fundamentals of Quantum Computing (Thapar University)',
       image: '/images/events/fall-fest/image7.png',
-      description: 'October 15, 2022',
+      date: 'October 15, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'thapar-university', location: 'fall-fest-page' }
       }
@@ -198,9 +219,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Penn State',
       image: '/images/events/fall-fest/image14.png',
-      description: 'October 22, 2022',
+      date: 'October 22, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'penn-state', location: 'fall-fest-page' }
       }
@@ -208,9 +230,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Quest - Qiskit Fall Fest, BITS Goa Chapter',
       image: '/images/events/fall-fest/image21.png',
-      description: 'November 25, 2022',
+      date: 'November 25, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'quest-qiskit-fall-fest-bits-goa-chapter', location: 'fall-fest-page' }
       }
@@ -218,9 +241,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Indian Institute of Technology Roorkee',
       image: '/images/events/fall-fest/image16.png',
-      description: 'October 21, 2022',
+      date: 'October 21, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'indian-institute-of-technology-roorkee', location: 'fall-fest-page' }
       }
@@ -228,9 +252,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Escuela de Computación Cuántica',
       image: '/images/events/fall-fest/image8.png',
-      description: 'October 17, 2022',
+      date: 'October 17, 2022',
+      detail: 'Online',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'escuela-de-computación-cuántica', location: 'fall-fest-page' }
       }
@@ -238,9 +263,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'University of Santiago de Compostela, Spain',
       image: '/images/events/fall-fest/image23.png',
-      description: 'October 14, 2022',
+      date: 'October 14, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'university-of-santiago-de-compostela-spain', location: 'fall-fest-page' }
       }
@@ -248,9 +274,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'University of Texas at Austin',
       image: '/images/events/fall-fest/image28.png',
-      description: 'October 10, 2022',
+      date: 'October 10, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'university-of-texas-at-austin', location: 'fall-fest-page' }
       }
@@ -258,9 +285,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Izmir Institute of Technology',
       image: '/images/events/fall-fest/image15.png',
-      description: 'October 28, 2022',
+      date: 'October 28, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'izmir-institute-of-technology', location: 'fall-fest-page' }
       }
@@ -268,9 +296,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Start innovation hub',
       image: '/images/events/fall-fest/image17.png',
-      description: 'October 8, 2022',
+      date: 'October 8, 2022',
+      detail: 'In Person',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'start-innovation-hub', location: 'fall-fest-page' }
       }
@@ -278,9 +307,10 @@ export default class UniversityDirectorySection extends Vue {
     {
       title: 'Quantastica, Fantastica',
       image: '/images/events/fall-fest/image19.png',
-      description: 'October 22, 2022',
+      date: 'October 22, 2022',
+      detail: 'Online',
       cta: {
-        label: 'Go to this page',
+        label: 'Learn more here',
         url: 'https://coding-club.example-university.edu',
         segment: { cta: 'quantastica-fantastica', location: 'fall-fest-page' }
       }
