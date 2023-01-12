cd3db12faeb8349e03cfa9d3503d89011f836129
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-01-12 16:40:29 +0100

    fix: replace hardcoded field name

--- hooks/event-conversion-utils.ts
+++ hooks/event-conversion-utils.ts
@@ -367,7 +367,7 @@ function dateParts (date: Date): [string, string, string] {
 }
 
 function getWebsite (record: any): string {
-  return record.get('Website')
+  return record.get(RECORD_FIELDS.eventWebsite)
 }
 
 export {
