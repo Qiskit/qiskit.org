da49330a452b354ad42db21c3cfbec2584c4d1e1
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-01-13 13:15:11 +0100

    refactor: rewrite as class

--- hooks/advocate-conversion-utils.ts
+++ hooks/advocate-conversion-utils.ts
@@ -22,84 +22,82 @@ const RECORD_FIELDS_IDS = Object.freeze({
   slackUsername: 'fldY1nP63OKVsdvRC'
 } as const)
 
-let RECORD_FIELDS: Record<string, any>
+class AdvocateAirtableAPI {
+  private _airtableBaseId = 'app8koO4BZifGFhCV'
+  private _apiKey: string
+  private _recordFields?: Record<string, any>
 
-const airtableBaseId = 'app8koO4BZifGFhCV'
-
-async function fetchAdvocates (apiKey: string): Promise<Advocate[]> {
-  if (!RECORD_FIELDS) {
-    RECORD_FIELDS = await getAllFieldNames(apiKey, airtableBaseId, 'Advocates', 'For website', RECORD_FIELDS_IDS)
+  constructor (apiKey: string, recordFields?: Record<string, any>) {
+    this._apiKey = apiKey
+    this._recordFields = recordFields
   }
 
-  const { slackId } = RECORD_FIELDS
-  const advocates: Advocate[] = []
-  const base = new Airtable({ apiKey }).base(airtableBaseId)
-  await base('Advocates').select({
-    fields: Object.values(RECORD_FIELDS),
-    filterByFormula: `AND({${slackId}})`,
-    sort: [{ field: RECORD_FIELDS.name, direction: 'asc' }]
-  }).eachPage((records, nextPage) => {
-    for (const record of records) {
-      const advocate = convertToAdvocate(record)
-      advocates.push(advocate)
+  async fetchAdvocates (): Promise<Advocate[]> {
+    if (!this._recordFields) {
+      this._recordFields = await getAllFieldNames(this._apiKey, this._airtableBaseId, 'Advocates', 'For website', RECORD_FIELDS_IDS)
     }
-    nextPage()
-  })
-  return Promise.resolve(advocates)
-}
 
-function convertToAdvocate (record: any): Advocate {
-  return {
-    name: getName(record),
-    image: getImage(record),
-    region: getRegion(record),
-    city: getCity(record),
-    country: getCountry(record),
-    slackId: getSlackId(record),
-    slackUsername: getSlackUsername(record)
+    const { slackId } = this._recordFields
+    const advocates: Advocate[] = []
+    const base = new Airtable({ apiKey: this._apiKey }).base(this._airtableBaseId)
+
+    await base('Advocates').select({
+      fields: Object.values(this._recordFields),
+      filterByFormula: `AND({${slackId}})`,
+      sort: [{ field: this._recordFields.name, direction: 'asc' }]
+    }).eachPage((records, nextPage) => {
+      for (const record of records) {
+        const advocate = this.convertToAdvocate(record)
+        advocates.push(advocate)
+      }
+      nextPage()
+    })
+    return Promise.resolve(advocates)
+  }
+
+  convertToAdvocate (record: any): Advocate {
+    return {
+      name: this.getName(record),
+      image: this.getImage(record),
+      region: this.getRegion(record),
+      city: this.getCity(record),
+      country: this.getCountry(record),
+      slackId: this.getSlackId(record),
+      slackUsername: this.getSlackUsername(record)
+    }
+  }
+
+  public getName (record: any): string {
+    return record.get(this._recordFields!.name)
+  }
+
+  public getImage (record: any): string {
+    const fallbackImage = '/images/advocates/no-advocate-photo.png'
+    const attachments = record.get(this._recordFields!.image)
+    const imageAttachment = attachments && findImageAttachment(attachments)
+    const imageUrl = imageAttachment && getImageUrl(imageAttachment)
+    return imageUrl || fallbackImage
+  }
+
+  public getCity (record: any): string {
+    return record.get(this._recordFields!.city)
+  }
+
+  public getCountry (record: any): string {
+    return record.get(this._recordFields!.country)
+  }
+
+  public getRegion (record: any): AdvocatesWorldRegion {
+    return record.get(this._recordFields!.region)
+  }
+
+  public getSlackId (record: any): string {
+    return record.get(this._recordFields!.slackId)
+  }
+
+  public getSlackUsername (record: any): string {
+    return record.get(this._recordFields!.slackUsername)
   }
 }
 
-function getName (record: any): string {
-  return record.get(RECORD_FIELDS.name)
-}
-
-function getImage (record: any): string {
-  const fallbackImage = '/images/advocates/no-advocate-photo.png'
-  const attachments = record.get(RECORD_FIELDS.image)
-  const imageAttachment = attachments && findImageAttachment(attachments)
-  const imageUrl = imageAttachment && getImageUrl(imageAttachment)
-  return imageUrl || fallbackImage
-}
-
-function getCity (record: any): string {
-  return record.get(RECORD_FIELDS.city)
-}
-
-function getCountry (record: any): string {
-  return record.get(RECORD_FIELDS.country)
-}
-
-function getRegion (record: any): AdvocatesWorldRegion {
-  return record.get(RECORD_FIELDS.region)
-}
-
-function getSlackId (record: any): string {
-  return record.get(RECORD_FIELDS.slackId)
-}
-
-function getSlackUsername (record: any): string {
-  return record.get(RECORD_FIELDS.slackUsername)
-}
-
-export {
-  fetchAdvocates,
-  convertToAdvocate,
-  getName,
-  getImage,
-  getCity,
-  getCountry,
-  getRegion,
-  getSlackId,
-  getSlackUsername
-}
+export default AdvocateAirtableAPI
--- hooks/update-advocates.ts
+++ hooks/update-advocates.ts
@@ -1,10 +1,11 @@
 import fs from 'fs'
 import util from 'util'
 
-import { fetchAdvocates } from './advocate-conversion-utils'
+import AdvocateAirtableAPI from './advocate-conversion-utils'
 
 export default async function (apiKey: any, outputFolder: string) {
-  const advocates = await fetchAdvocates(apiKey)
+  const advocateAirtableAPI = new AdvocateAirtableAPI(apiKey)
+  const advocates = await advocateAirtableAPI.fetchAdvocates()
 
   const writeFile = util.promisify(fs.writeFile)
   await writeFile(`${outputFolder}/advocates.json`, JSON.stringify(advocates, null, 2))
