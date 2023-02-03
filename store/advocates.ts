7fc9d61003a08375b1cbeaedc59d84c65fbdf2c6
Yaiza <yaiza.garcia.mm@gmail.com>
2023-02-03 12:22:54 +0100

    Migrates Advocates store from Vuex to Pinia

--- store/advocates.ts
+++ store/advocates.ts
@@ -1,4 +1,4 @@
-import type { ActionTree, GetterTree, MutationTree } from 'vuex'
+import { defineStore } from 'pinia'
 
 const ADVOCATES_WORLD_REGIONS = Object.freeze({
   northAmerica: 'America (North)',
@@ -34,48 +34,28 @@ export {
   Advocate
 }
 
-export class State {
-  advocates: Advocate[] = []
-  regionFilters: string[] = []
-}
+export const useAdvocatesStore = defineStore(
+  'advocates',
+  {
+    state: () => ({
+      advocates: [] as Advocate[],
+      regionFilters: [] as string[]
+    }),
+    getters: {
+      filteredAdvocates: ({ advocates, regionFilters }) => {
+        const noRegionFilters = regionFilters.length === 0
 
-const getters = <GetterTree<State, any>> {
-  /**
-   * List of advocates filtered by selected regions.
-   */
-  filteredAdvocates ({ advocates, regionFilters }): Advocate[] {
-    const noRegionFilters = regionFilters.length === 0
+        if (noRegionFilters) {
+          return advocates
+        }
 
-    if (noRegionFilters) {
-      return advocates
+        return advocates.filter(advocate => regionFilters.includes(advocate.region))
+      }
+    },
+    actions: {
+      async fetchAdvocates () {
+        this.advocates = await import('~/content/advocates/advocates.json') as Advocate[] || []
+      }
     }
-
-    return advocates.filter(advocate => regionFilters.includes(advocate.region))
   }
-}
-
-const mutations = <MutationTree<State>> {
-  setAdvocates (state, advocates: Advocate[]) {
-    state.advocates = advocates
-  },
-
-  setRegionFilters (state, regionFilters: string[]) {
-    state.regionFilters = regionFilters
-  }
-}
-
-const actions = <ActionTree<State, any>> {
-  async fetchAdvocates ({ commit }): Promise<void> {
-    const advocatesModule = await import('~/content/advocates/advocates.json')
-    const advocates = advocatesModule.default || []
-    commit('setAdvocates', advocates)
-  }
-}
-
-export default {
-  namespaced: true,
-  state: new State(),
-  actions,
-  mutations,
-  getters
-}
+)
