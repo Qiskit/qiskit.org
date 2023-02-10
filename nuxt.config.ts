a7e0a5fe80fc5617190507cb1ad3b6a7406f7731
Yaiza <yaiza.garcia.mm@gmail.com>
2023-02-10 13:35:43 +0100

    feat: adjust nuxt configuration (#2967)
    * Uses defineNuxtConfig
    
    * Moves buildModules into modules
    
    * Deletes unsupported modules
    
    * Updates tsconfig.json file
    
    * Uses runtimeConfig on nuxt.config.ts
    
    * Deletes unused configurations
    
    * Set the default components config
    
    * Deletes config for content in markdown
    
    * Adds TODO comment
    
    * Updates engines on package.json
    
    * Updates package-lock
    
    * Comment code to avoid import errors and warnings
    
    * Comment nuxt config code to try to run the app
    
    * Downgrade node to 16 again

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,13 @@ import path from 'path'
 import fs from 'fs'
 import consola from 'consola'
 
-import markdownIt from 'markdown-it'
-import miLinkAttributes from 'markdown-it-link-attributes'
-import miAnchor from 'markdown-it-anchor'
-import uslug from 'uslug'
-import Mode from 'frontmatter-markdown-loader/mode'
+// TODO: Deletes markdown references. We are not using md as content anymore
+// import markdownIt from 'markdown-it'
+// import miLinkAttributes from 'markdown-it-link-attributes'
+// import miAnchor from 'markdown-it-anchor'
+// import uslug from 'uslug'
+// import Mode from 'frontmatter-markdown-loader/mode'
 
-import { NuxtConfig } from '@nuxt/types'
 import pkg from './package.json'
 import fetchEvents from './hooks/update-events'
 import fetchAdvocates from './hooks/update-advocates'
@@ -25,66 +25,65 @@ const {
 
 const IS_PRODUCTION = NODE_ENV === 'production'
 
-const md = markdownIt({
-  linkify: true,
-  html: true
-})
+// const md = markdownIt({
+//   linkify: true,
+//   html: true
+// })
 
-md.use(miLinkAttributes, {
-  pattern: /^https?:/,
-  attrs: {
-    target: '_blank',
-    rel: 'noopener'
-  }
-})
+// md.use(miLinkAttributes, {
+//   pattern: /^https?:/,
+//   attrs: {
+//     target: '_blank',
+//     rel: 'noopener'
+//   }
+// })
 
-md.use(miAnchor, {
-  slugify (id: any) { return uslug(id) }
-})
+// md.use(miAnchor, {
+//   slugify (id: any) { return uslug(id) }
+// })
 
-const config: NuxtConfig = {
-  target: 'static',
+export default defineNuxtConfig({
+  // target: 'static',
 
   // Disable Server Side rendering
   ssr: false,
 
   // Inline server bundle dependencies
-  standalone: true,
+  // standalone: true,
 
-  env: {
-    analyticsScriptUrl: IS_PRODUCTION
-      ? 'https://cloud.ibm.com/analytics/build/bluemix-analytics.min.js'
-      : 'https://dev.console.test.cloud.ibm.com/analytics/build/bluemix-analytics.min.js',
-    analyticsKey: IS_PRODUCTION
-      ? 'ffdYLviQze3kzomaINXNk6NwpY9LlXcw'
-      : 'zbHWEXPUfXm0K6C7HbegwB5ewDEC8o1H'
-  },
-
-  publicRuntimeConfig: {
-    baseURL: IS_PRODUCTION
-      ? 'https://qiskit.org'
-      : 'localhost:3000'
+  runtimeConfig: {
+    // Keys within public are also exposed client-side
+    public: {
+      analyticsScriptUrl: IS_PRODUCTION
+        ? 'https://cloud.ibm.com/analytics/build/bluemix-analytics.min.js'
+        : 'https://dev.console.test.cloud.ibm.com/analytics/build/bluemix-analytics.min.js',
+      analyticsKey: IS_PRODUCTION
+        ? 'ffdYLviQze3kzomaINXNk6NwpY9LlXcw'
+        : 'zbHWEXPUfXm0K6C7HbegwB5ewDEC8o1H',
+    }
   },
 
   /*
   ** Headers of the page
   */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+  app: {
+    head: {
+      title: pkg.name,
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: pkg.description }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      ]
+    }
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  // loading: { color: '#fff' },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
@@ -92,24 +91,17 @@ const config: NuxtConfig = {
   ],
 
   /*
-  ** Content
-  */
-  content: {
-    dir: 'new-content'
-  },
-
-  /*
   ** Plugins to load before mounting the App.
   */
   plugins: [
-    '~/plugins/router-hooks.ts',
-    '~/plugins/carbon.ts',
-    '~/plugins/deep-load.ts',
-    { src: '~/plugins/hotjar.ts', mode: 'client' },
-    ...optional(
-      IS_PRODUCTION || ENABLE_ANALYTICS,
-      { src: '~/plugins/segment-analytics.ts', mode: 'client' } as const
-    )
+    // '~/plugins/router-hooks.ts',
+    // '~/plugins/carbon.ts',
+    // '~/plugins/deep-load.ts',
+    // { src: '~/plugins/hotjar.ts', mode: 'client' },
+    // ...optional(
+    //   IS_PRODUCTION || ENABLE_ANALYTICS,
+    //   { src: '~/plugins/segment-analytics.ts', mode: 'client' } as const
+    // )
   ],
 
   /*
@@ -119,127 +111,73 @@ const config: NuxtConfig = {
     '@nuxt/content'
   ],
 
-  components: [
-    {
-      /* According to https://stackoverflow.com/questions/66336557/nuxt-not-automatically-importing-components-from-nested-directory
-      ** we need to set `pathPrefix: false` to allow the registration of nested directories
-      */
-      path: '~/components',
-      pathPrefix: false
-    }
-  ],
-
-  // Nuxt Style Resources: @nuxtjs/style-resources
-  styleResources: {
-    scss: [
-      '~/assets/scss/helpers/index.scss'
-    ]
-  },
-
-  /*
-  ** Migrating from Nuxt 2.8.x to 2.9.y
-  ** https://typescript.nuxtjs.org/migration.html
-  */
-  buildModules: [
-    ['@nuxt/typescript-build', {
-      typeCheck: true,
-      ignoreNotFoundWarnings: true
-    }],
-    '@nuxtjs/style-resources',
-    // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module'
-  ],
-
   /*
   ** Build configuration
   */
   build: {
-    /*
-    ** You can extend webpack config here
-    */
-    extend (config) {
-      config.module = config.module || { rules: [] }
-      config.module.rules.push({
-        test: /\.md$/,
-        loader: 'frontmatter-markdown-loader',
-        include: path.resolve(__dirname, 'content'),
-        options: {
-          mode: [Mode.VUE_RENDER_FUNCTIONS, Mode.VUE_COMPONENT, Mode.HTML],
-          vue: {
-            root: 'content'
-          },
-          markdown: (body: any) => {
-            return md.render(body)
-          }
-        }
-      })
-      config.module.rules.push({
-        test: /\.js$/,
-        loader: require.resolve('@open-wc/webpack-import-meta-loader')
-      })
-    },
-
     // TODO: Workaround for dealing with. Remove once its solved:
     // https://github.com/nuxt/nuxt.js/issues/3877
-    splitChunks: {
-      layouts: true
-    }
+    // splitChunks: {
+    //   layouts: true
+    // }
   },
 
   generate: {
-    routes: (function () {
-      const events = getContentUrls('events')
+    // TODO It is preferred to use nitro.prerender.routes
+    // https://nuxt.com/docs/api/configuration/nuxt-config#routes
 
-      return [...events]
+    //TODO: This is the code for the old events
+    // routes: (function () {
+    //   const events = getContentUrls('events')
 
-      function getContentUrls (contentRoot: string): string[] {
-        return fs.readdirSync(path.resolve(__dirname, 'content', contentRoot))
-          .filter(isContentAndNotReadme)
-          .map(toContentUrl(contentRoot))
-      }
+    //   return [...events]
 
-      function isContentAndNotReadme (filename: string): boolean {
-        return path.extname(filename) === '.md' &&
-               path.parse(filename).name.toUpperCase() !== 'README'
-      }
+    //   function getContentUrls (contentRoot: string): string[] {
+    //     return fs.readdirSync(path.resolve(__dirname, 'content', contentRoot))
+    //       .filter(isContentAndNotReadme)
+    //       .map(toContentUrl(contentRoot))
+    //   }
 
-      function toContentUrl (contentRoot: string): (s: string) => string {
-        return (filename: string): string => {
-          return `/${contentRoot}/${path.parse(filename).name}`
-        }
-      }
-    })()
+    //   function isContentAndNotReadme (filename: string): boolean {
+    //     return path.extname(filename) === '.md' &&
+    //            path.parse(filename).name.toUpperCase() !== 'README'
+    //   }
+
+    //   function toContentUrl (contentRoot: string): (s: string) => string {
+    //     return (filename: string): string => {
+    //       return `/${contentRoot}/${path.parse(filename).name}`
+    //     }
+    //   }
+    // })()
   },
 
   hooks: {
-    build: {
-      async before () {
-        if (!IS_PRODUCTION && !GENERATE_CONTENT) {
-          console.warn('Skipping content generation. Set GENERATE_CONTENT to enable it.')
-          return
-        }
-        await generateContent()
-      }
-    }
+    // build: {
+    //   async before () {
+    //     if (!IS_PRODUCTION && !GENERATE_CONTENT) {
+    //       console.warn('Skipping content generation. Set GENERATE_CONTENT to enable it.')
+    //       return
+    //     }
+    //     await generateContent()
+    //   }
+    // }
   }
-}
+})
 
-function optional<T> (test: any, ...plugins: T[]): T[] {
-  return test ? plugins : []
-}
+// function optional<T> (test: any, ...plugins: T[]): T[] {
+//   return test ? plugins : []
+// }
 
-async function generateContent () {
-  if (AIRTABLE_API_KEY) {
-    consola.info('Generating community event previews')
-    await fetchEvents(AIRTABLE_API_KEY, './content/events')
+// async function generateContent () {
+//   if (AIRTABLE_API_KEY) {
+//     consola.info('Generating community event previews')
+//     await fetchEvents(AIRTABLE_API_KEY, './content/events')
 
-    consola.info('Generating advocate previews')
-    await fetchAdvocates(AIRTABLE_API_KEY, './content/advocates')
-  } else {
-    consola.warn('Cannot generate events: missing AIRTABLE_API_KEY environment variable')
-  }
+//     consola.info('Generating advocate previews')
+//     await fetchAdvocates(AIRTABLE_API_KEY, './content/advocates')
+//   } else {
+//     consola.warn('Cannot generate events: missing AIRTABLE_API_KEY environment variable')
+//   }
 
-  await fetchEcosystemMembers('./content/ecosystem')
-}
-
-export default config
+//   await fetchEcosystemMembers('./content/ecosystem')
+// }
