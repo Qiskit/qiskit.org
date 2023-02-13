547ae41ca50f1711dff4728acc36b5e97f599621
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-02-13 15:56:21 +0100

    chore: move learn/course pages

---
+++ pages/learn/course/prerequisites.vue
@@ -0,0 +1,83 @@
+<template></template>
+
+<script setup lang="ts">
+// TODO: Integrate old code
+// import { Course, Prerequisite } from 'constants/learnContent'
+// import { Component } from 'vue-property-decorator'
+// import CourseOverviewPage from '~/components/logic/CourseOverviewPage.vue'
+// import { GeneralLink, PrerequisitesUrl } from '~/constants/appLinks'
+// import { RecommendedReading } from '~/components/learn/ExternalRecommendedReadings.vue'
+
+useHead({
+  title: 'Prerequisites'
+})
+
+// TODO: Integrate old code
+// const headerTitle = 'Prerequisites'
+// const headerDescription = ['This is a comprehensive guide for setting up your environment on your personal computer for working with Qiskit Textbook. This will help you reproduce the results as you see them on the textbook website. The Qiskit Textbook is written in Jupyter Notebooks.']
+
+// const headerImg = '/images/learn/course/prerequisites/prerequisites.png'
+
+// const backToTextbookHomeLink: GeneralLink = {
+//   url: '/learn',
+//   label: 'Home',
+//   segment: {
+//     cta: 'back-to-textbook-home', location: 'header'
+//   }
+// }
+
+// const startLearningCTA: GeneralLink = {
+//   url: PrerequisitesUrl.Introduction,
+//   label: 'Start learning',
+//   segment: {
+//     cta: 'introduction', location: 'header'
+//   }
+// }
+
+// const references: string[] = []
+// const externalRecommendedReadingsPreamble: string = ''
+// const links: RecommendedReading[] = [
+//   {
+//     url: 'https://math.mit.edu/~gs/linearalgebra/',
+//     author: 'Gilbert Strang,',
+//     label: 'Introduction to Linear Algebra',
+//     segment: {
+//       cta: 'introduction-to-linear-algebra',
+//       location: 'external-recommended-readings'
+//     }
+//   },
+//   {
+//     url: 'https://machinelearningmastery.com/gentle-introduction-linear-algebra/',
+//     author: 'Jason Brownlee,',
+//     label: 'A Gentle Introduction to Linear Algebra',
+//     segment: {
+//       cta: 'gentle-introduction-to-linear-algebra',
+//       location: 'external-recommended-readings'
+//     }
+//   }
+// ]
+
+// const courses: Course[] = [
+//   {
+//     image: 'environment-setup.png',
+//     label: 'Setting Up Your Environment',
+//     segment: { cta: 'setting-up-your-environment', location: 'course' },
+//     url: PrerequisitesUrl.SettingUp
+//   },
+//   {
+//     image: 'introduction.png',
+//     label: 'Python and Jupyter Notebooks',
+//     segment: { cta: 'python-and-jupyter-notebooks', location: 'course' },
+//     url: PrerequisitesUrl.Introduction
+//   }
+// ]
+
+// const prerequisites: Prerequisite[] = []
+
+// const imageUrlBase = '/images/learn/course/prerequisites'
+
+// TODO: Refactor "logic" pages
+// export default class PrerequisitesPage extends CourseOverviewPage {
+//   routeName: string = 'prerequisites'
+// }
+</script>
