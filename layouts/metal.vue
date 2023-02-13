2f6d7af7de2772cb882db8442796aa1996e39417
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-02-13 16:04:30 +0100

    chore: move back directories

---
+++ layouts/metal.vue
@@ -0,0 +1,38 @@
+<template>
+  <!-- TODO: Integrate old code -->
+  <!-- tabindex is needed to allow hiding the menu in iOS Safari -->
+  <!-- <div class="layout" tabindex="-1">
+    <nuxt />
+    <PageFooter theme="dark" />
+  </div> -->
+</template>
+
+<script lang="ts">
+// TODO: Integrate old code
+// import Vue from "vue";
+// import { Component } from "vue-property-decorator";
+
+// @Component
+// export default class MetalLayout extends Vue {
+//   isMenuShown: boolean = false;
+// }
+</script>
+
+<style lang="scss" scoped>
+// TODO: Integrate old code
+// .layout {
+//   background-color: $background-color-black;
+//   color: $text-color-lighter-2;
+// }
+
+// ::v-deep {
+//   h2,
+//   p {
+//     color: $text-color-lighter-2;
+//   }
+
+//   h3 {
+//     color: $text-color-white;
+//   }
+// }
+</style>
