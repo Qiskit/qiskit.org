83ae55a0f3d2ec50f45b559eab4126a4327850f2
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-02-14 17:49:10 +0100

    feat: configure ESLint and Prettier

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
+};
