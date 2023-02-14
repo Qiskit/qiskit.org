697b1297bb1b29b8dcbd54ccc1d0898fac3eefaf
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-02-14 15:10:48 +0100

    feat(layouts): migrate metal

--- layouts/metal.vue
+++ layouts/metal.vue
@@ -1,38 +1,27 @@
 <template>
-  <!-- TODO: Integrate old code -->
   <!-- tabindex is needed to allow hiding the menu in iOS Safari -->
-  <!-- <div class="layout" tabindex="-1">
-    <nuxt />
-    <PageFooter theme="dark" />
-  </div> -->
+  <div class="layout" tabindex="-1">
+    <slot />
+    <Footer theme="dark" />
+  </div>
 </template>
 
-<script setup lang="ts">
-// TODO: Integrate old code
-// import Vue from "vue";
-// import { Component } from "vue-property-decorator";
-
-// @Component
-// export default class MetalLayout extends Vue {
-//   isMenuShown: boolean = false;
-// }
-</script>
-
 <style lang="scss" scoped>
-// TODO: Integrate old code
-// .layout {
-//   background-color: qiskit.$background-color-black;
-//   color: qiskit.$text-color-lighter-2;
-// }
+@use "~/assets/scss/helpers/index.scss" as qiskit;
 
-// ::v-deep {
-//   h2,
-//   p {
-//     color: qiskit.$text-color-lighter-2;
-//   }
+.layout {
+  background-color: qiskit.$background-color-black;
+  color: qiskit.$text-color-lighter-2;
+}
 
-//   h3 {
-//     color: qiskit.$text-color-white;
-//   }
-// }
+::v-deep {
+  h2,
+  p {
+    color: qiskit.$text-color-lighter-2;
+  }
+
+  h3 {
+    color: qiskit.$text-color-white;
+  }
+}
 </style>
