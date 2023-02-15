836652a12b72adac1e2c393438161e55777b4036
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-02-15 15:19:43 +0100

    feat(lint): added env config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,15 @@
 module.exports = {
-  parser: "vue-eslint-parser",
-  parserOptions: {
-    parser: "@typescript-eslint/parser",
+  env: {
+    browser: true,
+    node: true,
   },
   extends: [
     "eslint:recommended",
     "@nuxtjs/eslint-config-typescript",
     "plugin:prettier/recommended",
   ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
 };
