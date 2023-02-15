070c361037bc88d59944f183f9f88f66548b1f3a
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-02-15 15:46:55 +0100

    style: lint files

--- composables/useScrollBetweenSections.ts
+++ composables/useScrollBetweenSections.ts
@@ -1,31 +1,28 @@
-import { ref, onMounted, onUpdated, onBeforeUnmount } from 'vue'
-import { onBeforeRouteUpdate } from 'vue-router'
+import { ref, onMounted, onUpdated, onBeforeUnmount } from "vue";
+import { onBeforeRouteUpdate } from "vue-router";
 
-export function useScrollBetweenSections () {
-  const activeSection = ref('')
-  const _observer: IntersectionObserver | null = ref(null)
+export function useScrollBetweenSections() {
+  const activeSection = ref("");
+  const _observer: IntersectionObserver | null = ref(null);
 
-  onMounted (() => {
-    const threshold = [...Array(25).keys()].map(x => 4 * x / 100)
-    const windowTriggerMargins = '-16px 0px -80% 0px'
-    this._observer = new IntersectionObserver(
-      this._onSectionAppearing,
-      {
-        root: null, // the viewport
-        rootMargin: windowTriggerMargins,
-        threshold
-      }
-    )
-    this.updateObserved()
-  })
+  onMounted(() => {
+    const threshold = [...Array(25).keys()].map((x) => (4 * x) / 100);
+    const windowTriggerMargins = "-16px 0px -80% 0px";
+    this._observer = new IntersectionObserver(this._onSectionAppearing, {
+      root: null, // the viewport
+      rootMargin: windowTriggerMargins,
+      threshold,
+    });
+    this.updateObserved();
+  });
 
-  onUpdated (() => {
-    this.$nextTick(() => this.updateObserved())
-  })
+  onUpdated(() => {
+    this.$nextTick(() => this.updateObserved());
+  });
 
-  onBeforeUnmount (() => {
-    this._observer && this._observer.disconnect()
-  })
+  onBeforeUnmount(() => {
+    this._observer && this._observer.disconnect();
+  });
 
   // TODO: This is the original code. The onBeforeRouteEnter doesn't
   // exists on Vue 3, so we need to check if the code below works as expected
@@ -38,49 +35,47 @@ export function useScrollBetweenSections () {
   //   next()
   // }
 
-  onBeforeRouteUpdate ((route: any, _: any, next: any) => {
-    this._parseSectionFromUrl(route)
-    next()
-  })
+  onBeforeRouteUpdate((route: any, _: any, next: any) => {
+    this._parseSectionFromUrl(route);
+    next();
+  });
 
-  function updateObserved () {
+  function updateObserved() {
     (this.$el as HTMLElement)
-      .querySelectorAll('.scrollable')
+      .querySelectorAll(".scrollable")
       .forEach((section) => {
-        (this._observer as IntersectionObserver).observe(section)
-      })
+        (this._observer as IntersectionObserver).observe(section);
+      });
   }
 
-  function _onSectionAppearing (entries: Array<IntersectionObserverEntry>) {
-    let highestTopValue = Infinity
+  function _onSectionAppearing(entries: Array<IntersectionObserverEntry>) {
+    let highestTopValue = Infinity;
     entries.forEach((entry) => {
-      const {
-        target,
-        boundingClientRect,
-        rootBounds
-      } = entry
-      if (!rootBounds) { return }
-      const targetTop = boundingClientRect.top
-      const triggerWindowBottom = rootBounds.bottom
-      const onTop = targetTop >= 0 && targetTop <= triggerWindowBottom
+      const { target, boundingClientRect, rootBounds } = entry;
+      if (!rootBounds) {
+        return;
+      }
+      const targetTop = boundingClientRect.top;
+      const triggerWindowBottom = rootBounds.bottom;
+      const onTop = targetTop >= 0 && targetTop <= triggerWindowBottom;
       if (onTop && targetTop < highestTopValue) {
         if (this.activeSection !== target.id) {
-          this.activeSection = target.id
-          this.activeSectionChanged()
+          this.activeSection = target.id;
+          this.activeSectionChanged();
         }
-        highestTopValue = targetTop
+        highestTopValue = targetTop;
       }
-    })
+    });
   }
 
   /**
    * This methods gets called when the active section changes.
    */
-  function activeSectionChanged () {}
+  function activeSectionChanged() {}
 
- function  _parseSectionFromUrl (route: any) {
-    this.activeSection = route.hash.substr(1)
+  function _parseSectionFromUrl(route: any) {
+    this.activeSection = route.hash.substr(1);
   }
 
-  return { activeSection }
+  return { activeSection };
 }
--- tests/composables/useSegment.spec.ts
+++ tests/composables/useSegment.spec.ts
@@ -1,4 +1,4 @@
-const { trackPage, trackClickEvent, AnalyticsContext } = useSegment()
+const { trackPage, trackClickEvent, AnalyticsContext } = useSegment();
 
 const window: AnalyticsContext = {
   bluemixAnalytics: {
@@ -9,149 +9,155 @@ const window: AnalyticsContext = {
   digitalData: {
     page: {
       pageInfo: {
-        productTitle: 'Test Product Title',
+        productTitle: "Test Product Title",
         analytics: {
-          category: 'Test Analytics Category'
-        }
-      }
-    }
-  }
-}
+          category: "Test Analytics Category",
+        },
+      },
+    },
+  },
+};
 
-const cta = 'Join now!'
-const location = 'header'
+const cta = "Join now!";
+const location = "header";
 
-const routeName = 'sample route'
-const title = 'sample title'
+const routeName = "sample route";
+const title = "sample title";
 
-const uiElement = 'sample search component'
-const field = 'sample seach term'
+const uiElement = "sample search component";
+const field = "sample seach term";
 
 const commonSuite: [Function, any[], string][] = [
-  [trackPage, [routeName, title], 'pageEvent'],
-  [trackClickEvent, [cta, location], 'trackEvent'],
+  [trackPage, [routeName, title], "pageEvent"],
+  [trackClickEvent, [cta, location], "trackEvent"],
   // TODO: the search will be move to saiba so this won't be needed in the future.
   // I commmented it just in case we have to update the test
   // [trackPerformedSearch, [uiElement, field], 'trackEvent']
-]
+];
 
 commonSuite.forEach(([fn, args, delegate]) => {
   describe(`${fn.name}`, () => {
     beforeEach(() => {
-      window.bluemixAnalytics[delegate].mockClear()
-    })
+      window.bluemixAnalytics[delegate].mockClear();
+    });
 
     it(`delegates in Bluemix Analytics \`${delegate}\` function`, () => {
-      fn(window, ...args)
-      expect(window.bluemixAnalytics[delegate]).toHaveBeenCalledTimes(1)
-    })
+      fn(window, ...args);
+      expect(window.bluemixAnalytics[delegate]).toHaveBeenCalledTimes(1);
+    });
 
-    it('fails silently if the context lacks `bluemixAnalytics` object', () => {
+    it("fails silently if the context lacks `bluemixAnalytics` object", () => {
       fn(
         {
           ...window,
-          bluemixAnalytics: undefined
+          bluemixAnalytics: undefined,
         },
         ...args
-      )
-      expect(window.bluemixAnalytics[delegate]).not.toBeCalled()
-    })
+      );
+      expect(window.bluemixAnalytics[delegate]).not.toBeCalled();
+    });
 
-    it('fails silently if the context lacks `digitalData` object', () => {
+    it("fails silently if the context lacks `digitalData` object", () => {
       fn(
         {
           ...window,
-          digitalData: undefined
+          digitalData: undefined,
         },
         ...args
-      )
-      expect(window.bluemixAnalytics[delegate]).not.toBeCalled()
-    })
+      );
+      expect(window.bluemixAnalytics[delegate]).not.toBeCalled();
+    });
 
-    it('throws if `productTitle` is undefined', () => {
-      expect(() => fn(
-        {
-          ...window,
-          digitalData: {
-            page: {
-              pageInfo: {
-                analytics: {
-                  category: 'Test Analytics Category'
-                }
-              }
-            }
-          }
-        },
-        ...args
-      )).toThrow()
-    })
+    it("throws if `productTitle` is undefined", () => {
+      expect(() =>
+        fn(
+          {
+            ...window,
+            digitalData: {
+              page: {
+                pageInfo: {
+                  analytics: {
+                    category: "Test Analytics Category",
+                  },
+                },
+              },
+            },
+          },
+          ...args
+        )
+      ).toThrow();
+    });
 
-    it('throws if `category` is undefined', () => {
-      expect(() => fn(
-        {
-          ...window,
-          digitalData: {
-            page: {
-              pageInfo: {
-                productTitle: 'Test Product Title',
-                analytics: { }
-              }
-            }
-          }
-        },
-        ...args
-      )).toThrow()
-    })
+    it("throws if `category` is undefined", () => {
+      expect(() =>
+        fn(
+          {
+            ...window,
+            digitalData: {
+              page: {
+                pageInfo: {
+                  productTitle: "Test Product Title",
+                  analytics: {},
+                },
+              },
+            },
+          },
+          ...args
+        )
+      ).toThrow();
+    });
 
-    it('throws if the path to `pageTitle` or `category` is broken', () => {
-      expect(() => fn(
-        {
-          ...window,
-          digitalData: {}
-        },
-        ...args
-      )).toThrow()
-    })
-  })
-})
+    it("throws if the path to `pageTitle` or `category` is broken", () => {
+      expect(() =>
+        fn(
+          {
+            ...window,
+            digitalData: {},
+          },
+          ...args
+        )
+      ).toThrow();
+    });
+  });
+});
 
-describe('trackClickEvent', () => {
+describe("trackClickEvent", () => {
   beforeEach(() => {
-    window.bluemixAnalytics.trackEvent.mockClear()
-  })
+    window.bluemixAnalytics.trackEvent.mockClear();
+  });
 
   it('translates the event into a Bluemix Analytics "CTA Clicked" event', () => {
-    trackClickEvent(window, cta, location)
+    trackClickEvent(window, cta, location);
     expect(window.bluemixAnalytics.trackEvent).toHaveBeenCalledWith(
-      'CTA Clicked',
+      "CTA Clicked",
       {
         category: window.digitalData.page.pageInfo.analytics.category,
         CTA: cta,
         location,
-        productTitle: window.digitalData.page.pageInfo.productTitle
+        productTitle: window.digitalData.page.pageInfo.productTitle,
       }
-    )
-  })
-})
+    );
+  });
+});
 
-describe('trackPage', () => {
+describe("trackPage", () => {
   beforeEach(() => {
-    window.bluemixAnalytics.pageEvent.mockClear()
-  })
+    window.bluemixAnalytics.pageEvent.mockClear();
+  });
 
   it('translates the event into a Bluemix Analytics "pushState" page event', () => {
-    trackPage(window, routeName, title)
+    trackPage(window, routeName, title);
     expect(window.bluemixAnalytics.pageEvent).toHaveBeenCalledWith(
       window.digitalData.page.pageInfo.analytics.category,
       routeName,
       {
-        navigationType: 'pushState',
+        navigationType: "pushState",
         productTitle: window.digitalData.page.pageInfo.productTitle,
-        title
+        title,
       }
-    )
-  })
-})
+    );
+  });
+});
 
 //TODO: Commented until decide if we want it
 
@@ -172,4 +178,4 @@ describe('trackPage', () => {
 //       }
 //     )
 //   })
-})
+// })
