31a2890175a8f8e95464921e9391ea4ff94e7ca9
memolina2323 <maria.esperanza.molina.canizares@ibm.com>
2023-02-27 13:36:28 +0100

    reorganize html tags and styling

--- pages/events/index.vue
+++ pages/events/index.vue
@@ -86,45 +86,43 @@
           </div>
         </template>
         <template slot="extra-info">
-          <div id="follow-our-event-calendar" class="event-page__anchor">
+          <div class="event-page__section">
             <a href="#follow-our-event-calendar" />
-            <div class="event-page__section">
-              <h3>Follow our event calendar</h3>
-              <p class="event-page__section__description">
-                <!-- eslint-disable-next-line vue/singleline-html-element-content-newline -->
-                Stay up to date with all of our scheduled events by following our calendar. You can view the calendar by visiting <AppLink v-bind="qiskitCalendarLink">{{ qiskitCalendarLink.label }}</AppLink>, or subscribe to it by adding to the calendar app of your choice.
-              </p>
-              <div class="event-page__tabs">
-                <cv-tabs aria-label="Calendar applications">
-                  <cv-tab
-                    v-for="{ name, instructions } in calendarsInstructions"
-                    :id="`tab-${name}`"
-                    :key="name"
-                    :label="name"
-                    class="event-page__tab"
-                  >
-                    <p class="event-page__sync">
-                      Start by copying the calendar subscription link
-                      <cv-code-snippet
-                        kind="inline"
-                        feedback-aria-label="Copy calendar sync url"
-                      >
-                        <span>{{ qiskitCalendarSyncLink }}</span>
-                      </cv-code-snippet>
-                    </p>
-                    <component :is="instructions" class="event-page__instructions" />
-                  </cv-tab>
-                </cv-tabs>
-              </div>
+            <h3 id="follow-our-event-calendar">Follow our event calendar</h3>
+            <p class="event-page__section__description">
+              <!-- eslint-disable-next-line vue/singleline-html-element-content-newline -->
+              Stay up to date with all of our scheduled events by following our calendar. You can view the calendar by visiting <AppLink v-bind="qiskitCalendarLink">{{ qiskitCalendarLink.label }}</AppLink>, or subscribe to it by adding to the calendar app of your choice.
+            </p>
+            <div class="event-page__tabs">
+              <cv-tabs aria-label="Calendar applications">
+                <cv-tab
+                  v-for="{ name, instructions } in calendarsInstructions"
+                  :id="`tab-${name}`"
+                  :key="name"
+                  :label="name"
+                  class="event-page__tab"
+                >
+                  <p class="event-page__sync">
+                    Start by copying the calendar subscription link
+                    <cv-code-snippet
+                      kind="inline"
+                      feedback-aria-label="Copy calendar sync url"
+                    >
+                      <span>{{ qiskitCalendarSyncLink }}</span>
+                    </cv-code-snippet>
+                  </p>
+                  <component :is="instructions" class="event-page__instructions" />
+                </cv-tab>
+              </cv-tabs>
             </div>
-            <div class="event-page__section">
-              <h3>Start an event</h3>
-              <p class="event-page__section__description">
-                We can help you bring Qiskit experts to your campus for guest
-                lectures, hackathons, and other events.
-              </p>
-              <AppCta v-bind="eventRequestLink" />
-            </div>
+          </div>
+          <div class="event-page__section">
+            <h3>Start an event</h3>
+            <p class="event-page__section__description">
+              We can help you bring Qiskit experts to your campus for guest
+              lectures, hackathons, and other events.
+            </p>
+            <AppCta v-bind="eventRequestLink" />
           </div>
         </template>
       </AppFiltersResultsLayout>
@@ -353,18 +351,10 @@ export default class EventsPage extends QiskitPage {
       }
     }
 
-    a {
-      text-decoration: none;
-
-      h3{
-        color: black;
-      }
+    #follow-our-event-calendar{
+      scroll-margin-top: 65px;
     }
 
-  }
-
-  &__anchor{
-    scroll-margin-top: 65px;
   }
 
   &__tab {
