c8dcb8986ef35b1724a12b9dcf8752faa2a1ba1a
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-02-27 13:21:11 +0100

    chore: add TODOs for future refactor

--- components/advocates/MeetTheAdvocates.vue
+++ components/advocates/MeetTheAdvocates.vue
@@ -117,6 +117,7 @@ function updateRegionFilters(newRegionFilters: string) {
   regionFilters.value = newRegionFiltersAsArray;
 }
 
+// TODO: Refactor links (https://github.com/Qiskit/qiskit.org/issues/1566)
 const joinSlackLink = "https://ibm.co/joinqiskitslack";
 </script>
 
