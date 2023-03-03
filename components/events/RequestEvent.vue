8971423a7f0f1e08372fbefcfa6253850bc6f8af
memolina2323 <maria.esperanza.molina.canizares@ibm.com>
2023-03-03 16:39:58 +0100

    RequestEvent Component

---
+++ components/events/RequestEvent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="event-page__section">
+    <h3>Start an event</h3>
+    <p class="event-page__section__description">
+      We can help you bring Qiskit experts to your campus for guest
+      lectures, hackathons, and other events.
+    </p>
+    <AppCta v-bind="eventRequestLink" />
+  </div>
+</template>
+<script lang="ts">
+import Vue from 'vue'
+import { EVENT_REQUEST_LINK } from '~/constants/appLinks'
+export default class RequestEvent extends Vue {
+    eventRequestLink = EVENT_REQUEST_LINK
+}
+</script>
