461a3ce0250baaf1d2c746d2aed7015f734d993a
Randy Tolentino <randytolentino@ibm.com>
2023-03-07 14:00:14 -0800

    feat(providers): update homepage quick start feature

---
+++ components/landing/TheQuickStart/Algorithms.vue
@@ -0,0 +1,217 @@
+<template>
+  <section class="algorithms">
+    <div class="algorithms__container">
+      <h3>Get the SDK</h3>
+      <div class="algorithms__section">
+        <code class="algorithms__code-cell">
+          <pre class="algorithms__code-cell__line">pip install qiskit</pre>
+          <pre class="algorithms__code-cell__line">pip install qiskit-ibm-provider</pre>
+        </code>
+        <!--
+          @click="[copyToClipboard($event), $trackClickEvent(`Copy ${title} install code`, 'providers')]"
+        -->
+        <cv-button
+          title="Copy"
+          size="sm"
+          class="algorithms__copy-button"
+          @click="copyToClipboard($event)"
+        >
+          Copy
+        </cv-button>
+      </div>
+      <h3>Test some algorithms</h3>
+      <cv-tabs container>
+        <cv-tab label="Bell state">
+          <div class="algorithms__section">
+            <code class="algorithms__code-cell">
+              <pre class="algorithms__code-cell__line">import qiskit</pre>
+              <pre class="algorithms__code-cell__line">&nbsp;</pre>
+              <pre class="algorithms__code-cell__comment"># Oiskit quantum circuits libraries</pre>
+              <pre class="algorithms__code-cell__line">quantum_circuit = qiskit.circuit.library.QuantumVolume</pre>
+              <pre class="algorithms__code-cell__line">quantum_circuit.measure_all()</pre>
+              <pre class="algorithms__code-cell__line">quantum_circuit.draw()</pre>
+              <pre class="algorithms__code-cell__line">&nbsp;</pre>
+              <pre class="algorithms__code-cell__line">prepare your circuit to run</pre>
+              <pre class="algorithms__code-cell__line">from qiskit import IBMQ</pre>
+              <pre class="algorithms__code-cell__line">&nbsp;</pre>
+              <pre class="algorithms__code-cell__comment">#Get the API token in</pre>
+              <pre class="algorithms__code-cell__comment"># https://quantum-computing.ibm.com/</pre>
+              <pre class="algorithms__code-cell__line">IBMQ.save_account("YOUR TOKEN")</pre>
+              <pre class="algorithms__code-cell__line">&nbsp;</pre>
+              <pre class="algorithms__code-cell__line">provider = IBM.load_account()</pre>
+              <pre class="algorithms__code-cell__line">backend = provader.get_ backend('ibmq_quito')</pre>
+              <pre class="algorithms__code-cell__line">&nbsp;</pre>
+              <pre class="algorithms__code-cell__line">optimized_circuit = qiskit.transpile(quantum_circuit, backend)</pre>
+              <pre class="algorithms__code-cell__line">optimized_circuit.draw()</pre>
+              <pre class="algorithms__code-cell__line">&nbsp;</pre>
+              <pre class="algorithms__code-cell__comment"># run in real hardware</pre>
+              <pre class="algorithms__code-cell__line">job = backend.run (optimized_circuit)</pre>
+              <pre class="algorithms__code-cell__line">retrieved_job = backend.retrieve_job(job.job_id())</pre>
+              <pre class="algorithms__code-cell__line">result = retrieved_job.result()</pre>
+              <pre class="algorithms__code-cell__line">print result.get_counts()</pre>
+            </code>
+            <cv-button
+              title="Copy"
+              size="sm"
+              class="algorithms__copy-button"
+              @click="copyToClipboard($event)"
+            >
+              Copy
+            </cv-button>
+          </div>
+        </cv-tab>
+        <cv-tab label="Quantum Volume">
+          <div class="algorithms__section">
+            <code class="algorithms__code-cell">
+              <pre class="algorithms__code-cell__line">pip install qiskit</pre>
+              <pre class="algorithms__code-cell__line">pip install qiskit-ibm-provider</pre>
+            </code>
+            <cv-button
+              title="Copy"
+              size="sm"
+              class="algorithms__copy-button"
+              @click="copyToClipboard($event)"
+            >
+              Copy
+            </cv-button>
+          </div>
+        </cv-tab>
+        <cv-tab label="VQE">
+          <div class="algorithms__section">
+            <code class="algorithms__code-cell">
+              <pre class="algorithms__code-cell__line">pip install qiskit</pre>
+              <pre class="algorithms__code-cell__line">pip install qiskit-ibm-provider</pre>
+            </code>
+            <cv-button
+              title="Copy"
+              size="sm"
+              class="algorithms__copy-button"
+              @click="copyToClipboard($event)"
+            >
+              Copy
+            </cv-button>
+          </div>
+        </cv-tab>
+        <cv-tab label="QAOA">
+          <div class="algorithms__section">
+            <code class="algorithms__code-cell">
+              <pre class="algorithms__code-cell__line">pip install qiskit</pre>
+              <pre class="algorithms__code-cell__line">pip install qiskit-ibm-provider</pre>
+            </code>
+            <cv-button
+              title="Copy"
+              size="sm"
+              class="algorithms__copy-button"
+              @click="copyToClipboard($event)"
+            >
+              Copy
+            </cv-button>
+          </div>
+        </cv-tab>
+        <cv-tab label="QSVM">
+          <div class="algorithms__section">
+            <code class="algorithms__code-cell">
+              <pre class="algorithms__code-cell__line">pip install qiskit</pre>
+              <pre class="algorithms__code-cell__line">pip install qiskit-ibm-provider</pre>
+            </code>
+            <cv-button
+              title="Copy"
+              size="sm"
+              class="algorithms__copy-button"
+              @click="copyToClipboard($event)"
+            >
+              Copy
+            </cv-button>
+          </div>
+        </cv-tab>
+      </cv-tabs>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+
+@Component
+export default class Algorithms extends Vue {
+  copyCodeCta = 'Copy'
+
+  copyToClipboard (e: { target: any }): void {
+    const buttonElement = e.target
+    const codeBlock = buttonElement.parentNode
+    const codeSnippet = codeBlock.querySelector('.algorithms__code-cell')
+    if (codeSnippet !== null) {
+      navigator.clipboard.writeText(codeSnippet.innerText)
+      buttonElement.innerText = 'Copied!'
+      setTimeout(() => {
+        buttonElement.blur()
+        buttonElement.innerText = this.copyCodeCta
+      }, 3500)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$cta-max-width: 4rem;
+
+.algorithms {
+  padding: $spacing-05;
+
+  &__container {
+    background-color: $background-color-lighter;
+    padding: $spacing-05;
+  }
+
+  &__section {
+    position: relative;
+    margin-bottom: $spacing-07;
+  }
+
+  &__code-cell {
+    display: block;
+    background-color: $background-color-white;
+    color: $text-color;
+    padding: $spacing-05 $spacing-12 $spacing-05 $spacing-05;
+
+    &__comment {
+      color: $text-active-color;
+      word-break: break-word;
+      white-space: normal;
+    }
+
+    &__line {
+      word-break: break-word;
+      white-space: normal;
+    }
+  }
+
+  &__copy-button {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    max-width: $cta-max-width;
+    background-size: 200% 100%;
+    background-position-x: 100%;
+    background-image: linear-gradient(90deg, $button-background-color-light 0%, $button-background-color-light 50%, $background-color-secondary 50%, $background-color-secondary 100%);
+    transition: background-position-x 0.3s ease-out;
+
+    &:hover,
+    &:active {
+      background-position-x: 0;
+    }
+
+    &:focus {
+      border-color: $button-background-color-light;
+      box-shadow: inset 0 0 0 1px $button-background-color-light, inset 0 0 0 2px $white-0;
+    }
+
+    &__label {
+      display: block;
+      width: 100%;
+    }
+  }
+}
+</style>
---
+++ components/landing/TheQuickStart/ProvidersList.vue
@@ -0,0 +1,165 @@
+<template>
+  <section class="providers">
+    <h4>Providers</h4>
+    <ul class="providers__list">
+      <li v-for="item in providersList" :key="item.id">
+        <a class="providers__list__item" href="" target="_blank">
+          {{ item.title }}
+          <span class="providers__list__cta">
+            <span class="providers__list__cta-label">Learn more</span>
+            <Launch16 class="providers__list__cta-icon" />
+          </span>
+        </a>
+      </li>
+    </ul>
+    <AppCta
+      class="providers__btn"
+      v-bind="providersPage"
+    />
+  </section>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { Component } from 'vue-property-decorator'
+import { GeneralLink } from '~/constants/appLinks'
+
+type ProviderLink = {
+  title: string,
+  id: string,
+  link: string
+}
+
+type ProvidersList = Array<ProviderLink>
+
+@Component
+export default class StartLocally extends Vue {
+  providersPage: GeneralLink = {
+    url: '/providers',
+    label: 'See all providers'
+  }
+
+  // TODO: replace w/ proper provider information and figure out how we'll handle this data
+  providersList: ProvidersList = [
+    {
+      title: 'Provider 1',
+      id: '1',
+      link: ''
+    },
+    {
+      title: 'Provider 2',
+      id: '2',
+      link: ''
+    },
+    {
+      title: 'Provider 3',
+      id: '3',
+      link: ''
+    },
+    {
+      title: 'Provider 4',
+      id: '4',
+      link: ''
+    },
+    {
+      title: 'Provider 5',
+      id: '5',
+      link: ''
+    },
+    {
+      title: 'Provider 6',
+      id: '6',
+      link: ''
+    },
+    {
+      title: 'Provider 7',
+      id: '7',
+      link: ''
+    },
+    {
+      title: 'Provider 8',
+      id: '8',
+      link: ''
+    },
+    {
+      title: 'Provider 9',
+      id: '9',
+      link: ''
+    },
+    {
+      title: 'Provider 10',
+      id: '10',
+      link: ''
+    },
+    {
+      title: 'Provider 11',
+      id: '11',
+      link: ''
+    },
+    {
+      title: 'Provider 12',
+      id: '12',
+      link: ''
+    }
+  ]
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~carbon-components/scss/globals/scss/typography';
+
+.providers {
+  padding-top: $spacing-05;
+
+  &__list {
+    max-height: 29rem;
+    overflow-y: scroll;
+    margin-bottom: $spacing-07;
+    border-bottom: 1px solid $background-color-light;
+
+    &__cta {
+      display: flex;
+      align-items: center;
+      opacity: 0;
+    }
+
+    &__item {
+      display: flex;
+      justify-content: space-between;
+      color: $text-color;
+      text-decoration: none;
+      background-color: $background-color-lighter;
+      padding: $spacing-03;
+      border-left: 2px solid transparent;
+      margin-bottom: $spacing-04;
+
+      &:hover,
+      &:focus,
+      &:active {
+        background-color: $background-color-light;
+        cursor: pointer;
+        border-left: 2px solid $border-color-secondary;
+
+        .providers__list__cta {
+          opacity: 1;
+        }
+      }
+    }
+
+    &__cta-label {
+      padding-right: $spacing-03;
+      color: $text-active-color;
+    }
+
+    &__cta-icon {
+      fill: $text-active-color;
+    }
+  }
+
+  &__btn {
+    @include mq($until: large) {
+      margin-bottom: $spacing-07;
+    }
+  }
+}
+</style>
