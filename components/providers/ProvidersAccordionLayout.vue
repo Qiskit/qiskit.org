720be098aa6139c8ccf6ca0fd3c4c9b0118e6659
Randy <randytolentino@ibm.com>
2023-03-14 12:43:43 -0700

    [Providers] use json data in page (#3000)
    * feat(providers): use json data in Providers page
    
    * feat(providers): refactor Providers page layout
    
    * feat(providers): reorder accordion content
    
    * feat(providers): use code cell styling
    
    * feat(providers): add copy code button
    
    * feat(providers): use Carbon accordion web component
    
    * feat(providers): style copy button
    
    * feat(providers): remove unused styles
    
    * feat(providers): clean nav items and unused class
    
    * feat(providers): clean mobile styles,update to consistent buttons
    
    * feat(providers): add copy button feedback
    
    * feat(providers): instrument copy buttons
    
    * feat(providers): abstract CodeSnippet component, refactor provider content
    
    * feat(providers): fix lint issues
    
    * feat(providers): update CodeSnippet props
    
    * feat(providers): update code to use new json structure
    
    * feat(providers): updated w/ latest json structure
    
    * feat(providers): update img path
    
    * chore: remove empty line
    
    * chore: remove unused code
    
    ---------
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>

--- components/providers/ProvidersAccordionLayout.vue
+++ components/providers/ProvidersAccordionLayout.vue
@@ -1,50 +1,60 @@
 <template>
   <article class="accordion-layout">
-    <cv-code-snippet
-      kind="oneline"
-      light
-      feedback-aria-label="Copy code snippet"
-    >
-      <!-- TODO: replace w/ dynamic code snippet -->
-      <span>pip install something</span>
-    </cv-code-snippet>
-    <img :src="image" class="accordion-layout__image" />
     <p class="accordion-layout__description">
       {{ description }}
     </p>
-    <UiCta kind="ghost" :label="cta.label" :url="cta.url" />
-    <!-- TODO: replace w/ dynamic code sample -->
-    <cv-code-snippet
-      kind="multiline"
-      light
-      feedback-aria-label="Copy code snippet"
-    >
-      import qiskit from qiskit_runtime import ... (very) short code example
-      Lorem ipsum dolor sit amet consectetur adipiscing elit Suspendisse id
-      laoreet urna nec egestas lorem Donec porta mauris sed facilisis pulvinar
-    </cv-code-snippet>
+    <img v-if="image" :src="image" class="accordion-layout__image" />
+    <UiCodeSnippet
+      :code-lines="[installation]"
+      :code-snippet-title="title"
+      :code-snippet-location="'providers'"
+    />
+    <div class="accordion-layout__code-block">
+      <UiCodeSnippet
+        :code-lines="codeExamples[0].fullCode"
+        :code-snippet-title="title"
+        :code-snippet-location="'providers'"
+      />
+    </div>
+    <div class="accordion-layout__cta-group">
+      <UiCta
+        v-for="cta in validCtas"
+        :key="cta.label"
+        kind="ghost"
+        v-bind="cta"
+      />
+    </div>
   </article>
 </template>
 
 <script setup lang="ts">
-// TODO: It is not possible to extract the interface of the component props
-// so we need this redundant interface to do it. Explore if it is worth
-// removing the @Prop decorator in favour of:
-// https://class-component.vuejs.org/guide/props-definition.html
+import { GeneralLink } from "~/constants/appLinks";
+import { ProviderCodeExample } from "~/constants/providersContent";
+
 export interface AccordionLayoutProps {
-  image: string;
+  title: string;
+  image: string | null;
   description: string;
-  cta: {
-    url: string;
-    label: string;
-  };
+  installation: string;
+  websiteCta: GeneralLink;
+  docsCta: GeneralLink;
+  sourceCta: GeneralLink;
+  codeExamples: ProviderCodeExample[];
 }
 
-defineProps<AccordionLayoutProps>();
+const props = defineProps<AccordionLayoutProps>();
+
+const validCtas = computed(() => {
+  return [props.websiteCta, props.docsCta, props.sourceCta].filter(
+    (cta) => cta.url !== null
+  );
+});
 </script>
 
 <style lang="scss" scoped>
 @use "~/assets/scss/carbon.scss";
+
+$cta-max-width: 4rem;
 
 .accordion-layout {
   display: flex;
@@ -54,12 +64,28 @@ defineProps<AccordionLayoutProps>();
     align-self: center;
     height: auto;
     width: 100%;
-    max-width: 26rem;
     margin-bottom: carbon.$spacing-05;
   }
 
   &__description {
     margin-bottom: carbon.$spacing-06;
+  }
+
+  &__cta-group {
+    display: flex;
+
+    @include carbon.breakpoint-down(md) {
+      flex-direction: column;
+    }
+
+    .app-cta {
+      max-width: 10rem;
+      padding-left: carbon.$spacing-05;
+
+      @include carbon.breakpoint-down(md) {
+        max-width: initial;
+      }
+    }
   }
 }
 </style>
