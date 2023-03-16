dd1ee75257648aa6b0ef7768fb55f8347b5ab83a
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-03-16 19:06:44 +0100

    refactor(learn): "helpful resources" section

--- pages/learn/index.vue
+++ pages/learn/index.vue
@@ -14,17 +14,17 @@
     </qiskit-banner>
     <LearnPageHeader />
     <!-- <StartLearningSection class="learn-page__section" /> -->
-    <!-- <AppHelpfulResourcesSection
+    <UiAppHelpfulResourcesSection
       class="learn-page__section"
       :resources="helpfulResources"
-    /> -->
+    />
   </main>
 </template>
 
 <script setup lang="ts">
 import "@qiskit/web-components/banner";
-// import { DescriptionCard } from "~/components/ui/AppDescriptionCard.vue";
-// import { SOCIAL_MEDIA } from "~/constants/menuLinks";
+import { SOCIAL_MEDIA } from "~/constants/menuLinks";
+import type { DescriptionCard } from "~/types/uiComponents";
 
 definePageMeta({
   layout: "default-max",
@@ -35,51 +35,50 @@ useHead({
   title: "Qiskit Textbook",
 });
 
-// TODO: Integrate old code
-// const helpfulResources: DescriptionCard[] = [
-//   {
-//     title: "Documentation",
-//     description: `The Qiskit Documentation is the right place for you if
-//     you are looking for the installation guide, release notes, or API
-//     references.`,
-//     cta: {
-//       url: "https://qiskit.org/documentation/",
-//       label: "Go to documentation",
-//       segment: { cta: "documentation", location: "helpful-resources" },
-//     },
-//   },
-//   {
-//     title: SOCIAL_MEDIA.stack.label,
-//     description: `Have a question? Ask it on stack exchange! Qiskit
-//     advocates and core contributors monitor this forum and will happily
-//     answer your questions.`,
-//     cta: {
-//       url: SOCIAL_MEDIA.stack.url,
-//       label: "Visit Stack Exchange",
-//       segment: { cta: "stack-exchange", location: "helpful-resources" },
-//     },
-//   },
-//   {
-//     title: SOCIAL_MEDIA.slack.label,
-//     description: `The Slack community is a great place to engage in
-//     discussion on research and quantum development.`,
-//     cta: {
-//       url: SOCIAL_MEDIA.slack.url,
-//       label: "Visit Slack",
-//       segment: { cta: "slack", location: "helpful-resources" },
-//     },
-//   },
-//   {
-//     title: "Educators Program",
-//     description:
-//       "This program helps teachers in the growing quantum field connect with one another, and provides the learning resources, tools, and systems access they need to provide quality educational experiences.",
-//     cta: {
-//       url: "https://quantum-computing.ibm.com/programs/educators",
-//       label: "Visit the Educators Program",
-//       segment: { cta: "educators-program", location: "helpful-resources" },
-//     },
-//   },
-// ];
+const helpfulResources: DescriptionCard[] = [
+  {
+    title: "Documentation",
+    description: `The Qiskit Documentation is the right place for you if
+    you are looking for the installation guide, release notes, or API
+    references.`,
+    cta: {
+      url: "https://qiskit.org/documentation/",
+      label: "Go to documentation",
+      segment: { cta: "documentation", location: "helpful-resources" },
+    },
+  },
+  {
+    title: SOCIAL_MEDIA.stack.label,
+    description: `Have a question? Ask it on stack exchange! Qiskit
+    advocates and core contributors monitor this forum and will happily
+    answer your questions.`,
+    cta: {
+      url: SOCIAL_MEDIA.stack.url,
+      label: "Visit Stack Exchange",
+      segment: { cta: "stack-exchange", location: "helpful-resources" },
+    },
+  },
+  {
+    title: SOCIAL_MEDIA.slack.label,
+    description: `The Slack community is a great place to engage in
+    discussion on research and quantum development.`,
+    cta: {
+      url: SOCIAL_MEDIA.slack.url,
+      label: "Visit Slack",
+      segment: { cta: "slack", location: "helpful-resources" },
+    },
+  },
+  {
+    title: "Educators Program",
+    description:
+      "This program helps teachers in the growing quantum field connect with one another, and provides the learning resources, tools, and systems access they need to provide quality educational experiences.",
+    cta: {
+      url: "https://quantum-computing.ibm.com/programs/educators",
+      label: "Visit the Educators Program",
+      segment: { cta: "educators-program", location: "helpful-resources" },
+    },
+  },
+];
 
 // TODO: Refactor tracking
 const routeName = "learn";
