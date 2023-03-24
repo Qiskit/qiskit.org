966f9d26bda73ab013c047ad1e23e14f1c9f1cc0
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-03-24 17:23:35 +0100

    fix(analytics): capture title and name properties
    fix(analytics): capture title and name properties

--- middleware/trackPage.global.ts
+++ middleware/trackPage.global.ts
@@ -2,6 +2,6 @@ export default defineNuxtRouteMiddleware((to) => {
   // Analytics must be executed only in client side
   if (process.client) {
     const { trackPage } = useSegment();
-    trackPage(window, to.fullPath);
+    trackPage(to.meta.routeName, to.meta.pageTitle);
   }
 });
---
+++ types/modules.d.ts
@@ -0,0 +1,1 @@
+declare module "frontmatter-markdown-loader/mode";
