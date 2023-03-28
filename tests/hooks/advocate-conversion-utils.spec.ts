519efe42383bc35557afe309d4b6912f6cac1a4e
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-03-28 14:43:38 +0200

    test: advocates in Nuxt 3

--- tests/hooks/advocate-conversion-utils.spec.ts
+++ tests/hooks/advocate-conversion-utils.spec.ts
@@ -1,4 +1,5 @@
-import AdvocatesAirtableRecords from "~/hooks/advocate-conversion-utils";
+import { beforeEach, describe, expect, test, vi } from "vitest";
+import AdvocatesAirtableRecords from "../../hooks/advocate-conversion-utils";
 
 describe("getters", () => {
   let advocatesAirtableRecords: AdvocatesAirtableRecords;
@@ -35,27 +36,27 @@ describe("getters", () => {
     );
   });
 
-  it("gets the city from the record", () => {
+  test("gets the city from the record", () => {
     expect(advocatesAirtableRecords.getCity(fakeRecord)).toBe("Gotham City");
   });
 
-  it("gets the country from the record", () => {
+  test("gets the country from the record", () => {
     expect(advocatesAirtableRecords.getCountry(fakeRecord)).toBe("Canada");
   });
 
-  it("gets the region from the record", () => {
+  test("gets the region from the record", () => {
     expect(advocatesAirtableRecords.getRegion(fakeRecord)).toBe(
       "South America"
     );
   });
 
-  it("gets the Slack ID from the record", () => {
+  test("gets the Slack ID from the record", () => {
     expect(advocatesAirtableRecords.getSlackId(fakeRecord)).toBe(
       "ID1234567890"
     );
   });
 
-  it("gets the Slack username from the record", () => {
+  test("gets the Slack username from the record", () => {
     expect(advocatesAirtableRecords.getSlackUsername(fakeRecord)).toBe(
       "U1234567890"
     );
@@ -100,7 +101,7 @@ describe("convertToAdvocate", () => {
     );
   });
 
-  it("converts the record to an advocate object", async () => {
+  test("converts the record to an advocate object", async () => {
     const advocate = await advocatesAirtableRecords.convertToAdvocate(
       fakeRecord
     );
@@ -131,7 +132,7 @@ describe("getImage", () => {
     );
   });
 
-  it("defaults in a no-advocate-photo.png value if there is no attachment", async () => {
+  test("defaults in a no-advocate-photo.png value if there is no attachment", async () => {
     const fakeRecord = {
       get: (field: string) => {
         if (field === "Image") {
@@ -143,7 +144,7 @@ describe("getImage", () => {
     expect(result).toBe("/images/advocates/no-advocate-photo.png");
   });
 
-  it("defaults in a no-advocate-photo.png value if the attachment is of no image type", async () => {
+  test("defaults in a no-advocate-photo.png value if the attachment is of no image type", async () => {
     const fakeRecord = {
       get: (field: string) => {
         if (field === "Image") {
@@ -159,7 +160,7 @@ describe("getImage", () => {
     expect(result).toBe("/images/advocates/no-advocate-photo.png");
   });
 
-  it("stores the attachment URL if there are no thumbnails", async () => {
+  test("stores the attachment URL if there are no thumbnails", async () => {
     const attachmentUrl = "http://url.to/image.jpg";
     const fakeRecord = {
       get: (field: string) => {
@@ -174,14 +175,14 @@ describe("getImage", () => {
       },
     };
 
-    const mockStoreImage = jest
+    const mockStoreImage = vi
       .spyOn(advocatesAirtableRecords, "storeImage")
       .mockImplementation(() => Promise.resolve());
     await advocatesAirtableRecords.getImage(fakeRecord);
     expect(mockStoreImage).toBeCalledWith(attachmentUrl, expect.any(String));
   });
 
-  it("stores the thumbnail URL if there is a large thumbnail available", async () => {
+  test("stores the thumbnail URL if there is a large thumbnail available", async () => {
     const thumbnailUrl = "http://url.to/thumbnails/large.jpg";
     const fakeRecord = {
       get: (field: string) => {
@@ -199,7 +200,7 @@ describe("getImage", () => {
       },
     };
 
-    const mockStoreImage = jest
+    const mockStoreImage = vi
       .spyOn(advocatesAirtableRecords, "storeImage")
       .mockImplementation(() => Promise.resolve());
     await advocatesAirtableRecords.getImage(fakeRecord);
