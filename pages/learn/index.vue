6d1733482986683d038949bdc860a8afdb8e7485
Junye Huang <h.jun.ye@gmail.com>
2023-03-28 11:32:16 +0200

    use single quotes and one line string

--- pages/learn/index.vue
+++ pages/learn/index.vue
@@ -42,9 +42,7 @@ export default class LearnPage extends QiskitPage {
   helpfulResources: DescriptionCard[] = [
     {
       title: 'Documentation',
-      description: `The Qiskit Documentation is the right place for you if
-      you are looking for the installation guide, release notes, or API
-      references.`,
+      description: 'The Qiskit Documentation is the right place for you if you are looking for the installation guide, release notes, or API references.',
       cta: {
         url: 'https://qiskit.org/documentation/',
         label: 'Go to documentation',
@@ -53,9 +51,7 @@ export default class LearnPage extends QiskitPage {
     },
     {
       title: SOCIAL_MEDIA.support.label,
-      description: `If you need support on anything relating to using and
-      developing Qiskit, you can use the following forums to direct your
-      issue / queries to the appropriate audience.`,
+      description: 'If you need support on anything relating to using and developing Qiskit, you can use the following forums to direct your issue / queries to the appropriate audience.',
       cta: {
         url: SOCIAL_MEDIA.support.url,
         label: 'Visit Support Channels',
@@ -64,8 +60,7 @@ export default class LearnPage extends QiskitPage {
     },
     {
       title: SOCIAL_MEDIA.slack.label,
-      description: `The Slack community is a great place to engage in
-      discussion on research and quantum development.`,
+      description: 'The Slack community is a great place to engage in discussion on research and quantum development.',
       cta: {
         url: SOCIAL_MEDIA.slack.url,
         label: 'Visit Slack',
