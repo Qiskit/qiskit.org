d8a20781df447d785a6ba796e7ebff3c2e2ac66b
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-03-31 15:51:36 +0200

    style: BEM and Carbon naming conventions

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -6,5 +6,10 @@ module.exports = {
 
   rules: {
     "declaration-block-no-redundant-longhand-properties": null,
+
+    "selector-class-pattern": [
+      // IBM Carbon and BEM (https://bem.info) naming conventions
+      "^((bx|cds)--)|(([a-z0-9]+(-[a-z]+)*(__[a-z]+(-[a-z]+)*)?(_[a-z]+(-[a-z]+)*)*)(--[a-z]+(-[a-z]+)*)?)$",
+    ],
   },
 };
