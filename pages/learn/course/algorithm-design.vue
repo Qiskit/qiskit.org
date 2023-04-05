515b709341ad2c5436def2d60c9d51220922c583
Sanket Panda <pandasa123@gmail.com>
2023-04-04 23:23:08 -0400

    Added Near Term Algorithm Design landing page

---
+++ pages/learn/course/algorithm-design.vue
@@ -0,0 +1,82 @@
+<script lang="ts">
+import { Course, Prerequisite } from 'constants/learnContent'
+import { Component } from 'vue-property-decorator'
+import CourseOverviewPage from '~/components/logic/CourseOverviewPage.vue'
+import { GeneralLink, StartLearningUrl } from '~/constants/appLinks'
+import { RecommendedReading } from '~/components/learn/ExternalRecommendedReadings.vue'
+
+@Component({
+  head: () => ({
+    title: 'Near-Term Algorithm Design'
+  })
+})
+export default class NearTermAlgorithmDesignCoursePage extends CourseOverviewPage {
+  routeName: string = 'algorithm-design'
+
+  headerTitle = 'Near-Term Algorithm Design'
+
+  headerDescription = ['This course is designed to teach you how to write variational algorithms, near-term hybrid quantum-classical algorithms that are ideal candidates to achieve Quantum Advantage. Throughout this course, you\'ll learn each step in the variational algorithm design workflow, tradeoffs associated with each step, and how to use Qiskit Runtime primitives to optimize for speed and accuracy.']
+
+  headerImg = '/images/learn/course/basics-quantum-information/atom.png'
+
+  backToTextbookHomeLink: GeneralLink = {
+    url: '/learn',
+    label: 'Home',
+    segment: {
+      cta: 'back-to-textbook-home', location: 'header'
+    }
+  }
+
+  startLearningCTA: GeneralLink = {
+    url: StartLearningUrl.NearTermAlgorithmDesign,
+    label: 'Start learning',
+    segment: {
+      cta: 'algorithm-design-cta', location: 'header'
+    }
+  }
+
+  references: string[] = []
+
+  externalRecommendedReadingsPreamble: string = `To make the most out of this
+  course, we recommend familiarity with Basics of Quantum Information. You may
+  also want to install Qiskit and the IBM Qiskit Runtime package:`
+
+  links: RecommendedReading[] = [
+    {
+      url: 'https://qiskit.org/learn/course/basics-quantum-information/',
+      author: '',
+      label: 'Basics of quantum information',
+      description: `Learn about single systems, multiple systems, quantum circuits,
+                    and explore entanglement in action with John Watrous`,
+      segment: {
+        cta: 'basics-of-quantum-information',
+        location: 'external-recommended-readings'
+      }
+    },
+    {
+      url: 'https://quantum-computing.ibm.com/lab/docs/iql/runtime/',
+      author: '',
+      label: 'Qiskit Runtime Overview',
+      description: `Explore Qiskit Runtime service documentation and tutorials.`,
+      segment: {
+        cta: 'runtime-docs',
+        location: 'external-recommended-readings'
+      }
+    }
+  ]
+
+  courses: Course[] = [
+    // {
+    //   image: '',
+    //   label: 'Single systems',
+    //   segment: { cta: 'single-systems', location: 'course' },
+    //   url: StartLearningUrl.NearTermAlgorithmDesign + '/'
+    // }
+    // Stored in /static/images/learn/course/*
+  ]
+
+  prerequisites: Prerequisite[] = []
+
+  imageUrlBase = '/images/learn/course/algorithm-design'
+}
+</script>
