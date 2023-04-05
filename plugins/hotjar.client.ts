975d4e59e74819f4442dcee91856fdb405abb6a6
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-04-05 11:35:14 +0200

    refactor: plugins in Nuxt 3 (#3092)
    * chore: remove unused plugin
    
    * refactor: Hotjar plugin
    
    * chore: remove unused plugin
    
    * refactor: Hotjar plugin
    
    * chore: remove unnecessary plugin
    
    The clicks are already being sent to Segment using a Vue composable in the components themselves.

--- plugins/hotjar.client.ts
+++ plugins/hotjar.client.ts
@@ -5,12 +5,12 @@ declare global {
   }
 }
 
-export default () => {
-  /* eslint-disable */
+export default defineNuxtPlugin(() => {
   (function (h, o, t, j, a?: any, r?: any) {
     h.hj =
       h.hj ||
       function () {
+        // eslint-disable-next-line prefer-rest-params
         (h.hj.q = h.hj.q || []).push(arguments);
       };
     h._hjSettings = { hjid: 1586421, hjsv: 6 };
@@ -20,5 +20,4 @@ export default () => {
     r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
     a.appendChild(r);
   })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
-  /* eslint-enable */
-};
+});
