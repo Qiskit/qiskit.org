8b681fc78d8f890f337446d53bbc2dd198835287
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-04-18 11:03:06 +0200

    test(providers): at least one provider file exists

--- tests/providers.spec.ts
+++ tests/providers.spec.ts
@@ -2,7 +2,24 @@ import * as fs from "fs/promises";
 import * as path from "path";
 import { describe, test } from "vitest";
 
-describe("providers", () => {
+describe("list", () => {
+  test("at least one content file exists", async () => {
+    const contentFilesPath = "content/providers/list";
+
+    const resolvedContentFilesPath = path.resolve(
+      process.cwd(),
+      contentFilesPath
+    );
+
+    const contentFiles = await fs.readdir(resolvedContentFilesPath);
+
+    if (contentFiles.length === 0) {
+      throw new Error(`No content files found in ${contentFilesPath}`);
+    }
+  });
+});
+
+describe("quick-start", () => {
   test("content file exists", async () => {
     const contentFilePath = "content/providers/quick-start/data.yaml";
 
