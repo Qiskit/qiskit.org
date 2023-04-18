8b5bd39c93f8632294c2f6031c0d794a555ee70b
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-04-18 15:39:37 +0200

    test(providers): providers list files are valid

--- tests/providers.spec.ts
+++ tests/providers.spec.ts
@@ -1,6 +1,8 @@
 import * as fs from "fs/promises";
 import * as path from "path";
 import { describe, test } from "vitest";
+import { parse } from "yaml";
+import { ProvidersSection } from "~/types/providers";
 
 describe("list", () => {
   test("at least one content file exists", async () => {
@@ -15,6 +17,54 @@ describe("list", () => {
 
     if (contentFiles.length === 0) {
       throw new Error(`No content files found in ${contentFilesPath}`);
+    }
+  });
+
+  test("content files are valid", async () => {
+    const contentFilesPath = "content/providers/list";
+
+    const resolvedContentFilesPath = path.resolve(
+      process.cwd(),
+      contentFilesPath
+    );
+
+    const contentFiles = await fs.readdir(resolvedContentFilesPath);
+
+    for (const contentFile of contentFiles) {
+      const resolvedContentFilePath = path.resolve(
+        resolvedContentFilesPath,
+        contentFile
+      );
+
+      const contentFileContent = await fs.readFile(
+        resolvedContentFilePath,
+        "utf-8"
+      );
+
+      const parsedContent: ProvidersSection[] = parse(contentFileContent);
+
+      if (typeof parsedContent !== "object") {
+        throw new TypeError(`Content file ${contentFile} is not valid`);
+      }
+
+      type KeysOfProvidersSection = keyof ProvidersSection;
+
+      const expectedProperties: KeysOfProvidersSection[] = [
+        "id",
+        "title",
+        "description",
+        "providers",
+      ];
+
+      const parsedContentProperties = Object.keys(parsedContent);
+
+      for (const expectedProperty of expectedProperties) {
+        if (!parsedContentProperties.includes(expectedProperty)) {
+          throw new Error(
+            `Content file ${contentFile} is missing the ${expectedProperty} property`
+          );
+        }
+      }
     }
   });
 });
