21d863f533c9aaa16f70054a338814b4d4743b61
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-04-20 12:58:30 +0200

    fix: enable analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,7 +27,7 @@ export default defineNuxtConfig({
       analyticsKey: IS_PRODUCTION
         ? "ffdYLviQze3kzomaINXNk6NwpY9LlXcw"
         : "zbHWEXPUfXm0K6C7HbegwB5ewDEC8o1H",
-      isAnalyticsEnabled: false,
+      isAnalyticsEnabled: true,
     },
   },
 
