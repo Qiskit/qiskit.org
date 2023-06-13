542c6c7ba0a602d7d730b70a195fcb0651976026
Pablo Aragón <pablo.aragon@ibm.com>
2023-06-13 13:10:54 +0200

    feat(code-quality): move reusable functionallity to common place (#3324)
    * move reusable functionallity to common place
    
    * change test mock data
    
    * fix test description

--- hooks/update-advocates.ts
+++ hooks/update-advocates.ts
@@ -1,15 +1,9 @@
-import fs from "fs";
-import util from "util";
-
 import AdvocatesAirtableRecords from "./advocate-conversion-utils";
+import { writeJSONToFile } from "./utils/conversion-utils";
 
 export default async function (apiKey: any, outputFolder: string) {
   const advocatesAirtableRecords = new AdvocatesAirtableRecords(apiKey);
   const advocates = await advocatesAirtableRecords.fetchAdvocates();
 
-  const writeFile = util.promisify(fs.writeFile);
-  await writeFile(
-    `${outputFolder}/advocates.json`,
-    JSON.stringify(advocates, null, 2)
-  );
+  await writeJSONToFile(outputFolder, "advocates.json", advocates);
 }
