568e59ee9b4e3fede6d9b570ec97f8a334d8639f
Pablo Aragón <pablo.aragon@ibm.com>
2023-06-15 16:59:39 +0200

    create folder if not exists

--- hooks/utils/conversion-utils.ts
+++ hooks/utils/conversion-utils.ts
@@ -2,11 +2,15 @@ import fs from "fs";
 import util from "util";
 
 const writeFile = util.promisify(fs.writeFile);
+const mkdir = util.promisify(fs.mkdir);
 
 export async function writeJSONToFile(
   outputFolder: string,
   filename: string,
   data: any
 ) {
+  if (!fs.existsSync(outputFolder)) {
+    fs.mkdirSync(outputFolder);
+  }
   await writeFile(`${outputFolder}/${filename}`, JSON.stringify(data, null, 2));
 }
