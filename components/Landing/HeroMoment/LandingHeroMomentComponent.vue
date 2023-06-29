98316b02eef1a1385b17fc2950405f39a9feb672
Pablo Aragón <pablo.aragon@ibm.com>
2023-06-29 17:20:22 +0200

    delete unused files and use nuxt-img

--- components/Landing/HeroMoment/LandingHeroMomentComponent.vue
+++ components/Landing/HeroMoment/LandingHeroMomentComponent.vue
@@ -39,12 +39,14 @@
         />
         <nuxt-img
           class="hero-moment__container__image"
+          format="webp"
           src="/images/landing-page/cryo.png"
           alt="A visual composition of a sketched a quantum computer"
         />
         <div class="hero-moment__container__laptop">
           <nuxt-img
             class="hero-moment__container__laptop__frame"
+            format="webp"
             src="/images/landing-page/laptop.png"
             :alt="`A laptop with the following code sample: ${heroCodeSnippet}`"
           />
