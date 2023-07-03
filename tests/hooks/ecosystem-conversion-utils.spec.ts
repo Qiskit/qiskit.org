0c0711aff5840a666091a8731726a1e7b17ce3b2
Pablo Aragón <pablo.aragon@ibm.com>
2023-07-03 16:21:24 +0200

    new tests for advocate, airtable and event conversion utils

--- tests/hooks/ecosystem-conversion-utils.spec.ts
+++ tests/hooks/ecosystem-conversion-utils.spec.ts
@@ -1,6 +1,9 @@
 import axios from "axios";
 import { beforeEach, describe, expect, test, vi } from "vitest";
-import { fetchMembers } from "../../hooks/ecosystem-conversion-utils";
+import {
+  fetchMembers,
+  fetchTiers,
+} from "../../hooks/ecosystem-conversion-utils";
 
 vi.mock("axios");
 
@@ -48,6 +51,47 @@ describe("fetchMembers", () => {
 
     (axios.get as any).mockRejectedValueOnce("example error");
     await fetchMembers();
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(consoleSpy).toHaveBeenCalled();
+    expect(consoleSpy).toHaveBeenCalledWith("example error");
+  });
+});
+
+describe("fetchTiers", () => {
+  beforeEach(() => {
+    vi.clearAllMocks();
+  });
+
+  const tiers = [
+    {
+      name: "Main",
+      description: "The main Qiskit packages maintained by IBM Quantum",
+    },
+    {
+      name: "Extensions",
+      description: "IBM Quantum supported Qiskit extensions",
+    },
+    {
+      name: "Community",
+      description:
+        "Software packages supported by the Qiskit community, not maintained by IBM Quantum",
+    },
+  ];
+
+  test("fetch tiers and formats correctly", async () => {
+    (axios.get as any).mockResolvedValueOnce(tiers);
+    const result = await fetchTiers();
+
+    expect(axios.get).toHaveBeenCalledTimes(1);
+    expect(result).toStrictEqual(result);
+  });
+
+  test("throws error if api call fails", async () => {
+    const consoleSpy = vi.spyOn(console, "error").mockImplementation(() => {});
+
+    (axios.get as any).mockRejectedValueOnce("example error");
+    await fetchTiers();
 
     expect(axios.get).toHaveBeenCalledTimes(1);
     expect(consoleSpy).toHaveBeenCalled();
