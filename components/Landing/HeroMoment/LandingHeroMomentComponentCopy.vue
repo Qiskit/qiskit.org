6b4f79e26ad4e58dcaac362d64d02709b0436e6e
Pablo Aragón <pablo.aragon@ibm.com>
2023-07-04 16:52:52 +0200

    fix lint

---
+++ components/Landing/HeroMoment/LandingHeroMomentComponentCopy.vue
@@ -0,0 +1,187 @@
+<template>
+  <article class="hero-moment">
+    <LandingHeroMomentGrid>
+      <div class="cds--grid hero-moment__container">
+        <div class="cds--row">
+          <h1
+            class="cds--col-md-4 cds--col-lg-5 cds--col-xlg-5 cds--col-max-7 hero-moment__title"
+          >
+            Qiskit
+          </h1>
+        </div>
+        <div class="cds--row">
+          <div
+            class="cds--col-sm cds--col-md-4 cds--col-lg-5 cds--col-xlg-5 hero-moment__description"
+          >
+            <p>
+              <strong>{{ qiskitPronunciation }}</strong> <em>noun, software</em>
+            </p>
+            <hr />
+            <ol>
+              <li>open-source toolkit for useful quantum computing</li>
+              <li>production-ready circuit compiler.</li>
+            </ol>
+          </div>
+        </div>
+        <UiCta
+          class="hero-moment__cta"
+          :label="getStartedLink.label"
+          :segment="getStartedLink.segment"
+          :url="getStartedLink.url"
+        />
+        <LandingHeroMomentVersionInfo
+          class="hero-moment__version-info"
+          :version="version"
+        />
+        <img
+          class="hero-moment__container__image"
+          src="/images/landing-page/new-hero-illustration.png"
+          alt="A visual composition of a sketched a quantum computer backdrop, a progress bar indicating a running job, and a laptop screen displaying the text 'running job...'"
+        />
+      </div>
+    </LandingHeroMomentGrid>
+  </article>
+</template>
+
+<script setup lang="ts">
+import { TextLink } from "~/types/links";
+
+interface Props {
+  version: string;
+}
+
+defineProps<Props>();
+
+const config = useRuntimeConfig();
+
+const getStartedLink: TextLink = {
+  url: `${config.public.siteUrl}/documentation/getting_started.html`,
+  label: "Get started",
+  segment: { cta: "get-started", location: "hero-moment" },
+};
+
+// TODO: Fix hydration text content mismatch
+const qiskitPronunciation = Math.random() < 0.5 ? "[kiss-kit]" : "[quiss-kit]";
+</script>
+
+<style lang="scss">
+@use "sass:math";
+@use "~/assets/scss/carbon.scss";
+@use "~/assets/scss/helpers/index.scss" as qiskit;
+
+.hero-moment {
+  height: calc(#{math.div(56rem * 40, 64)} + 2px);
+  position: relative;
+  overflow: hidden;
+
+  @include carbon.breakpoint-up(lg) {
+    // In Figma, the height is not enforced but the background is always
+    // visible completely so we do it in the CSS. A small correction is needed
+    // to be able of displaying the bottom lines of the grid.
+    height: calc(56rem + 2px);
+  }
+
+  &__square-link {
+    width: 4rem;
+    height: 4rem;
+    background-color: qiskit.$background-color-black;
+    position: absolute;
+    top: 21%;
+    left: 43%;
+  }
+
+  &__container {
+    height: 100%;
+    overflow: hidden;
+    padding-top: carbon.$spacing-10;
+    pointer-events: none;
+    position: relative;
+
+    @include carbon.breakpoint-down(md) {
+      background-image: none;
+    }
+
+    &__image {
+      height: 100%;
+      position: absolute;
+      right: 0;
+      top: 0;
+      z-index: -1;
+
+      @include carbon.breakpoint-down(lg) {
+        display: none;
+      }
+    }
+  }
+
+  &__version-info {
+    margin-bottom: carbon.$spacing-06;
+
+    @include carbon.breakpoint-up(md) {
+      margin-bottom: carbon.$spacing-07;
+    }
+
+    @include carbon.breakpoint-up(lg) {
+      margin-bottom: carbon.$spacing-11;
+    }
+  }
+
+  &__title {
+    margin-bottom: carbon.$spacing-06;
+    font-weight: 500;
+    font-size: 7rem;
+
+    // TODO: Force pointer events to allow the user to select text. Remove
+    // when decomissioning the Metal page.
+    pointer-events: auto;
+
+    @include carbon.breakpoint-up(md) {
+      margin-bottom: carbon.$spacing-07;
+    }
+
+    @include carbon.breakpoint-up(lg) {
+      margin-bottom: carbon.$spacing-08;
+    }
+  }
+
+  &__description {
+    margin-bottom: carbon.$spacing-06;
+
+    // TODO: Force pointer events to allow the user to select text. Remove
+    // when decomissioning the Metal page.
+    pointer-events: auto;
+
+    p {
+      color: carbon.$gray-80;
+      font-size: 1.3rem;
+    }
+
+    hr {
+      border: 1px solid carbon.$gray-80;
+      border-width: 1px 0 0;
+      margin: 0 0 carbon.$spacing-05 0;
+    }
+
+    ol {
+      list-style: decimal;
+      margin-left: carbon.$spacing-06;
+      font-size: 1.3rem;
+    }
+
+    @include carbon.breakpoint-up(md) {
+      margin-bottom: carbon.$spacing-07;
+    }
+
+    @include carbon.breakpoint-up(lg) {
+      margin-bottom: carbon.$spacing-08;
+    }
+  }
+
+  &__cta {
+    // TODO: Force pointer events to allow the user to select text. Remove
+    // when decomissioning the Metal page.
+    pointer-events: auto;
+    margin-bottom: carbon.$spacing-06;
+  }
+}
+</style>
