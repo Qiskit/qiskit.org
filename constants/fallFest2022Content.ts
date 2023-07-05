f6c0701225446ee671d43c35037909efa4728be5
Randy Tolentino <randytolentino@ibm.com>
2023-07-05 13:18:38 -0700

    fix(fall-fest): update links with descriptive text

--- constants/fallFest2022Content.ts
+++ constants/fallFest2022Content.ts
@@ -3,7 +3,7 @@ import { DescriptionCard } from "~/types/uiComponents";
 const header = {
   titleLine1: "Qiskit Fall Fest 2022:",
   cta: {
-    label: "Learn more!",
+    label: "Read the blog",
     url: "https://medium.com/qiskit/introducing-the-qiskit-fall-fest-feb8456b557",
     segment: {
       cta: "learn",
@@ -19,7 +19,7 @@ const header = {
     location: "",
     time: "",
     to: "https://medium.com/qiskit/introducing-the-qiskit-fall-fest-feb8456b557",
-    ctaLabel: "Learn more",
+    ctaLabel: "Learn about the Fall Fest",
     segment: {
       cta: "qiskit-fall-fest-blog",
       location: "header",
