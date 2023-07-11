3a45ac7008de0c0fc88e3986c181ff89d81439dd
Yaiza <yaiza.garcia.mm@gmail.com>
2023-07-11 16:38:57 +0200

    Fix lint

--- components/Layout/Banner/LayoutBannerNewLearningPlatform.vue
+++ components/Layout/Banner/LayoutBannerNewLearningPlatform.vue
@@ -1,16 +1,14 @@
 <template>
   <qiskit-banner>
     Introducing&nbsp;
-    <a href="#">
-      IBM Quantum Learning
-    </a>
-    , a new home for free quantum education resources. Continue to use Learn on
-    Qiskit until December 1st, 2023
+    <a href="#">IBM Quantum Learning</a>, a new home for free quantum education
+    resources. Continue to use Learn on Qiskit until December 1st, 2023
   </qiskit-banner>
 </template>
 
 <style lang="scss" scoped>
 @use "~/assets/scss/carbon.scss";
+
 qiskit-banner {
   display: inline-block;
   width: 100%;
