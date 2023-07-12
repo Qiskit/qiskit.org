f881264b2497b8eed4d35f56368eadcdb8e07060
Yaiza <yaiza.garcia.mm@gmail.com>
2023-07-12 12:38:08 +0200

    Adresses requested changes

--- components/Layout/Banner/LayoutBannerNewLearningPlatform.vue
+++ components/Layout/Banner/LayoutBannerNewLearningPlatform.vue
@@ -2,7 +2,7 @@
   <qiskit-banner>
     Introducing&nbsp;
     <a href="#">IBM Quantum Learning</a>, a new home for free quantum education
-    resources. Continue to use Learn on Qiskit until December 1st, 2023
+    resources. Continue to use Learn on Qiskit until December 1st, 2023.
   </qiskit-banner>
 </template>
 
@@ -12,7 +12,7 @@
 qiskit-banner {
   display: inline-block;
   width: 100%;
-  background: carbon.$cool-gray-20 0%;
+  background: carbon.$cool-gray-20;
   color: inherit;
 
   a,
