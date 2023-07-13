2b2acddf8104d844364a6d7655132994fcb6ba23
Yaiza <yaiza.garcia.mm@gmail.com>
2023-07-13 12:36:21 +0200

    Adds segment to the links on the banner

--- components/Layout/Banner/LayoutBannerNewLearningPlatform.vue
+++ components/Layout/Banner/LayoutBannerNewLearningPlatform.vue
@@ -1,11 +1,35 @@
 <template>
   <qiskit-banner>
     Introducing&nbsp;
-    <a href="#">IBM Quantum Learning</a>, a new home for free quantum education
-    resources. Continue to use&nbsp;
-    <a href="/learn">qiskit.org/learn</a>&nbsp;until December 1st, 2023.
+    <UiLink
+      :segment="newLearningPlatformLink.segment"
+      :url="newLearningPlatformLink.url"
+    >
+      {{ newLearningPlatformLink.label }}
+    </UiLink>
+    , a new home for free quantum education resources. Continue to use&nbsp;
+    <UiLink :segment="qiskitLearnLink.segment" :url="qiskitLearnLink.url">
+      {{ qiskitLearnLink.label }}
+    </UiLink>
+    &nbsp;until December 1st, 2023.
   </qiskit-banner>
 </template>
+
+<script setup lang="ts">
+import { TextLink } from "~/types/links";
+
+const newLearningPlatformLink: TextLink = {
+  url: "#",
+  label: "IBM Quantum Learning",
+  segment: { cta: "new-learning-platform", location: "announcement-banner" },
+};
+
+const qiskitLearnLink: TextLink = {
+  url: "/learn",
+  label: "qiskit.org/learn",
+  segment: { cta: "qiskit-learn", location: "announcement-banner" },
+};
+</script>
 
 <style lang="scss" scoped>
 @use "~/assets/scss/carbon.scss";
