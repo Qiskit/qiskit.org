5e733c49c0c384cbecc17c25955e7416aa0491ec
Maria Hernández <maria.hernandez.baeza@ibm.com>
2023-07-14 13:01:11 +0200

    fix: fixed template positioning

--- components/Layout/LeadSpace/LayoutLeadSpaceWithCard.vue
+++ components/Layout/LeadSpace/LayoutLeadSpaceWithCard.vue
@@ -43,81 +43,81 @@
 
     defineProps<Props>();
   </script>
-
-  <style lang="scss" scoped>
-    @use "sass:math";
-    @use "~/assets/scss/carbon.scss";
-    @use "~/assets/scss/helpers/index.scss" as qiskit;
-
-    .layout-lead-space-with-card {
-      @include qiskit.responsive-grid-bg-strip(
-        "/images/grid/grid-hero-learn.svg",
-        auto,
-        28rem
-      );
-
-      padding-top: carbon.$spacing-12;
-
-      @include carbon.breakpoint-down(md) {
-        padding-top: carbon.$spacing-09;
-      }
-
-      &__content {
-        display: flex;
-        flex-flow: column;
-        gap: carbon.$spacing-05;
-        justify-content: space-between;
-
-        @include carbon.breakpoint-down(xlg) {
-          gap: carbon.$spacing-06;
-        }
-
-        @include carbon.breakpoint-down(lg) {
-          gap: carbon.$spacing-09;
-        }
-      }
-
-      &__headline {
-        margin-top: carbon.$spacing-07;
-      }
-
-      &__description {
-        margin-top: carbon.$spacing-05;
-
-        @include carbon.breakpoint-down(xlg) {
-          margin-top: carbon.$spacing-06;
-        }
-
-        @include carbon.breakpoint-down(lg) {
-          margin-top: carbon.$spacing-09;
-        }
-
-        @include carbon.breakpoint-up(lg) {
-          $grid-columns: math.div(
-            6,
-            11
-          ); // Number of columns that the element will use at this breakpoint.
-
-          max-width: 100% * $grid-columns;
-        }
-      }
-
-      &__aside {
-        @include carbon.breakpoint-down(md) {
-          margin-top: carbon.$spacing-09;
-        }
-      }
-
-      &__card-title {
-        border-bottom: 4px solid qiskit.$border-color-tertiary;
-        display: inline;
-        padding-bottom: carbon.$spacing-02;
-        padding-right: carbon.$spacing-03;
-
-        &-wrapper {
-          margin-bottom: carbon.$spacing-06;
-        }
-      }
-    }
-  </style>
 </template>
+
+<style lang="scss" scoped>
+@use "sass:math";
+@use "~/assets/scss/carbon.scss";
+@use "~/assets/scss/helpers/index.scss" as qiskit;
+
+.layout-lead-space-with-card {
+  @include qiskit.responsive-grid-bg-strip(
+    "/images/grid/grid-hero-learn.svg",
+    auto,
+    28rem
+  );
+
+  padding-top: carbon.$spacing-12;
+
+  @include carbon.breakpoint-down(md) {
+    padding-top: carbon.$spacing-09;
+  }
+
+  &__content {
+    display: flex;
+    flex-flow: column;
+    gap: carbon.$spacing-05;
+    justify-content: space-between;
+
+    @include carbon.breakpoint-down(xlg) {
+      gap: carbon.$spacing-06;
+    }
+
+    @include carbon.breakpoint-down(lg) {
+      gap: carbon.$spacing-09;
+    }
+  }
+
+  &__headline {
+    margin-top: carbon.$spacing-07;
+  }
+
+  &__description {
+    margin-top: carbon.$spacing-05;
+
+    @include carbon.breakpoint-down(xlg) {
+      margin-top: carbon.$spacing-06;
+    }
+
+    @include carbon.breakpoint-down(lg) {
+      margin-top: carbon.$spacing-09;
+    }
+
+    @include carbon.breakpoint-up(lg) {
+      $grid-columns: math.div(
+        6,
+        11
+      ); // Number of columns that the element will use at this breakpoint.
+
+      max-width: 100% * $grid-columns;
+    }
+  }
+
+  &__aside {
+    @include carbon.breakpoint-down(md) {
+      margin-top: carbon.$spacing-09;
+    }
+  }
+
+  &__card-title {
+    border-bottom: 4px solid qiskit.$border-color-tertiary;
+    display: inline;
+    padding-bottom: carbon.$spacing-02;
+    padding-right: carbon.$spacing-03;
+
+    &-wrapper {
+      margin-bottom: carbon.$spacing-06;
+    }
+  }
+}
+</style>
