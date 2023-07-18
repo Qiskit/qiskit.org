811641d9842dfd59061dd17b9a255e6363758c29
Yaiza <yaiza.garcia.mm@gmail.com>
2023-07-18 10:39:04 +0200

    feat: remove "UQIC" course (#3426)
    * Delete Basics of quantum information page and images
    
    * Deletes Fundamentals of quantum algorithms page and images
    
    * Deletes unnecesary links
    
    * Changes CTA text
    
    * Open courses as external links

--- constants/links.ts
+++ constants/links.ts
@@ -2,8 +2,6 @@ import { TextLink } from "~/types/links";
 
 enum LearnStartLearningUrl {
   Introduction = "https://learn.qiskit.org/course/introduction",
-  BasicsQuantumInformation = "https://learn.qiskit.org/course/basics",
-  FundamentalsQuantumAlgorithms = "https://learn.qiskit.org/course/algorithms",
   QML = "https://learn.qiskit.org/course/machine-learning",
   SummerSchool2020 = "https://learn.qiskit.org/summer-school/2020/",
   SummerSchool2021 = "https://learn.qiskit.org/summer-school/2021/",
