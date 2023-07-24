3480a540b24ffb8d1c7e152cddd89aea078704f8
Pablo Aragón <pablo.aragon@ibm.com>
2023-07-24 11:38:07 +0200

    test(hooks): increase coverage (#3395)
    * new tests for advocate, airtable and event conversion utils
    
    * fix lint
    
    * new tests for advocate, airtable and event conversion utils
    
    * improve test coverage
    
    * hooks with 100% coverage 😁
    
    * fix lint
    
    * [WIP]
    
    * fix test
    
    * fix lint
    
    * fix mkdir in parallel
    
    * fix date test
    
    * fix character
    
    * new tests for advocate, airtable and event conversion utils
    
    * fix lint
    
    * new tests for advocate, airtable and event conversion utils
    
    * improve test coverage
    
    * hooks with 100% coverage 😁
    
    * fix lint
    
    * [WIP]
    
    * fix test
    
    * fix lint
    
    * fix mkdir in parallel
    
    * fix date test
    
    * fix character
    
    * Update tests/hooks/airtable-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/airtable-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/airtable-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * fix(#3395): remove unnecesary file
    
    Signed-off-by: Pablo Aragón <pablo.aragon@ibm.com>
    
    * Update tests/hooks/utils/conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/utils/conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * fix(#3395): remove unnecesary file
    
    Signed-off-by: Pablo Aragón <pablo.aragon@ibm.com>
    
    * fake mock data
    
    * fix test
    
    * Update tests/hooks/airtable-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/advocate-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/advocate-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/airtable-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/airtable-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/airtable-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/ecosystem-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/airtable-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/ecosystem-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/event-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Update tests/hooks/event-conversion-utils.spec.ts
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * fix: change Member interfecae for already existent
    
    * fix test
    
    * fix lint
    
    * improve test description
    
    Signed-off-by: Pablo Aragón <pablo.aragon@ibm.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
    
    * changes from PR suggestions
    
    * fix test
    
    ---------
    
    Signed-off-by: Pablo Aragón <pablo.aragon@ibm.com>
    Co-authored-by: Eddybrando Vásquez <eddybrando.vasquez@gmail.com>

--- tests/hooks/advocate-conversion-utils.spec.ts
+++ tests/hooks/advocate-conversion-utils.spec.ts
@@ -1,5 +1,19 @@
 import { beforeEach, describe, expect, test, vi } from "vitest";
-import AdvocatesAirtableRecords from "../../hooks/advocate-conversion-utils";
+import { AirtableBase } from "airtable/lib/airtable_base";
+import AdvocatesAirtableRecords, {
+  RECORD_FIELDS_IDS,
+} from "../../hooks/advocate-conversion-utils";
+
+class FakeAdvocatesAirtableRecords extends AdvocatesAirtableRecords {
+  constructor(
+    apiKey: string,
+    airtableBase: AirtableBase,
+    public recordFields?: Record<string, any>
+  ) {
+    super(apiKey, recordFields);
+    this.airtableBase = airtableBase;
+  }
+}
 
 describe("getters", () => {
   let advocatesAirtableRecords: AdvocatesAirtableRecords;
@@ -205,5 +219,125 @@ describe("getImage", () => {
       .mockImplementation(() => Promise.resolve());
     await advocatesAirtableRecords.getImage(fakeRecord);
     expect(mockStoreImage).toBeCalledWith(thumbnailUrl, expect.any(String));
+  });
+});
+
+describe("fetchAdvocates", () => {
+  // PREPARING MOCKED DATA
+  const mockRecordFields = {
+    name: "Name",
+    city: "City",
+    country: "Country",
+    region: "Region",
+    slackId: "Slack ID",
+    slackUsername: "Slack Username",
+  };
+  const fakeRecords = [
+    {
+      get: (field: string) => {
+        switch (field) {
+          case "Name":
+            return "Nova";
+          case "City":
+            return "Gotham City";
+          case "Country":
+            return "Canada";
+          case "Region":
+            return "South America";
+          case "Slack ID":
+            return "ID1234567890";
+          case "Slack Username":
+            return "U1234567890";
+        }
+      },
+    },
+    {
+      get: (field: string) => {
+        switch (field) {
+          case "Name":
+            return "Nova";
+          case "City":
+            return "Gotham City";
+          case "Country":
+            return "Canada";
+          case "Region":
+            return "South America";
+          case "Slack ID":
+            return "ID1234567890";
+          case "Slack Username":
+            return "U1234567890";
+        }
+      },
+    },
+  ];
+
+  const airtableEachPageMockFn = vi
+    .fn()
+    .mockImplementation((cb: (records: any, nextPage: any) => void): void => {
+      cb(fakeRecords, () => {});
+    });
+  const airtableSelectMockFn = vi.fn().mockReturnValue({
+    eachPage: airtableEachPageMockFn,
+  });
+  const airtableBase = vi.fn().mockReturnValue({
+    select: airtableSelectMockFn,
+  });
+  // END PREPARING MOCKED DATA
+
+  test("has a working Airtable query", async () => {
+    const advocatesAirtableRecords = new FakeAdvocatesAirtableRecords(
+      "testApiKey",
+      airtableBase as unknown as AirtableBase,
+      mockRecordFields
+    );
+
+    const result = await advocatesAirtableRecords.fetchAdvocates();
+
+    expect(airtableBase).toHaveBeenCalledWith("Advocates");
+    expect(airtableSelectMockFn).toHaveBeenCalledWith({
+      fields: Object.values(mockRecordFields),
+      filterByFormula: `AND({${mockRecordFields.slackId}})`,
+      sort: [{ field: mockRecordFields.name, direction: "asc" }],
+    });
+    expect(result).toStrictEqual([
+      {
+        city: "Gotham City",
+        country: "Canada",
+        image: "/images/advocates/no-advocate-photo.png",
+        name: "Nova",
+        region: "South America",
+        slackId: "ID1234567890",
+        slackUsername: "U1234567890",
+      },
+      {
+        city: "Gotham City",
+        country: "Canada",
+        image: "/images/advocates/no-advocate-photo.png",
+        name: "Nova",
+        region: "South America",
+        slackId: "ID1234567890",
+        slackUsername: "U1234567890",
+      },
+    ]);
+  });
+
+  test("gets record fields from `getAllFieldNames` if there are none yet", async () => {
+    const advocatesAirtableRecords = new FakeAdvocatesAirtableRecords(
+      "testApiKey",
+      airtableBase as unknown as AirtableBase
+    );
+
+    advocatesAirtableRecords.getAllFieldNames = vi
+      .fn()
+      .mockReturnValue(mockRecordFields);
+
+    await advocatesAirtableRecords.fetchAdvocates();
+
+    expect(advocatesAirtableRecords.getAllFieldNames).toBeCalledWith(
+      RECORD_FIELDS_IDS
+    );
+    expect(advocatesAirtableRecords.recordFields).toStrictEqual(
+      mockRecordFields
+    );
   });
 });
