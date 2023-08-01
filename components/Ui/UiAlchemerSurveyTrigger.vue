958f95e7c021427d7c092b8f0e3735d86ba136e0
Eddybrando Vásquez <eddybrando.vasquez@gmail.com>
2023-08-01 17:51:05 +0200

    feat(alchemer): popup trigger "feedback" tab

---
+++ components/Ui/UiAlchemerSurveyTrigger.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- The class `alchemer-survey-trigger` is required to trigger the popup -->
+  <bx-btn
+    class="ui-alchemer-survey-trigger alchemer-survey-trigger"
+    size="sm"
+    type="button"
+  >
+    Feedback</bx-btn
+  >
+</template>
+
+<style lang="scss" scoped>
+@use "~/assets/scss/carbon.scss";
+
+.ui-alchemer-survey-trigger {
+  &::part(button) {
+    padding: carbon.$spacing-04 carbon.$spacing-02;
+    position: fixed;
+    right: 0;
+    top: 50%;
+    transform: rotate(180deg);
+    writing-mode: vertical-lr;
+    z-index: 300;
+  }
+}
+</style>
