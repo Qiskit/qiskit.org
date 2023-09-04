14568285b0388555f4ce7f94f3d4090b60a3e66b
Randy <randytolentino@ibm.com>
2023-09-04 03:48:00 -0700

    [Fall-fest]: prepare fall fest page 2023 (#3525)
    * feat(fall-fest): add fall fest 2023 page and content file
    
    * feat(fall-fest): add fall fest 2023 directory file, event img, and intro copy
    
    * feat(fall-fest): remove directory and agenda for 2023
    
    * Update pages/events/fall-fest.vue
    
    Co-authored-by: Yaiza <yaiza.garcia.mm@gmail.com>
    
    * Update pages/events/fall-fest.vue
    
    Co-authored-by: Yaiza <yaiza.garcia.mm@gmail.com>
    
    * Update pages/events/fall-fest.vue
    
    Co-authored-by: Yaiza <yaiza.garcia.mm@gmail.com>
    
    * feat(fall-fest): add description to content file
    
    * feat(fall-fest): fix lint error
    
    * feat(fall-fest): fix errors
    
    ---------
    
    Co-authored-by: Yaiza <yaiza.garcia.mm@gmail.com>

---
+++ pages/events/fall-fest.vue
@@ -0,0 +1,97 @@
+<template>
+  <article class="fall-fest-page">
+    <LayoutLeadSpaceWithCard
+      :cta="headerData.cta"
+      :card-title="headerData.cardSectionHeading"
+    >
+      <template #title>
+        {{ headerData.titleLine1 }}
+      </template>
+      <template #description>
+        <p>{{ headerData.description }}</p>
+      </template>
+      <template #card>
+        <EventsCard
+          :cta-label="headerData.card.ctaLabel"
+          :image="headerData.card.image"
+          :location="headerData.card.location"
+          :segment="headerData.card.segment"
+          :time="headerData.card.time"
+          :title="headerData.card.title"
+          :to="headerData.card.to"
+          vertical-layout
+        >
+          {{ headerData.card.description }}
+        </EventsCard>
+      </template>
+    </LayoutLeadSpaceWithCard>
+
+    <div class="cds--grid fall-fest-page__content">
+      <section class="fall-fest-page__section">
+        <UiHelpfulResources
+          class="fall-fest-page__section"
+          :title="helpfulResourcesData.title"
+          :resources="helpfulResourcesData.resources"
+        />
+      </section>
+    </div>
+  </article>
+</template>
+
+<script setup lang="ts">
+import { header, helpfulResources } from "~/constants/fallFest2023Content";
+
+definePageMeta({
+  pageTitle: header.titleLine1,
+  routeName: "fall-fest",
+});
+
+const config = useRuntimeConfig();
+
+const title = header.titleLine1;
+const description =
+  "The Qiskit Fall Fest is a collection of quantum computing events from universities around the world, organized by students and supported by the IBM Quantum community.";
+const image = `${config.public.siteUrl}/images/events/fall-fest/fall-fest-logo-2023.png`;
+const pageUrl = `${config.public.siteUrl}/events/fall-fest`;
+
+useSeoMeta({
+  title,
+  ogTitle: title,
+  description,
+  ogDescription: description,
+  ogImage: image,
+  ogImageAlt: title,
+  ogUrl: pageUrl,
+  ogType: "website",
+  twitterCard: "summary_large_image",
+  twitterDescription: description,
+});
+
+const headerData = header;
+const helpfulResourcesData = helpfulResources;
+</script>
+
+<style lang="scss" scoped>
+@use "~/assets/scss/carbon.scss";
+@use "~/assets/scss/helpers/index.scss" as qiskit;
+
+.fall-fest-page {
+  display: flex;
+  flex-direction: column;
+
+  &__section {
+    margin-top: carbon.$spacing-10;
+    margin-bottom: carbon.$spacing-07;
+
+    @include carbon.breakpoint-down(lg) {
+      margin-bottom: carbon.$spacing-05;
+    }
+  }
+
+  &__content {
+    @include carbon.breakpoint-down(lg) {
+      max-width: 100%;
+    }
+  }
+}
+</style>
